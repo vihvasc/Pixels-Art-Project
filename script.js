71deea6223902476817699a7420dbe16a2840b13
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-13 15:20:15 -0300

    Refatora requisitos 1-2 para dentro de funçoes.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // 01. Adicione à página o título "Paleta de Cores".
 // O título deverá ficar dentro de uma tag h1 com o id denominado title;
 // O texto do título deve ser exatamente "Paleta de Cores".
+function criarElementoH1() {
+    const novoH1 = document.createElement('h1');
+    novoH1.textContent = 'Paleta de Cores';
+    novoH1.id = 'title';
 
-const novoH1 = document.createElement('h1');
-novoH1.textContent = 'Paleta de Cores';
-novoH1.id = 'title';
-
-document.body.appendChild(novoH1);
+    document.body.appendChild(novoH1);
+};
+criarElementoH1();
 
 // 02. Adicione à página uma paleta de quatro cores distintas.
 // A paleta de cores deve ser um elemento com id denominado color-palette, ao passo que cada cor individual da paleta de cores deve possuir a classe color;
@@ -14,15 +16,18 @@ document.body.appendChild(novoH1);
 // Cada elemento da paleta de cores deverá ter uma borda preta, sólida e com 1 pixel de largura;
 // A paleta de cores deverá listar todas as cores disponíveis para utilização lado a lado, e deverá ser posicionada abaixo do título "Paleta de Cores";
 
-const paletaCores = document.createElement('div');
-paletaCores.id = 'color-palette';
-const totalCores = 4;
+function criarPaletaCores() {
+    const paletaCores = document.createElement('div');
+    paletaCores.id = 'color-palette';
+    const totalCores = 4;
 
-document.body.appendChild(paletaCores);
-
-for (let index = 0; index < totalCores; index += 1) {
-    const novaCor = document.createElement('div');
-    novaCor.className = 'color';
+    document.body.appendChild(paletaCores);
     
-    paletaCores.appendChild(novaCor);
-}
+    for (let index = 0; index < totalCores; index += 1) {
+        const novaCor = document.createElement('div');
+        novaCor.className = 'color';
+        paletaCores.appendChild(novaCor);
+    };
+};
+criarPaletaCores();
+
