b30c906973258213c751805bc87b71b892525fe2
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-13 17:53:10 -0300

    Add exercise 01

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let h1 = document.createElement('h1');
-h1.id = 'title';
-h1.innerText = 'Paleta de Cores';
-document.querySelector('body').appendChild(h1);
+
+// Requisito 01
+function createTitle () {
+  let h1 = document.createElement( 'h1' );
+  h1.id = 'title';
+  h1.innerText = 'Paleta de Cores';
+  document.querySelector( 'body' ).appendChild( h1 );
+}
+createTitle();
+
+
+
+
+
