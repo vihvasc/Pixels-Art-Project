b2f12c35889739a788e94097b71786fb35972342
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2021-08-13 16:05:53 -0300

    [Desafio 2] Cria paleta com 4 cores diferentes

---
+++ script.js
@@ -0,0 +1,42 @@
+function fourDistinctColorPalette (quantCores) {
+	let div = document.querySelector('#color-palette');
+
+	for (let i = 1; i <= quantCores; i += 1) {
+		let palette = document.createElement('div');
+		
+		palette.className = 'color';
+		palette.style.backgroundColor = colorRepeat();
+		palette.style.border = '1px solid black';
+		div.appendChild(palette);		
+	}
+}
+
+fourDistinctColorPalette(4);
+
+function colorGenerator () {
+	let req1 = (Math.random() * 256) -1;
+	let req2 = (Math.random() * 256) -1;
+	let req3 = (Math.random() * 256) -1;
+
+  if(req1 === 255 && req2 === 255 && req3 === 255) {
+		return colorGenerator();
+	}
+
+	let randomColor = 'rgb(' + req1 + ',' + req2 + ',' + req3 + ')';
+	return randomColor;
+}
+
+function colorRepeat () {
+	let cores = document.querySelectorAll('.color');
+	let newColor = colorGenerator();
+
+	if (cores.length === 0) {
+		return newColor;
+	}
+	for (let i = 0; i < cores.length; i += 1) {
+		if (newColor === cores[i]) {
+			colorGenerator();
+		}
+		return newColor;
+	}
+}
