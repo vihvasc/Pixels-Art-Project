31cdf7efe16babc3421d47e51b1559cccf1efd31
Gustavo <fariaguga@hotmail.com>
2021-08-13 15:26:32 -0300

    Adiciona Requisito 2 e 3

---
+++ script.js
@@ -0,0 +1,12 @@
+let preto = document.querySelectorAll(".color")[0];
+preto.style.backgroundColor = "black";
+let vermelho = document.querySelectorAll(".color")[1];
+vermelho.style.backgroundColor = "red";
+let azul = document.querySelectorAll(".color")[2];
+azul.style.backgroundColor = "blue";
+let verde = document.querySelectorAll(".color")[3];
+verde.style.backgroundColor = "green";
+
+
+
+
