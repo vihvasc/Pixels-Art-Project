51179f92c2e67fce8598f1ab3913d0283f18f400
Edson Lima <edsoneduardo526@gmail.com>
2021-08-13 18:32:40 -0300

    Requisito 4 (take 2)

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ div.className = "pixel";
 linha1.appendChild(div);
 }
 let linha2 = window.document.getElementById("linha2")
-for (let index = 0; index < 6; index += 1){
+for (let index = 0; index < 5; index += 1){
 let div = document.createElement("div");
 div.className = "pixel";
 linha2.appendChild(div);
 }
 let linha3 = window.document.getElementById("linha3")
-for (let index = 0; index < 6; index += 1){
+for (let index = 0; index < 5; index += 1){
 let div = document.createElement("div");
 div.className = "pixel";
 linha3.appendChild(div);
 }
 let linha4 = window.document.getElementById("linha4")
-for (let index = 0; index < 6; index += 1){
+for (let index = 0; index < 5; index += 1){
 let div = document.createElement("div");
 div.className = "pixel";
 linha4.appendChild(div);
 }
 let linha5 = window.document.getElementById("linha5")
-for (let index = 0; index < 6; index += 1){
+for (let index = 0; index < 5; index += 1){
 let div = document.createElement("div");
 div.className = "pixel";
 linha5.appendChild(div);
