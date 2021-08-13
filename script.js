e4eef6841ba33d0ed62c5052381891c2e91a0cf6
taiserrao <contato.taianeserrao@gmail.com>
2021-08-13 17:15:13 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const redBloc = document.querySelector('#red');
 const blueBloc = document.querySelector('#blue');
 const greenBloc = document.querySelector('#green');
 
+// Requisito 7
 function selectorColor(event) {
   const classSelected = document.querySelector('.selected');
   classSelected.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
+// Requisito 8
 blackBloc.addEventListener('click', selectorColor);
 redBloc.addEventListener('click', selectorColor);
 blueBloc.addEventListener('click', selectorColor);
@@ -21,3 +23,13 @@ function coloredPixel(event) {
 }
 
 pixelBd.addEventListener('click', coloredPixel);
+
+// Requisito 9
+const pixel = document.querySelectorAll('.pixel');
+const clearButton = document.querySelector('#clear-board');
+
+function clearBloc() {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
+  }
+} clearButton.addEventListener('click', clearBloc);
