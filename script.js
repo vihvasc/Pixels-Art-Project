53836b9e2e77eb176c18df7e760d158210379626
Rodolfo <rodolfoserralha@hotmail.com>
2021-08-13 16:18:16 -0300

    arrumando espacos do lint

--- script.js
+++ script.js
@@ -5,31 +5,29 @@ h1.id = 'title';
 h1.innerText = 'Paleta de Cores';
 document.body.appendChild(h1);
 
-// Requisito 2
+// Requisito 2 e 3
 
 function createDivPai() {
-        const div = document.createElement('div');
-        div.id = 'color-palette';
-        document.body.appendChild(div);
+  const div = document.createElement('div');
+  div.id = 'color-palette';
+  document.body.appendChild(div);
 }
 
 createDivPai();
 
 function createDivFilhas() {
-    const arrayColors = ['black', 'blue', 'pink', 'yellow'];
-    for (let index = 0; index < 4; index += 1) {
-        const div = document.createElement('div');
-        div.className = 'color';
-        div.style.backgroundColor = arrayColors[index];
-        div.style.border = '1px solid black';
-        const divCores = document.querySelector('#color-palette');
-        divCores.appendChild(div);
-    }
+  const arrayColors = ['black', 'blue', 'pink', 'yellow'];
+  for (let index = 0; index < 4; index += 1) {
+    const div = document.createElement('div');
+    div.className = 'color';
+    div.style.backgroundColor = arrayColors[index];
+    div.style.border = '1px solid black';
+    const divCores = document.querySelector('#color-palette');
+    divCores.appendChild(div);
+  }
 }
 
 createDivFilhas();
-
-// Exercício 3
 
 
 
