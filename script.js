28cd6d8b8ca5e1551ba8bd6041409f9ac314b720
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-13 15:48:38 -0300

    Adiciona quadro de 25 pixels a pagina

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function paintColorPalette() {
 }
 
 paintColorPalette();
+
+function createPixelBoard() {
+  let pixelBoard = document.getElementById('pixel-board');
+  for (let i = 0; i < 5; i += 1) {
+    let boardLine = document.createElement('div');
+    boardLine.className = 'boardLine';
+    for (let j = 0; j < 5; j+= 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      boardLine.appendChild(pixel);
+      pixel.style.backgroundColor = 'white';
+    }
+    pixelBoard.appendChild(boardLine);
+  }
+}
+
+createPixelBoard();
