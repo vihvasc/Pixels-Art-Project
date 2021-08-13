741bd26fcff3d1151cbac9128aaede8b53036453
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-13 16:21:31 -0300

    add requisito 02

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-window.onload = function(){
+window.onload = function () {
   // requisito 1 
-  let getTitle = document.getElementById('title');
+  const getTitle = document.getElementById('title');
   getTitle.innerText = 'Paleta de Cores';
+
+  // requisito 2
+  function addPalette (){
+  const colors = ['purple', 'black', 'green', 'blue'];
+  const getDivColors = document.getElementsByClassName('color');
+  for (let i = 0; i < getDivColors.length; i += 1){
+    let divs = getDivColors[i];
+    divs.style.backgroundColor = colors[i];
+    divs.style.display = 'inline-block'
+  }
 }
+addPalette();
+}
+
+  // addPalette();
