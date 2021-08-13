b387fe25bb386d4bdacdb0cf4dbbad20a748bf38
David Souza <dabcs@poli.ufrj.br>
2021-08-13 12:04:16 -0300

    criacao do quadro a ser pintado e configuracao do estilo

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("oi");
+window.onload = function (){
+    createPixel(25);
+}
+
+function createPixel(nPixel){
+    
+    for(let i = 0; i < nPixel; i += 1){
+        let divPixel = document.createElement('div');
+        divPixel.className = 'pixel';
+        document.querySelector('#pixel-board').appendChild(divPixel);
+        console.log('teste');
+    }
+}
