b89ca1a7979c5bc5e1c004e4bf044f171e94101c
Matheus <matheusfelippeh@gmail.com>
2021-08-13 16:30:05 -0300

    add requisite 4

---
+++ script.js
@@ -0,0 +1,14 @@
+function createGrid(){
+    numberOfLines = 5;
+    numberOfColumns = 5;
+    for(let index = 0; index < numberOfLines; index += 1){
+        let line = document.querySelectorAll('.line-pixel')[index]
+        for(let index2 = 0; index2 < numberOfColumns; index2 +=1 ){
+            let pixelBox = document.createElement('div')
+            pixelBox.className = 'pixel'
+            line.appendChild(pixelBox) 
+        }
+    }
+}
+createGrid();
+
