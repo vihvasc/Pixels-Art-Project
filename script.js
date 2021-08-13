c60c4c2856a94e47bf67a30aa3cd267b890d9152
DanielHott <daniel_hott@outlook.com>
2021-08-13 16:11:24 -0300

    Adicionando requisito numero 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let classe = document.getElementsByClassName('color')
 
-for(let i = 0; i < classe.length; i+= 1){
+for (let i = 0; i < classe.length; i += 1) {
     let valor = classe[i].textContent
     classe[i].style.backgroundColor = valor
 }
