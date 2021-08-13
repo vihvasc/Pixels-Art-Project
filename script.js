b5a39c6090d86ed7161abc861c7c98b611a8b778
Gabriel Rodrigues <huijirohankei@gmail.com>
2021-08-13 15:57:32 -0300

    Feito requisito 4-7

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 function setupGrid(y, x, pixelSize) {
-  let numberOfSquares = y * x;
-  let squareSize = pixelSize;
-
   let grid = document.createElement('div');
   document.body.appendChild(grid);
 
-  grid.id = 'pixel-board';
-  grid.style.width = `${x * squareSize} px`;
-  grid.style.height = `${y * squareSize} px`;
+  document.documentElement.style.setProperty('--pixel-size', pixelSize + 'px');
 
-  for (let index = 0; index < numberOfSquares; index++) {
-    let pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    pixel.style.width = `${squareSize} px`;
-    pixel.style.height = `${squareSize} px`;
-    pixel.style.backgroundColor = 'white';
+  grid.id = `pixel-board`;
+  grid.style.maxWidth = `${x * pixelSize} px`;
+  grid.style.maxHeight = `${y * pixelSize} px`;
 
-    grid.appendChild(pixel);
+  for (let index = 0; index < x; index++) {
+    let line = document.createElement('div');
+    line.style.display = 'flex';
+    for (let index = 0; index < y; index++) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.style.backgroundColor = 'white';
+      pixel.style.border = '1px solid black';
+
+      line.appendChild(pixel);
+    }
+    grid.appendChild(line);
   }
 
-  console.log(
-    `Created a grid of ${x} by ${y} with a size of ${squareSize} px.`
-  );
+  console.log(`Created a grid of ${x} by ${y} with a size of ${pixelSize} px.`);
 }
 
-setupGrid(5, 5, 32);
+setupGrid(5, 5, 40);
+
+let palette = document.querySelectorAll('.color');
+
+palette.forEach((e) => {
+  e.addEventListener('click', (e) => {
+    let selectedColor = document.querySelector('.selected');
+    selectedColor.className = 'color';
+    console.log('clicked' + e.target);
+    e.target.className = 'color selected';
+  });
+});
