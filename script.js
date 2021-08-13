f6447bca678e7445183ba05bb0e8480a3072c5c4
Amanda Fernandes <aferanda@gmail.com>
2021-08-13 17:11:51 -0300

    Adiciona requisito 4

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function createTitle() {
 
 createTitle();
 
-// Requisito 2
+// Requisito 2 e 3
 function createPalette() {
   const colors = ['black', 'lightblue', 'lightgreen', 'pink'];
   const divFather = document.createElement('div');
@@ -19,9 +19,23 @@ function createPalette() {
     const divChild = document.createElement('div');
     divChild.className = 'color';
     divChild.style.backgroundColor = colors[index];
-    divChild.style.border = '1px solid black';
     divFather.appendChild(divChild);
   }
 }
 
 createPalette();
+
+// Requisito 4
+function createFrame() {
+  const divFather = document.createElement('div');
+  divFather.id = 'pixel-board';
+  document.body.appendChild(divFather);
+  for (let index = 0; index < 25; index += 1) {
+    const divChild = document.createElement('div');
+    divChild.className = 'pixel';
+    divChild.style.backgroundColor = 'white';
+    divFather.appendChild(divChild);
+  }
+}
+
+createFrame();
