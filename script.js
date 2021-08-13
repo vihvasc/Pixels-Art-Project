9db7745976a8c6cb514df7ab2afe1de073984a99
Flávio Bianchetti <fjb.developer@gmail.com>
2021-08-13 17:39:37 -0300

    atendido requisito 8 - validado via npm

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 function startPage() {
-  let parent = document.getElementById('color-palette');
-  let selectedColor = parent.firstElementChild;
-  selectedColor.classList.add('selected');
+  const parent = document.getElementById('color-palette').children;
+  parent[0].style.backgroundColor = 'black';
+  parent[1].style.backgroundColor = 'green';
+  parent[2].style.backgroundColor = 'gold';
+  parent[3].style.backgroundColor = 'blue';
+  parent[0].classList.add('selected');
 }
 
 startPage();
 
+function selectColorInPalette(event) {
+  if (event.target.className === 'color') {
+    const selectedColor = document.querySelector('.selected');
+    selectedColor.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+
 function selectPalette() {
-  let parent = document.getElementById('color-palette');
-  
-  parent.addEventListener('click', function(event) {
-    if (event.target.className === 'color') {
-      let selectedColor = document.querySelector('.selected');
-      selectedColor.classList.remove('selected');
-      event.target.classList.add('selected');
-    }    
-  });
+  const parent = document.getElementById('color-palette');
+  parent.addEventListener('click', selectColorInPalette);
 }
 
 selectPalette();
+
+function paintSquareInBoard(event) {
+  if(event.target.className === 'pixel') {
+    const selectedColor = document.querySelector('.selected');
+    event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+  }
+}
+
+function paintSquares() {
+  const parent = document.getElementById('pixel-board');
+  parent.addEventListener('click', paintSquareInBoard);
+}
+
+paintSquares();
