856ba479210192cae10b8f6c4b728a8803a4b0ed
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-13 17:17:10 -0300

    adiciona requisito 4

---
+++ script.js
@@ -0,0 +1,13 @@
+// window.onload = function() {
+// const heightPxBoard = 5;
+// const basePixelBoard = 5;
+// createBoard(basePixelBoard, heightPxBoard);
+// }
+
+// function createBoard(base, height) {
+//   const pixelBoard = document.querySelector('#board');
+//   const pixel = document.createElement('div');
+//   pixel.className = 'pixel';
+//   pixelBoard.appendChild(pixel);
+   
+// }
