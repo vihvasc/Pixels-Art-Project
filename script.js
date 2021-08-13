dd853b0589834922e51cb33a5ffb01ad63470b9e
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-13 16:01:06 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const blackColor = document.querySelector('.color');
+const colors = document.querySelectorAll('.color');
+const colorsArray = Array.from(colors);
+const colorsArrayLength = colorsArray.length;
+const blackColor = colorsArray[0];
 blackColor.classList.add('selected');
+
+function addSelected(originEvent) {
+  const element = originEvent.target;
+  for (let counter = 0; counter < colorsArrayLength; counter += 1) {
+    if (colorsArray[counter].classList.contains('selected')) {
+      colorsArray[counter].classList.remove('selected');
+    }
+  }
+  element.classList.add('selected');
+  console.log(colorsArray[0].classList)
+}
+
+for (let index = 0; index < colorsArrayLength; index += 1) {
+  colorsArray[index].addEventListener('click', addSelected);
+}
