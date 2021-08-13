5af2d8999bb0ef6b28aac00a5991fe84e4a0c7ae
leandro oliveira <leandrooliveira35@gmail.com>
2021-08-13 15:08:22 -0300

    requisitos 1 e 2 completos

---
+++ script.js
@@ -0,0 +1,9 @@
+const firstColor = document.getElementById('first-color');
+const secondColor = document.getElementById('second-color');
+const thirdColor = document.getElementById('third-color');
+const fourthColor = document.getElementById('fourth-color');
+
+firstColor.style.backgroundColor = 'black';
+secondColor.style.backgroundColor = 'green';
+thirdColor.style.backgroundColor = 'orange';
+fourthColor.style.backgroundColor = 'purple';
