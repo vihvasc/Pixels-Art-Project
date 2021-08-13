c572c55a7689f7c08a6c64a7019d8b13c716340c
aaguero96 <andre.luiz_1996@hotmail.com>
2021-08-13 15:51:57 -0300

    Fix errors in lint part3

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 const pixelBoard = document.getElementById('pixel-board');
 
 function createPixelBoard(m, n) {
-  for (let i = 0; i < m; i++) {
-    let line = document.createElement('div');
+  for (let i = 0; i < m; i += 1) {
+    const line = document.createElement('div');
     line.className = 'linePixelBoard';
     pixelBoard.appendChild(line);
-    for (let j = 0; j < n; j++) {
-      let column = document.createElement('div');
+    for (let j = 0; j < n; j += 1) {
+      const column = document.createElement('div');
       column.className = 'pixel';
       document.querySelectorAll('#pixel-board .linePixelBoard')[i].appendChild(column);
     }
   }
 }
 
-window.onload = function() {
+window.onload = function () {
   createPixelBoard(5, 5);
-}
+};
