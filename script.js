60d46475eab7fd87de1935647dbe2b94fc4e3624
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-08-13 16:14:47 -0300

    requesito 4

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function colorPalette () {
 }
 
 colorPalette () 
+
+function board () {
+  let pixel = 5;
+  let wide = pixel;
+  let height = pixel
+  let lines = document.getElementsByClassName('line');
+
+  for (let i = 0; i < lines.length; i +=1){
+    for (let index = 0; index < wide; index += 1){
+      let square = document.createElement('div');
+      lines[i].appendChild(square);
+      square.className = 'pixel';
+    };
+  };
+}
+
+board ();
