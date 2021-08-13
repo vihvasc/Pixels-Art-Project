b1c783149dbb5fad8ce7cf1d10aa1e541e76bd94
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-13 16:23:19 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const colors = document.querySelectorAll('.color');
 const colorsArray = Array.from(colors);
 const colorsArrayLength = colorsArray.length;
 const blackColor = colorsArray[0];
+const pixels = document.querySelectorAll('.pixel');
+const pixelsArray = Array.from(pixels);
+const pixelsArrayLength = pixelsArray.length;
 blackColor.classList.add('selected');
 
 function addSelected(originEvent) {
@@ -12,9 +15,21 @@ function addSelected(originEvent) {
     }
   }
   element.classList.add('selected');
-  console.log(colorsArray[0].classList)
 }
 
 for (let index = 0; index < colorsArrayLength; index += 1) {
   colorsArray[index].addEventListener('click', addSelected);
 }
+
+function paintSquare(originEvent) {
+  const selectedColorElement = document.querySelector('.selected');
+  const selectedColor = getComputedStyle(selectedColorElement).backgroundColor;
+  const myPixel = originEvent.target;
+  myPixel.style.background = selectedColor;
+}
+
+for (let index = 0; index < pixelsArrayLength; index += 1) {
+  pixelsArray[index].addEventListener('click', paintSquare);
+}
+
+
