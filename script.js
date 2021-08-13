8309a0851caf335a45bc0751415df45945a9dcaa
Danilo <dcalado.fisio@yahoo.com.br>
2021-08-13 18:43:58 -0300

    adiciona tabela e deixa a cor inicial do primeirp pixel preta

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ let divcolorPallete = document.getElementsByClassName('color selected')[0].style
     let pixel = document.querySelectorAll('.pixel');
     let corselecionada = paleta[0].style.backgroundColor
  }
+
+ function createBoard(pixels) {
+    const pixelBoard = document.querySelector('#pixel-board');
+    pixelBoard.innerHTML = '';
+    for (let i = 0; i < pixels; i += 1) {
+      const pixelRow = document.createElement('div');
+      pixelRow.className = 'pixel-row flex center';
+      for (let ia = 0; ia < pixels; ia += 1) {
+        const newPixel = document.createElement('div');
+        newPixel.className = 'pixel';
+        newPixel.addEventListener('click', (e) => {
+          const selectedColor = document.querySelector('.selected');
+          e.target.style.backgroundColor = getComputedStyle(selectedColor).backgroundColor;
+        });
+        newPixel.addEventListener('dblclick', (e) => {
+          e.target.style.backgroundColor = 'white';
+        });
+        pixelRow.appendChild(newPixel);
+      }
+      pixelBoard.appendChild(pixelRow);
+    }
+  }
+ 
+  createBoard(5);
+
+  
+
+
+
+   
+
+    
+
+    
