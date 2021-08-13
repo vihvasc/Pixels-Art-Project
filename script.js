b50a57ee25c6b7b8aea8b29fda5dcddeea9c4ca9
PedroAugmed <pedroaugmed7@gmail.com>
2021-08-13 18:04:16 -0300

    ADD Requisitos 2 ao 5

---
+++ script.js
@@ -0,0 +1,29 @@
+let ChoiceSizeQuadrado = 5
+let altura = ChoiceSizeQuadrado
+let largura = ChoiceSizeQuadrado
+ 
+let last = document.querySelectorAll(".pixel")[4]
+function CreatQuadroDePixels (){
+let number = 0
+
+
+    for (let i = 1; i <= ChoiceSizeQuadrado; i+= 1){
+    
+    let CreatPixel = document.createElement('div')
+    let SectionPixels = document.getElementById("pixel-board")
+    SectionPixels.appendChild(CreatPixel)
+  number ++
+    CreatPixel.className ="pixel"  
+  if(number === 5){
+      CreatPixel.classList.add("last")
+  }
+  
+}
+
+}
+CreatQuadroDePixels()
+CreatQuadroDePixels()
+CreatQuadroDePixels()
+CreatQuadroDePixels()
+CreatQuadroDePixels()
+
