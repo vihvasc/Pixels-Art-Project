bc0016fa831cdfe4dd1b7baf45786cdd31680b38
davi <davisoarescoura@gmail.com>
2021-08-13 21:52:29 +0200

    adiciona o setimo requisito do projeto

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const colorTwo = document.getElementById("color-two");
 const colorThree = document.getElementById("color-three");
 const colorFour = document.getElementById("color-four");
 
-
+colorOne.style.backgroundColor = "black";
+colorTwo.style.backgroundColor = "red";
+colorThree.style.backgroundColor = "green";
+colorFour.style.backgroundColor = "yellow";
 
 colorOne.addEventListener('click', addSelectedClass);
 colorTwo.addEventListener('click', addSelectedClass);
@@ -16,3 +19,8 @@ function addSelectedClass(event) {
     checkSelected.classList.remove('selected');
     event.target.classList.add('selected');
    }
+
+function changeColor(event) {
+    const colorSelected = document.querySelector(".selected").style.backgroundColor;
+    event.target.style.backgroundColor = colorSelected;
+}   
