eac85857fe07ff4321fb89a2e91fdaff20349ba3
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-13 17:59:51 -0300

    Cumprido o requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+
 const blackFirst = document.querySelector('.color-green');
 blackFirst.style.backgroundColor = 'rgb(0,0,0)';
+blackFirst.classList.add('selected');
+
+let colorsPalette = document.querySelectorAll('.color')
+
+for (let index = 0; index < colorsPalette.length; index += 1) {
+  colorsPalette[index].addEventListener('click', getClick);
+}
+
+function getClick (originEvent) {
+  // let classes =  originEvent.target.classList;
+  let selectedBox = document.querySelector('.selected');
+  selectedBox.classList.remove('selected');
+  originEvent.target.classList.add('selected');
+  console.log(originEvent.target.classList);
+  console.log(originEvent.target.style.backgroundColor);
+}
