83b85a0562f3ecbe3b53db3adb8e1a6a241af01d
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-08-13 16:29:11 -0300

    realizando exercicios 2 e 3

---
+++ script.js
@@ -0,0 +1,12 @@
+function coresDasPaletas() {
+    let cor1 = document.getElementById('cor1');
+    let cor2 = document.getElementById('cor2');
+    let cor3 = document.getElementById('cor3');
+    let cor4 = document.getElementById('cor4');
+
+    cor1.style.backgroundColor = 'black';
+    cor2.style.backgroundColor = '#5900F5';
+    cor3.style.backgroundColor = '#00E9F0';
+    cor4.style.backgroundColor = '#FAC114';
+}
+coresDasPaletas();
