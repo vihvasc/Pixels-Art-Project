d94f80ee3879dffb1b9da1afa1569ca0412c2fd4
Rafael Colombo <rcolombodev@gmail.com>
2021-08-13 15:57:15 -0300

    Adiciona Requisito 3

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ window.onload = function pixelsArt() {
     const secondPallete = document.getElementsByClassName('color')[1];
     const thirdPallete = document.getElementsByClassName('color')[2];
     const fourthPallete = document.getElementsByClassName('color')[3];
-    firstPallete.style.backgroundColor = '#003F63';
-    secondPallete.style.backgroundColor = '#353D40';
+    firstPallete.style.backgroundColor = 'black';
+    secondPallete.style.backgroundColor = '#003F63';
     thirdPallete.style.backgroundColor = '#F2B138';
     fourthPallete.style.backgroundColor = '#A1A5A6';
   }
