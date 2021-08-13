2173e874f6e66a5513ee903a88d944533c883677
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-13 17:04:00 -0300

    Adiciona requisito obrigatorio 7

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let firstColor = document.getElementById('color-palette').children[0];
+let secondColor = document.getElementById('color-palette').children[1];
+let thirdColor = document.getElementById('color-palette').children[2];
+let fourthColor = document.getElementById('color-palette').children[3];
+
+let arrayColors = [firstColor, secondColor, thirdColor, fourthColor];
 
 function handleBlackFirst(element) {
   element.style.background = 'black';
 }
 handleBlackFirst(firstColor);
+
+function handleSelectColor(event) {
+  let elementSelectorClass = document.querySelector('.selected');
+  elementSelectorClass.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+for (color of arrayColors) {
+  color.addEventListener('click', handleSelectColor);
+}
