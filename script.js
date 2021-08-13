bd0ef76c128301347df996d179f1b892257970c7
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-13 17:53:49 -0300

    Add exercise 02 e 03

--- script.js
+++ script.js
@@ -8,7 +8,30 @@ function createTitle () {
 }
 createTitle();
 
+// Requsito 02 e 03
+function createPalette () {
+  let createList = document.createElement( 'div' );
+  createList.id = 'color-palette';
+  document.querySelector( 'body' ).appendChild( createList );
+}
 
+let bgColors = ['#000000', '#ffb3ff', '#b3ffcc', '#ccb3ff'];
+
+function addColor () {
+  for (let index = 0; index < bgColors.length; index += 1) {
+    let colorPallete = document.createElement( 'div' );
+    colorPallete.className = 'color';
+    colorPallete.style.border = '1px solid black';
+    colorPallete.style.display = 'inline-block';
+    colorPallete.style.margin = '10px';
+    colorPallete.style.width = '40px';
+    colorPallete.style.height = '40px';
+    colorPallete.style.backgroundColor = bgColors[index];
+    document.querySelector( '#color-palette' ).appendChild( colorPallete );
+  }
+}
+createPalette();
+addColor();
 
 
 
