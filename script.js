6c2d60f7645a6f6090d822a281693043825c4ddd
aaguero96 <andre.luiz_1996@hotmail.com>
2021-08-13 17:00:25 -0300

    Finish part 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Constantes
 const pixelBoard = document.getElementById('pixel-board');
 const colors = document.getElementsByClassName('color');
+const pixels = document.getElementsByClassName('pixel');
 
 // Function to create a pixel board m x n
 function createPixelBoard(m, n) {
@@ -21,7 +22,7 @@ function classSelected(eventoDeOrigem) {
   for (let i = 0; i < colors.length; i += 1) {
     colors[i].className = 'color';
   }
-  let selectedColor = eventoDeOrigem.target;
+  const selectedColor = eventoDeOrigem.target;
   selectedColor.className = 'color selected';
 }
 
@@ -30,7 +31,26 @@ for (let i = 0; i < colors.length; i += 1) {
   colors[i].addEventListener('click', classSelected);
 }
 
+// Function to give a class pixel the color of a class selected
+function paintPixel(eventoDeOrigem) {
+  let element = eventoDeOrigem.target;
+  let color = document.getElementsByClassName('selected')[0].style.backgroundColor;
+  element.style.background = color;
+}
+
 // function used to start the page
 window.onload = function startPage() {
+  // colors of palete
+  colors[0].style.background = 'black';
+  colors[1].style.background = 'red';
+  colors[2].style.background = 'green';
+  colors[3].style.background = 'blue';
+
   createPixelBoard(5, 5);
+
+  // Add event Listener to class pixel
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', paintPixel);
+  }
+
 };
