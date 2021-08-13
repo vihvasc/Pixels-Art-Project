ff8ecfc28eaba5ff32895471be7eca1025509f39
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-13 16:34:15 -0300

    Adiciona requisito obrigatorio 4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let firstColor = document.getElementById('color-palette').children[0];
+let pixelBoard = document.getElementById('pixel-board');
 
 function handleBlackFirst(element) {
   element.style.background = 'black';
