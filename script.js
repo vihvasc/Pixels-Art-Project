8008e52c71128b8fde4620581fe9202e73b6e031
Ronald_D_Bezerra <rebulgame@gmail.com>
2021-08-13 15:46:23 -0300

    requisito02

---
+++ script.js
@@ -0,0 +1,12 @@
+let cor1 = document.getElementsByClassName("color")[0]
+cor1.style.backgroundColor = "green"
+
+let cor2 = document.getElementsByClassName("color")[1]
+cor2.style.backgroundColor = "red"
+
+let cor3 = document.getElementsByClassName("color")[2]
+cor3.style.backgroundColor = "blue"
+
+let cor4 = document.getElementsByClassName("color")[3]
+cor4.style.backgroundColor = "purple"
+
