87e78eb8aac3d8469d83a6a11a514dda64e247a5
Marina Marques <marina.marqueso@outlook.com>
2021-08-13 19:01:48 -0300

    Trabalhando no requisito 2

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function titlePage() {
 }
 titlePage();
 
+function colorPalette() {
+    let paleta = ['green', 'yellow', 'purple', 'orange'];
+    let divTag = document.createElement('div');
+    divTag.id = 'color-palette';
+    document.body.appendChild(divTag);
+
+    for (let i = 0; i < paleta.length; i ++) {
+        let paletteTag = document.createElement('div');
+        paletteTag.className = 'color';
+        document.getElementById('color-palette').appendChild(paletteTag);
+    }
+    return paletteTag;
+}
+colorPalette();
+
