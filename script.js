871b92a2f407c96ad5b1a66f7d653e6c0d520432
Deivid <deividmborges@gmail.com>
2021-08-13 16:14:39 -0300

    adiciona requisito 2 com CSS e dinamicmente

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 window.onload = pixelArts;
 
 function pixelArts() {
-    pageTitle();
+  pageTitle();
+  createColorPalette();
+  createColors();
 }
-
+// requisito 1
 function pageTitle() {
-    titleH1 = document.createElement('h1');
-    titleH1.id = 'title';
-    titleH1.innerText = 'Paleta de Cores';
-    document.body.appendChild(titleH1);
+  let titleH1 = document.createElement('h1');
+  titleH1.id = 'title';
+  titleH1.innerText = 'Paleta de Cores';
+  document.body.appendChild(titleH1);
+}
+// requisito 2
+function createColorPalette() {
+  let colorPalette = document.createElement('ul');
+  colorPalette.id = 'color-palette';
+  document.body.appendChild(colorPalette);
+}
+function createColors() {
+  let colors = ['green', 'orange', 'brown', 'red']
+  for(color of colors) {
+  let liColors = document.createElement('li');
+  let ulColorPalette = document.getElementById('color-palette');
+  ulColorPalette.appendChild(liColors);
+  liColors.className = 'color';
+  liColors.style.backgroundColor = color;
+  }
 }
