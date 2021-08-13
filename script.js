34adde0e0315bc6182f7861bf210739d7a093576
claudiocassimiro <days_surfing@live.com>
2021-08-13 15:52:29 -0300

    Requisito 3 completo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function paletteColors() {
     let paletteColors = document.getElementById('color-palette');
     let contentColor = document.getElementsByClassName('color');
-    let colors = ['red', 'yellow', 'green', 'black'];
+    let colors = ['black', 'yellow', 'green', 'red'];
 
     for (let index = 0; index < contentColor.length; index += 1) {
         contentColor[index].style.backgroundColor = colors[index];
