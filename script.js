e4b33974273518312e4f4dba66bda6ba7cb346b9
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-13 15:59:19 -0300

    Adiciona requisito obrigatorio 3

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstColor = document.getElementById('color-palette').children[0];
+
+function handleBlackFirst(element) {
+  element.style.background = 'black';
+}
+handleBlackFirst(firstColor);
