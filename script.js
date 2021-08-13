3d3ab7a15f2690d420b073daa17a954533905551
Paulo Flóra <tflora.2004@gmail.com>
2021-08-13 17:25:51 -0300

    Feito o requisito 2.

---
+++ script.js
@@ -0,0 +1,17 @@
+const colorPallet = document.createElement('div');
+colorPallet.id = 'color-palette';
+document.body.appendChild(colorPallet);
+const numberOfColors = 4;
+
+for (let i = 0; i < numberOfColors; i += 1) {
+  const newDiv = document.createElement('div');
+  newDiv.className = 'color';
+  colorPallet.appendChild(newDiv);
+}
+
+const colorBox = document.getElementsByClassName('color');
+const colorSample = ['green', 'red', 'blue', 'purple'];
+
+for (let i = 0; i < colorBox.length; i += 1) {
+  colorBox[i].style.background = colorSample[i];
+}
