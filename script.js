0c46d0f76a3a6753f95fa7273b2bfe79dd70b24c
Gabrielle-Murat <belle.murat@gmail.com>
2021-08-13 16:31:19 -0300

    Adiciona os requisitos 1 e 2

---
+++ script.js
@@ -0,0 +1,5 @@
+const colorsList = document.querySelectorAll('.color');
+colorsList[0].style.background = 'pink';
+colorsList[1].style.background = 'lightblue';
+colorsList[2].style.background = 'lightgreen';
+colorsList[3].style.background = 'orange';
