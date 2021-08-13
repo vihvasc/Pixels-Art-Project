384369749166b6373d95d284be8db7c7153e3bb3
Jesse Benevides <jessebenevides@gmail.com>
2021-08-13 19:10:40 -0300

    Adiciona requisito 4 e 5

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = function () {
   let colorList = ['black', 'gray', 'blue', 'green'];
+  let pixelBoardSize = 5;
   let colorPallete = document.getElementById('color-palette');
   let pixelBoard = document.getElementById('pixel-board');
 
   createColorPallete(colorList);
+  createPixelBoard(pixelBoardSize);
 
   //cria a paleta com as cores presentes em colorList
   function createColorPallete(colorList) {
@@ -20,11 +22,25 @@ window.onload = function () {
 
   //CRIANDO O PIXEL BOARD
 
-  //Cria os pixels do pixel board
-  // function createPixel() {
-  //   let pixel = createElement('div');
+  //Cria o pixel board de acordo com a quantidade de linhas e colunas passadas por parâmetro
+  function createPixelBoard(boardSize) {
+    let lines = createLines(boardSize);
 
-  // }
+    for (let line of lines) {
+      for (let i = 0; i < boardSize; i += 1) {
+        let pixel = createPixel();
+        line.appendChild(pixel);
+      }
+    }
+  }
+
+  //Cria um pixel para o pixel board
+  function createPixel() {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    return pixel;
+  }
+
   //Cria as linhas do pixel board
   function createLines(numberOfLines) {
     for (let i = 0; i < numberOfLines; i += 1) {
@@ -32,5 +48,7 @@ window.onload = function () {
       line.classList.add('line');
       pixelBoard.appendChild(line);
     }
+
+    return document.getElementsByClassName('line');
   }
 };
