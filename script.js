29f091914d0bff6ec24725466ddd989f3a05d441
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-13 16:15:50 -0300

    Requisto 6 completo

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function colorPalette() {
   const paletaCores = document.querySelectorAll('.color');
   const cores = ['black', 'purple', 'darkblue', 'darkred'];
   for (let key = 0; key < paletaCores.length; key += 1) {
+    if (cores[key] === 'black') {
+      paletaCores[key].classList.add('selected');
+    }
     paletaCores[key].classList.add(cores[key]);
   }
 }
