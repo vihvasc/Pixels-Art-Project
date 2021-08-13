09b9b8598587d357bc889b94cfae5ab0ec149e17
Flávio Bianchetti <fjb.developer@gmail.com>
2021-08-13 17:00:40 -0300

    atendido requisito 7 - validado via npm

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function startPage() {
 }
 
 startPage();
+
+function selectPalette() {
+  let parent = document.getElementById('color-palette');
+  
+  parent.addEventListener('click', function(event) {
+    if (event.target.className === 'color') {
+      let selectedColor = document.querySelector('.selected');
+      selectedColor.classList.remove('selected');
+      event.target.classList.add('selected');
+    }    
+  });
+}
+
+selectPalette();
