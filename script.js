e373f975e964289e46da4b86cd235afdd478ae80
Gabrielle-Murat <belle.murat@gmail.com>
2021-08-13 17:15:43 -0300

    Adiciona os requisitos 3, 4 e 5

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const colorsList = document.querySelectorAll('.color');
-colorsList[0].style.background = 'pink';
+colorsList[0].style.background = 'black';
 colorsList[1].style.background = 'lightblue';
 colorsList[2].style.background = 'lightgreen';
-colorsList[3].style.background = 'orange';
+colorsList[3].style.background = 'pink';
+
+function createLine(width) {
+  const pixelBoard = document.getElementById('pixel-board');
+  const ul = document.createElement('ul');
+  pixelBoard.appendChild(ul);
+  for (let index = 0; index < width; index += 1) {
+    const pixelSquare = document.createElement('li');
+    pixelSquare.className = 'pixel';
+    ul.appendChild(pixelSquare);
+  }
+}
+
+function createBoard(height) {
+  for (let index = 0; index < height; index += 1) {
+    createLine(5);
+  }
+}
+
+createBoard(5);
