34939514dae0597260a26e2037e5666f9dbec39b
Danilo <dcalado.fisio@yahoo.com.br>
2021-08-13 16:56:36 -0300

    adiciona as 4 caixas da paleta de cores a primeiro sendo preta req 2 e 3

---
+++ script.js
@@ -0,0 +1,12 @@
+let divcolorPallete = document.getElementsByClassName('color selected')[0].style.backgroundColor ="black";
+ divcolorPallete = document.getElementsByClassName('color')[1].style.backgroundColor ="gold";
+ divcolorPallete = document.getElementsByClassName('color')[2].style.backgroundColor ="darkred";
+ divcolorPallete = document.getElementsByClassName('color')[3].style.backgroundColor ="blue";
+
+
+ window.onload = function(){
+
+    let paleta = document.querySelectorAll('.color');
+    let pixel = document.querySelectorAll('.pixel');
+    let corselecionada = paleta[0].style.backgroundColor
+ }
