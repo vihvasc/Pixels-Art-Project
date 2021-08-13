6d906e4f82fc6996342c2c21dc6163e4144b3b79
Mauricio Lopes <mlopeesz@gmail.com>
2021-08-13 17:35:25 -0300

    Realizado requisito 5

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Cria quadro de pixels
-function createPixels() {
-  const pixelBoard = document.getElementById('pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 
-  for (let index = 0; index < 25; index += 1) {
-    const pixelDiv = document.createElement('div');
-    pixelDiv.className = 'pixel';
-    pixelBoard.appendChild(pixelDiv);
+// Cria quadro de pixels
+function createPixels(value) {
+  for (let indexLines = 0; indexLines < value; indexLines += 1) {
+    const lines = document.createElement('div');
+    lines.className = 'lines';
+    pixelBoard.appendChild(lines);
+    for (let indexColumns = 0; indexColumns < value; indexColumns += 1) {
+      const pixels = document.createElement('div');
+      pixels.className = 'pixel';
+      lines.appendChild(pixels);
+    }
   }
 }
-createPixels();
+createPixels(5);
