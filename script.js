6b72be24001c2413781f6b616a73b0b1b954c19c
Marco <marquinco@outlook.com>
2021-08-13 16:34:53 -0300

    conserta requisito 4 e faz o 5

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+  let primeiro = document.getElementsByClassName("color")[0];
+  let segundo = document.getElementsByClassName("color")[1];
+  let terceiro = document.getElementsByClassName("color")[2];
+  let quarto = document.getElementsByClassName("color")[3];
+  primeiro.style.backgroundColor = '#000';
+	primeiro.addEventListener("click",selecionaCor)
+  segundo.style.backgroundColor = '#6D597A';
+	segundo.addEventListener("click",selecionaCor)
+  terceiro.style.backgroundColor = '#67697C';
+	terceiro.addEventListener("click",selecionaCor)
+  quarto.style.backgroundColor = '#C1C3C1';
+	quarto.addEventListener("click",selecionaCor)
+	
+	let pixels = document.getElementsByClassName("pixel")
+	for (pixel of pixels) {
+		pixel.addEventListener("click", adicionaCor)
+		pixel.style.backgroundColor = 'white'
+	}
+}
+function adicionaCor(evento) {
+	let cor = sessionStorage.getItem("cor")
+	let pixel = evento.target
+	pixel.style.backgroundColor = cor
+}
+function selecionaCor(evento) {
+  let cor = evento.target.style.backgroundColor
+	sessionStorage.setItem("cor",cor)
+}
