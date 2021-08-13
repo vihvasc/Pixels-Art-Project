9d486bf06ecf73c7a4dc570ada5ea7f81417ae41
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-08-13 16:43:26 -0300

    requisitos 4 e 5 finalizados

--- script.js
+++ script.js
@@ -3,25 +3,37 @@
 window.onload = function () {
   let colorsList = ["black", "red", "green", "blue"];
   startPallet(colorsList);
+  selectFirstColor();
+  startPixelBox();
 };
 
 // Color Pallet
 
 function startPallet(colorsList) {
+  createPalletDivs(colorsList.length)
   setColors(colorsList);
   setPalletEventListener();
 }
 
+function createPalletDivs(lenght) {
+  let palletBox = document.getElementById("color-palette");
+  for (let index = 0; index < lenght; index += 1) {
+    let palletDiv = document.createElement("div");
+    palletDiv.className = "color";
+    palletBox.appendChild(palletDiv);
+  };
+};
+
 function setColors (colorsList) {
-  let baseDivs = document.getElementsByClassName("color");
-  for (let index = 0; index < baseDivs.length; index += 1) {
-    baseDivs[index].style.backgroundColor = colorsList[index];
+  let palletDivs = document.getElementsByClassName("color");
+  for (let index = 0; index < palletDivs.length; index += 1) {
+    palletDivs[index].style.backgroundColor = colorsList[index];
   };
 };
 
 function setPalletEventListener() {
-  let baseDivs = document.getElementsByClassName("color");
-  for (div of baseDivs) {
+  let palletDivs = document.getElementsByClassName("color");
+  for (div of palletDivs) {
     div.addEventListener("click", selectColor);
   };
 };
@@ -29,14 +41,25 @@ function setPalletEventListener() {
 function selectColor(event) {
   let selectedDiv = event.target;
   let color = selectedDiv.style.backgroundColor;
-  sessionStorage.setItem("selectedColor", color);
-  removeFocous();
-  setFocous(selectedDiv);
+  let sessionStorageColor = sessionStorage.getItem("selectedColor");
+  if (sessionStorageColor !== color) {
+    sessionStorage.setItem("selectedColor", color);
+  } else {
+    sessionStorage.removeItem("selectedColor");
+  };
+  
+  // if (selectedDiv.style.transform === "scale(1.3, 1.3)") {
+  //   removeFocous();
+  //   sessionStorage.removeItem("selectedColor");
+  // } else {
+  //   removeFocous();
+  //   setFocous(selectedDiv);
+  // };
 };
 
 function removeFocous() {
-  let baseDivs = document.getElementsByClassName("color");
-  for (div of baseDivs) {
+  let palletDivs = document.getElementsByClassName("color");
+  for (div of palletDivs) {
     if (div.style.transform === "scale(1.3, 1.3)") {
       div.style.transform = ""
     };
@@ -47,4 +70,21 @@ function setFocous(selectedDiv) {
   selectedDiv.style.transform = "scale(1.3, 1.3)"
 };
 
-// Pixel Art
+function selectFirstColor() {
+  firstPalletColorDiv = document.querySelector(".color");
+  firstPalletColor = firstPalletColorDiv.style.backgroundColor;
+  sessionStorage.setItem("selectedColor", firstPalletColor);
+  // setFocous(firstPalletColorDiv);
+};
+
+// Pixel box
+
+function startPixelBox() {
+  pixelBoard = document.getElementById("pixel-board")
+  for (let index = 0; index < 25; index += 1) {
+    let pixel = document.createElement("div");
+    pixel.className = "pixel";
+    pixel.style.backgroundColor = "white";
+    pixelBoard.appendChild(pixel);
+  };
+};
