b9d1b67501cdf42aa9fc042c886f67a18a145902
ArthurMoreira <arthurmcs96@gmail.com>
2021-08-13 20:35:26 -0300

    Adiciona o requisito 4

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-/* window.onload = function load() {
-  const sectionOne = document.createElement('section');
-  sectionOne.id = "color-palette";
-  for (let i = 0; i < 4; i += 1) {
-    const divColors = document.createElement('button');
-    divColors.className = 'color';
+function squareOfPixels() {
+  for (let i = 0; i < 5; i += 1) {
+    const line = document.createElement('div');
+    line.id = 'pixel-board';
+    document.body.appendChild(line);
+    console.log(line);
+
+    for (let index = 0; index < 5; index += 1) {
+      const column = document.createElement('div');
+      column.className = 'pixel';
+      column.style.backgroundColor = 'white';
+      line.appendChild(column);
+      console.log(column);
+    }
   }
-};
- */
+}
+squareOfPixels();
