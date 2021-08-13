0001e7136722fee0ccbff6990973edade1230d6b
fernandamichetti <f.michetti@hotmail.com>
2021-08-13 18:05:17 -0300

    Realizando o requisito 7

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ window.onload = function () {
   let black = document.getElementById('dark-black');
   black.className = 'color' + ' selected';
 };
+
+let pickingDarkPink = document.getElementById('dark-pink');
+let pickingBrightPink = document.getElementById('bright-pink');
+let pickingDarkBlue = document.getElementById('dark-blue');
+let pickingDarkBlack = document.getElementById('dark-black');
+
+pickingDarkPink.addEventListener('click', darkPink);
+
+function darkPink() {
+  pickingDarkPink.className = 'color' + ' selected';
+  pickingBrightPink.className = 'color';
+  pickingDarkBlue.className = 'color';
+  pickingDarkBlack.className = 'color';
+}
+
+pickingBrightPink.addEventListener('click', brightPink);
+
+function brightPink() {
+  pickingBrightPink.className = 'color' + ' selected';
+  pickingDarkPink.className = 'color';
+  pickingDarkBlue.className = 'color';
+  pickingDarkBlack.className = 'color';
+}
+
+pickingDarkBlue.addEventListener('click', darkBlue);
+
+function darkBlue() {
+  pickingDarkBlue.className = 'color' + ' selected';
+  pickingBrightPink.className = 'color';
+  pickingDarkPink.className = 'color';
+  pickingDarkBlack.className = 'color';
+}
