f5e696ccf7662102898c4a639169adb773226cae
Arthur Teixeira <tuzinf5@gmail.com>
2021-08-13 15:22:26 -0300

    Requisito 1 concluído

---
+++ script.js
@@ -0,0 +1,8 @@
+function createH1() {
+  const tagH1 = document.createElement('h1');
+  tagH1.id = 'title';
+  tagH1.innerText = 'Paleta de Cores';
+  document.body.appendChild(tagH1);
+}
+
+createH1();
