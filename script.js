bc8d0d5fd0664ec5fbb33eefc8c4726e40bb0c3a
Jesse Benevides <jessebenevides@gmail.com>
2021-08-13 18:18:04 -0300

    Requisito 4 - cria as linhas do pixel board

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 window.onload = function () {
   let colorList = ['black', 'gray', 'blue', 'green'];
   let colorPallete = document.getElementById('color-palette');
+  let pixelBoard = document.getElementById('pixel-board');
 
+  createColorPallete(colorList);
+
+  //cria a paleta com as cores presentes em colorList
   function createColorPallete(colorList) {
     for (let color of colorList) {
       let newColor = document.createElement('div');
+
       newColor.classList.add('color');
       newColor.classList.add(color);
       newColor.style.backgroundColor = color;
+
       colorPallete.appendChild(newColor);
     }
   }
 
-  createColorPallete(colorList);
+  //CRIANDO O PIXEL BOARD
+
+  //Cria os pixels do pixel board
+  // function createPixel() {
+  //   let pixel = createElement('div');
+
+  // }
+  //Cria as linhas do pixel board
+  function createLines(numberOfLines) {
+    for (let i = 0; i < numberOfLines; i += 1) {
+      let line = document.createElement('div');
+      line.classList.add('line');
+      pixelBoard.appendChild(line);
+    }
+  }
 };
