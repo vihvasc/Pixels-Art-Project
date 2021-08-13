44a75f81deda34af3f25d10b9e3d7a4f9ea52e0f
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-13 16:11:55 -0300

    Requisito 5 completo

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ colorPalette();
 
 function createPixel(value) {
   const pixelBorder = document.getElementById('pixel-board');
-  for (let key = 0; key < value; key += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    pixelBorder.appendChild(pixel);
+  const linha = value / 5;
+  for (let key = 0; key < linha; key += 1) {
+    for (let key1 = 0; key1 < 5; key1 += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixelBorder.appendChild(pixel);
+    }
+    const br = document.createElement('br');
+    pixelBorder.appendChild(br);
   }
 }
 createPixel(25);
