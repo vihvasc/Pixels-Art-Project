515078b343fa4dc6685c6a1b4c553e11974dc254
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-08-13 17:49:00 -0300

    requesito 7

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function colorPalette () {
   let paletteContainer = document.getElementById('color-palette');
 
   for ( let i = 0; i < 4; i += 1) {
-    let paletteColor = colors[i]
+    var paletteColor = colors[i]
     let paletteColorItem = document.createElement('li');
     paletteColorItem.style.backgroundColor = paletteColor;
     paletteColorItem.className = 'color'
@@ -34,9 +34,28 @@ function board () {
 
 board ();
 
-function selected () {
+function select () {
   let selectColorBlack=document.querySelector('.color');
   selectColorBlack.classList.add('selected')
 }
 
-selected();
+select();
+
+function selectedColor () {
+
+    let selectedColor = document.getElementById('color-palette')
+    let newColor = document.getElementsByClassName('color');
+      
+    selectedColor.addEventListener('click', newColors)
+  //  acessado: https://github.com/tryber/sd-08-project-pixels-art/blob/fabio-ferreira-pixels-art-project/script.js
+    function removeSelected() {
+        for (let i = 0; i < newColor.length; i += 1) {
+          newColor[i].className = 'color';
+        }
+      }
+
+      function newColors (event) {
+        removeSelected ();
+        event.target.className = 'color selected';
+      }
+    }
