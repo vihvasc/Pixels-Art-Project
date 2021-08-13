8b43c532e1cac7e1fe7baa857cc0a4aa4b449e4f
Edson Lima <edsoneduardo526@gmail.com>
2021-08-13 18:27:11 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,30 @@
+let linha1 = window.document.getElementById("linha1")
+for (let index = 0; index < 6; index += 1){
+let div = document.createElement("div");
+div.className = "pixel";
+linha1.appendChild(div);
+}
+let linha2 = window.document.getElementById("linha2")
+for (let index = 0; index < 6; index += 1){
+let div = document.createElement("div");
+div.className = "pixel";
+linha2.appendChild(div);
+}
+let linha3 = window.document.getElementById("linha3")
+for (let index = 0; index < 6; index += 1){
+let div = document.createElement("div");
+div.className = "pixel";
+linha3.appendChild(div);
+}
+let linha4 = window.document.getElementById("linha4")
+for (let index = 0; index < 6; index += 1){
+let div = document.createElement("div");
+div.className = "pixel";
+linha4.appendChild(div);
+}
+let linha5 = window.document.getElementById("linha5")
+for (let index = 0; index < 6; index += 1){
+let div = document.createElement("div");
+div.className = "pixel";
+linha5.appendChild(div);
+}
