fdc7d3927028499f7dfb6a5ff2aa72fa82bd13ec
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-08-13 18:37:51 -0300

    Adiciona requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let idPaletaDeCores = document.querySelector('#color-palette');
-let classColors = document.querySelectorAll('.color');
-console.log(idPaletaDeCores);
-
-function coresPaleta() {
-
+function createBoard() {
+  let linhas = 5;
+  let colunas = 5;
+  for (let criaLinhas = 0; criaLinhas < linhas; criaLinhas += 1) {
+    let line = document.querySelectorAll('.line-pixel')[criaLinhas];
+    for (let criaColunas = 0; criaColunas < colunas; criaColunas += 1) {
+      let divBox = document.createElement('div');
+      divBox.className = 'pixel';
+      line.appendChild(divBox);
+    }
+  }
 }
-coresPaleta()
+createBoard();
