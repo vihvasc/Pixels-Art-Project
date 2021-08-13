9113e71836d3a0f48e84c9cc2936efd1833f2cca
Vitor <vitoremerich.contato@gmail.com>
2021-08-13 16:57:26 -0300

    Requisito 6

---
+++ script.js
@@ -0,0 +1,10 @@
+let black = document.getElementById("black");
+let blue = document.getElementById("blue");
+let red = document.getElementById("red");
+let purple = document.getElementById("purple");
+
+function blackSelected () {
+  black.classList = "color selected";
+}
+
+blackSelected();
