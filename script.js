8b39fb7cee54143aeb022abfeba60931e9bf2917
anniehau <anniehaurani@gmail.com>
2021-08-13 16:16:31 -0300

    Exercicio 7 completo.

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ window.onload = function() {
 // Variaveis
 const boardHeight = 5;
 const boardWidth = 5;
+const black = document.querySelector('.color.black');
+const red = document.querySelector('.color.red');
+const green = document.querySelector('.color.green');
+const blue = document.querySelector('.color.blue');
+
+// Event Listeners
+black.addEventListener('click', handleSelection);
+red.addEventListener('click', handleSelection);
+green.addEventListener('click', handleSelection);
+blue.addEventListener('click', handleSelection);
 
 // Funções
+makePixelBoard();
+
 function makePixelBoard() {
   for (let index = 1; index <= boardHeight; index += 1) {
     let line = document.createElement('div');
@@ -14,12 +26,25 @@ function makePixelBoard() {
     for (let index2 = 1; index2 <= boardWidth; index2 += 1) {
       let pixel = document.createElement('div');
       pixel.classList.add('pixel');
-      
+
       document.querySelectorAll('.board-line')[index-1].appendChild(pixel);
     }
   }
 }
 
-makePixelBoard();
+function handleSelection(event) {
+  let currentColor = event.target.classList;
+  let previousColor = document.querySelector('.selected');
+
+  if (!currentColor.contains('selected')) {
+    currentColor.add('selected');
+    previousColor.classList.remove('selected');
+
+  } else if (currentColor.contains('selected')) {
+    currentColor.remove('selected');
+  }
+
+  console.log(document.querySelectorAll('.selected'))
+}
 
 }
