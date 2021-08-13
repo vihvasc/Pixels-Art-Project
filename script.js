969e0b96fbc253503aba3e2b9999e50228cc5bfe
Rafael Colombo <rcolombodev@gmail.com>
2021-08-13 19:09:11 -0300

    Adiciona Requisito 4

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-window.onload = function pixelsArt() {
-  function colorPalette() {
-    const firstPallete = document.getElementsByClassName('color')[0];
-    const secondPallete = document.getElementsByClassName('color')[1];
-    const thirdPallete = document.getElementsByClassName('color')[2];
-    const fourthPallete = document.getElementsByClassName('color')[3];
-    firstPallete.style.backgroundColor = 'black';
-    secondPallete.style.backgroundColor = '#003F63';
-    thirdPallete.style.backgroundColor = '#F2B138';
-    fourthPallete.style.backgroundColor = '#A1A5A6';
-  }
+function colorPalette() {
+  const firstPallete = document.getElementsByClassName('color')[0];
+  const secondPallete = document.getElementsByClassName('color')[1];
+  const thirdPallete = document.getElementsByClassName('color')[2];
+  const fourthPallete = document.getElementsByClassName('color')[3];
+  firstPallete.style.backgroundColor = 'black';
+  secondPallete.style.backgroundColor = '#003F63';
+  thirdPallete.style.backgroundColor = '#F2B138';
+  fourthPallete.style.backgroundColor = '#A1A5A6';
+}
 
+function pixelsArt() {
   colorPalette();
-};
+}
+
+window.onload = pixelsArt;
