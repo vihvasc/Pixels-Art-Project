9eff23c0c6fb0eaa25833fce8cf6271fb4b642fd
Ronald_D_Bezerra <rebulgame@gmail.com>
2021-08-13 18:20:27 -0300

    requisito 5 e tira as bordas

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+window.onload = corbranca;
+
+function corbranca (){
+let pixels = document.getElementsByClassName("pixel")
+for(i = 0; i < pixels.length; i +=1) {
+    pixels[i].style.backgroundColor = "white"
+}
+}
+
 let cor1 = document.getElementsByClassName("color")[0]
-cor1.style.backgroundColor = "black"
+cor1.style.backgroundColor = "black";
 
 let cor2 = document.getElementsByClassName("color")[1]
-cor2.style.backgroundColor = "red"
+cor2.style.backgroundColor = "red";
 
 let cor3 = document.getElementsByClassName("color")[2]
-cor3.style.backgroundColor = "blue"
+cor3.style.backgroundColor = "blue";
 
 let cor4 = document.getElementsByClassName("color")[3]
-cor4.style.backgroundColor = "purple"
-
+cor4.style.backgroundColor = "purple";
