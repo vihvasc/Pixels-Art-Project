be1d47af54186c5e51d98c7131b993526f07de67
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-13 17:22:01 -0300

    Adiciona função createRGBString e fillPalette

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = generateRGB();
+window.onload = function(){
+  generateRGB();
+}
 
 createBoard();
 firstColor();
@@ -19,18 +21,31 @@ function createBoard(){
 function firstColor(){
   let firstBox = document.querySelector(".color");
   firstBox.style.backgroundColor = "black";
+  firstBox.className = "selected color box";
 }
 
 //gera as cores para preencher a palheta
 function generateRGB(){
-  let secondColor = [];
+let secondColor = [];
 let thirdColor = [];
 let fourthColor = [];
-
   for(let i = 0; i < 3; i ++){
     secondColor[i] = Math.random(0, 256);
     thirdColor[i] = Math.random(0, 256);
     fourthColor[i] = Math.random(0, 256);
   }
-  console.log(secondColor);
+console.log(secondColor);
+}
+
+//cria um string com o formato de declaração rgb do CSS
+function createRGBString(color){
+  let rgbString = 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')';
+  return rgbString;
+}
+//preenche a palheta de cores com as cores aleatórias geradas
+function fillPalette(second, third, fourth){
+  let boxesPalette = document.querySelectorAll(".color");
+  for(let i = 1; i < 4; i ++){
+    boxesPalette[i].style.backgroundColor = createRGBString([255, 30, 6]);
+  }
 }
