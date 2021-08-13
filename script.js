9fdb5cda295ea313ca6835182f7fd620ab8eb5db
Alex Menezes <alexmenezes.business@outlook.com>
2021-08-13 18:37:28 -0300

    adiciona requisito 4

---
+++ script.js
@@ -0,0 +1,26 @@
+let quadroPixel = document.querySelector('#pixel-board');
+
+let linha = 5;
+
+function makeLine() {
+  for (let i = 0; i < linha; i += 1) {
+    let line = document.createElement('div');
+    line.className = 'line';
+    quadroPixel.appendChild(line);
+    makePixels(line)
+  }
+}
+
+
+makeLine();
+
+function makePixels(ondeGuarda) {
+  for (let i = 0; i < linha; i += 1) {
+    let pixel = document.createElement('div');
+    // let line = document.querySelector(".line")
+    pixel.className = 'pixel';
+
+    ondeGuarda.appendChild(pixel);
+  }
+}
+
