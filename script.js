a98e7d25cad63c233a5621e0d4e7a038441a2bd6
DanielHott <daniel_hott@outlook.com>
2021-08-13 18:52:39 -0300

    Adicionando requisito numero 7

--- script.js
+++ script.js
@@ -25,6 +25,42 @@ criaQuadrado('fileira4');
 criaQuadrado('fileira5');
 
 function selecionaPrimeira() {
-  let paletaPreta = document.getElementsByClassName('color')[0];
-  paletaPreta.classList.add('selected');
+  let corInicial = document.getElementsByClassName('color')[0];
+  corInicial.classList.add('selected');
+}
+let corPreta = document.getElementsByClassName('color')[0];
+let corVermelha = document.getElementsByClassName('color')[1];
+let corAmarela = document.getElementsByClassName('color')[2];
+let corVerde = document.getElementsByClassName('color')[3];
+
+corVermelha.addEventListener('click', selecionarVermelha);
+function selecionarVermelha() {
+  corVermelha.classList.add('selected');
+  corPreta.classList.remove('selected');
+  corAmarela.classList.remove('selected');
+  corVerde.classList.remove('selected');
+}
+
+corAmarela.addEventListener('click', selecionarAmarela);
+function selecionarAmarela() {
+  corAmarela.classList.add('selected');
+  corPreta.classList.remove('selected');
+  corVermelha.classList.remove('selected');
+  corVerde.classList.remove('selected');
+}
+
+corVerde.addEventListener('click', selecionarVerde);
+function selecionarVerde() {
+  corVerde.classList.add('selected');
+  corPreta.classList.remove('selected');
+  corVermelha.classList.remove('selected');
+  corAmarela.classList.remove('selected');
+}
+
+corPreta.addEventListener('click', selecionarPreta);
+function selecionarPreta() {
+  corPreta.classList.add('selected');
+  corAmarela.classList.remove('selected');
+  corVermelha.classList.remove('selected');
+  corVerde.classList.remove('selected');
 }
