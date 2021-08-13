3d8dbf967aaadecb9bffb7ffa0c82073021f8b8c
Paulo Flóra <tflora.2004@gmail.com>
2021-08-13 17:56:21 -0300

    Feito o requisito 4 e 5

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ const colorSample = ['black', 'red', 'green', 'purple'];
 for (let i = 0; i < colorBox.length; i += 1) {
   colorBox[i].style.background = colorSample[i];
 }
+
+const pixelBoard = document.createElement('div');
+pixelBoard.id = 'pixel-board';
+document.body.appendChild(pixelBoard);
+
+const boardSize = 5;
+
+for (let i = 0; i < boardSize; i += 1) {
+  const pixelLine = document.createElement('div');
+  pixelLine.className = 'pixel-line';
+  pixelBoard.appendChild(pixelLine);
+}
+
+const pixelLine = document.getElementsByClassName('pixel-line');
+
+for (let i = 0; i < boardSize; i += 1) {
+  for (let n = 0; n < boardSize; n += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelLine[n].appendChild(pixel);
+  }
+}
