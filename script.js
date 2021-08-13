d42ab6ffcbaf707504ab3e45837e9667f2e07f79
leandro oliveira <leandrooliveira35@gmail.com>
2021-08-13 16:56:57 -0300

    requisito 9 completo

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const secondColor = document.getElementById('second-color');
 const thirdColor = document.getElementById('third-color');
 const fourthColor = document.getElementById('fourth-color');
 const pixelBoard = document.getElementById('pixel-board');
+const allPixel = document.getElementsByClassName('pixel');
+const clearButton = document.getElementById('clear-board');
 
 firstColor.style.backgroundColor = 'black';
 secondColor.style.backgroundColor = 'green';
@@ -14,6 +16,7 @@ secondColor.addEventListener('click', addSelectedClass);
 thirdColor.addEventListener('click', addSelectedClass);
 fourthColor.addEventListener('click', addSelectedClass);
 pixelBoard.addEventListener('click', changeColor);
+clearButton.addEventListener('click', clearColor);
 
 function addSelectedClass(event) {
   const checkSelected = document.querySelector('.selected');
@@ -24,4 +27,10 @@ function addSelectedClass(event) {
 function changeColor(event) {
   const ColorSelected = document.querySelector('.selected').style.backgroundColor;
   event.target.style.backgroundColor = ColorSelected;
+}
+
+function clearColor() {
+  for (let index = 0; index < allPixel.length; index += 1) {
+    allPixel[index].style.backgroundColor = 'white';
+  }
 }
