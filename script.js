b9f5126316b556d628397f4aaf72cd76d667d40b
David Souza <dabcs@poli.ufrj.br>
2021-08-13 14:14:25 -0300

    implementacao da coloracao do quadro de pixel e alteracao do ponteiro no css

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 window.onload = function (){
     createPixel(25);
+    selectColor();
+    document.addEventListener("click", selectColor);
+    document.addEventListener('click', colorPixel);
+
 }
 
 function createPixel(nPixel){
@@ -8,6 +12,23 @@ function createPixel(nPixel){
         let divPixel = document.createElement('div');
         divPixel.className = 'pixel';
         document.querySelector('#pixel-board').appendChild(divPixel);
-        console.log('teste');
     }
+}
+
+function selectColor(event){
+  let  sClass = document.getElementsByClassName('selected');
+  if (sClass.length === 0){
+      document.querySelector('#color-palette').firstElementChild.className += ' selected';
+  } else if(event.target.className === 'color'){
+      document.querySelector('.selected').className = 'color';
+      event.target.className += ' selected';
+  }
+}
+
+function colorPixel(event){
+    let selected = document.querySelector('.selected');
+    if(event.target.className === 'pixel'){
+        event.target.style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
+    }
+
 }
