144a427592cf07456f45903570eae3cc9ab7196f
Joao Marco <joaomarco.matta@gmail.com>
2021-08-13 19:25:15 -0300

    Implementa requisito 7.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // Criar função que aumenta o tamanho do quadrado da paleta de cores quando selecionada (utilizar a classe .selected-color do CSS).
 
 // Inicia Scripts:
+const getColorPalette = document.getElementsByClassName('color');
 
+function handleColorPaletteSelection(event) {
+  const targetEvent = event;
+  for (let i = 0; i < getColorPalette.length; i += 1) {
+    getColorPalette[i].classList.remove('selected');
+  }
+  targetEvent.target.classList.add('selected');
+}
+
+for (let i = 0; i < getColorPalette.length; i += 1) {
+  getColorPalette[i].addEventListener('click', handleColorPaletteSelection);
+}
