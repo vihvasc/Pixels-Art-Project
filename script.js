7461bc14c890473a0012d126c6085ef454b44b67
jpcp0614 <jpcp0614@gmail.com>
2021-08-13 19:10:35 -0300

    fix: created function for seletc pixel

--- script.js
+++ script.js
@@ -14,4 +14,13 @@ darkBlueColor.addEventListener('click', changeSelected);
 lightBlueColor.addEventListener('click', changeSelected);
 lightGreenColor.addEventListener('click', changeSelected);
 
-// 8.
+// 8. Created function for select pixel
+function selectPixelColor(event) {
+  const selector = document.querySelector('.selected');
+  const selectColor = window.getComputedStyle(selector).getPropertyValue('background-color');
+  event.target.style.backgroundColor = selectColor.id;
+}
+document.getElementById('pixel-board').addEventListener('click', selectPixelColor);
+
+// ref.1: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+// ref.2: https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/getPropertyValue
