6dcecfe06756c76bb9bb48bf70f88727604a910b
claudiocassimiro <days_surfing@live.com>
2021-08-13 17:01:05 -0300

    Requisito 6 completo

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 function paletteColors() {
     let paletteColors = document.getElementById('color-palette');
     let contentColor = document.getElementsByClassName('color');
+    contentColor[0].classList.add('selected');
     let colors = ['black', 'yellow', 'green', 'red'];
 
     for (let index = 0; index < contentColor.length; index += 1) {
         contentColor[index].style.backgroundColor = colors[index];
+        contentColor[index].addEventListener('click', changeClass);
+    }
+
+    function changeClass(event) {
+        if (event.target.classList.contains('selected')) {
+            event.target.classList.remove('selected');
+        }
+        else {
+            event.target.classList.add('selected')
+        }
     }
 }
 
 paletteColors();
+
 
 function pixelBoard() {
     let pixelBoard = document.getElementById('pixel-board');
