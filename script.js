f4edb5b99f4023b82ebe3456744dff0f2cd1c48e
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2021-08-13 16:15:00 -0300

    [Desafio 3] Faz a primeira cor da paleta ser a cor preta

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
+//Desafio 2
 function fourDistinctColorPalette (quantCores) {
-	let div = document.querySelector('#color-palette');
+  let div = document.querySelector('#color-palette');
 
-	for (let i = 1; i <= quantCores; i += 1) {
-		let palette = document.createElement('div');
+  for (let i = 1; i <= quantCores; i += 1) {
+    let palette = document.createElement('div');
 		
-		palette.className = 'color';
-		palette.style.backgroundColor = colorRepeat();
-		palette.style.border = '1px solid black';
-		div.appendChild(palette);		
-	}
+    palette.className = 'color';
+    palette.style.backgroundColor = colorRepeat();
+    palette.style.border = '1px solid black';
+    div.appendChild(palette);		
+  }
 }
 
 fourDistinctColorPalette(4);
 
+//Gera cor aleatória para o Desafio 2
 function colorGenerator () {
-	let req1 = (Math.random() * 256) -1;
-	let req2 = (Math.random() * 256) -1;
-	let req3 = (Math.random() * 256) -1;
+  let req1 = (Math.random() * 256) -1;
+  let req2 = (Math.random() * 256) -1;
+  let req3 = (Math.random() * 256) -1;
 
   if(req1 === 255 && req2 === 255 && req3 === 255) {
-		return colorGenerator();
-	}
+    return colorGenerator();
+  }
 
-	let randomColor = 'rgb(' + req1 + ',' + req2 + ',' + req3 + ')';
-	return randomColor;
+  let randomColor = 'rgb(' + req1 + ',' + req2 + ',' + req3 + ')';
+  return randomColor;
 }
 
+//Verifica se a função acima não tem cor repetida
 function colorRepeat () {
-	let cores = document.querySelectorAll('.color');
-	let newColor = colorGenerator();
+  let cores = document.querySelectorAll('.color');
+  let newColor = colorGenerator();
 
-	if (cores.length === 0) {
-		return newColor;
-	}
-	for (let i = 0; i < cores.length; i += 1) {
-		if (newColor === cores[i]) {
-			colorGenerator();
-		}
-		return newColor;
-	}
+  if (cores.length === 0) {
+    return newColor;
+  }
+  for (let i = 0; i < cores.length; i += 1) {
+    if (newColor === cores[i]) {
+      colorGenerator();
+    }
+    return newColor;
+  }
 }
+
+//Desafio 3
+function firstColorBlack () {
+	let cores = document.querySelectorAll('.color');
+	let tornaPreto = cores[0];
+
+	tornaPreto.style.backgroundColor = 'black';
+}
+
+firstColorBlack();
