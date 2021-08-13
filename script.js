28a73a32652151a810dd0e7d3177765492c5455e
Sayro <sayrodamaceno70@gmail.com>
2021-08-13 17:05:17 -0300

    cumpre qequisito 3, 4, 5, 6, 7

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('oi')
+const pixelBoard = document.getElementById('pixel-board');
+
+for (let index = 0; index < 25; index += 1) {
+  const divPixel = document.createElement('div');
+  divPixel.className = 'pixel';
+  pixelBoard.appendChild(divPixel);
+}
+
+const blackColor = document.getElementById('black');
+const blueColor = document.getElementById('blue');
+const yellowColor = document.getElementById('yellow');
+const greenColor = document.getElementById('green');
+
+function addClass(event) {
+  const corSelecionada = document.querySelector('.selected');
+  corSelecionada.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+blackColor.addEventListener('click', addClass);
+blueColor.addEventListener('click', addClass);
+yellowColor.addEventListener('click', addClass);
+greenColor.addEventListener('click', addClass);
