4d82c9dbfa62c8765ebc554d261020ab6afe6135
Wander Luiz <wander.srv@gmail.com>
2021-08-13 17:09:09 -0300

    Resolução dos Requisitos 5 e 6

--- script.js
+++ script.js
@@ -11,6 +11,23 @@ function corBox(){
 }
 corBox();
 
+// Função Selected
+function selecionar() {
+  let selecao = document.querySelectorAll('.color')[0];
+  selecao.className = 'color selected';
+
+  for (let index = 0; index < selecao.length; index += 1) {
+    if (selecao[index] === 0) {
+      selecao.className = 'color selected';
+    } else {
+      selecao.className = 'color';
+    }
+  } 
+}
+selecionar();
+
+
+
 
 
 
