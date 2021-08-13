cac7ba6df22a561a52330db4d8b18d8efd4b4f8e
fernandamichetti <f.michetti@hotmail.com>
2021-08-13 20:44:06 -0300

    Adicionando parte do requisito 8 e o 9 inteiro

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const pixelBoard = document.querySelector('#pixel-board');
 let amount = 25;
+const pickingDarkPink = document.getElementById('dark-pink');
+const pickingBrightPink = document.getElementById('bright-pink');
+const pickingDarkBlue = document.getElementById('dark-blue');
+const pickingDarkBlack = document.getElementById('dark-black');
+const clearBoard = document.getElementById('clear-board');
+const allPixels = document.getElementsByClassName('pixel');
 
 for (let i = 0; i < amount; i += 1) {
   const pixel = document.createElement('div');
   pixel.className = 'pixel';
   pixelBoard.appendChild(pixel);
 }
+
+for (let index = 0; index < allPixels.length; index += 1) {
+  allPixels[index].addEventListener('click', changeColor);
+}
+
 window.onload = function () {
   let black = document.getElementById('dark-black');
   black.className = 'color' + ' selected';
 };
-
-let pickingDarkPink = document.getElementById('dark-pink');
-let pickingBrightPink = document.getElementById('bright-pink');
-let pickingDarkBlue = document.getElementById('dark-blue');
-let pickingDarkBlack = document.getElementById('dark-black');
 
 pickingDarkPink.addEventListener('click', darkPink);
 
@@ -41,4 +47,16 @@ function darkBlue() {
   pickingBrightPink.className = 'color';
   pickingDarkPink.className = 'color';
   pickingDarkBlack.className = 'color';
+}
+
+function changeColor(event) {
+  const selected = document.querySelector('.selected');
+  event.target.style.backgroundColor = selected.style.backgroundColor;
+}
+
+clearBoard.addEventListener('click', clearingBoard);
+function clearingBoard() {
+  for (pixel of allPixels) {
+    pixel.style.backgroundColor = 'white';
+  }
 }
