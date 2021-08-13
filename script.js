38de3fb0efd43427fda8769917989d1f526e2b47
Rodolfo <rodolfoserralha@hotmail.com>
2021-08-13 16:07:34 -0300

    adicionando requisito 2

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
+// Requisito 1
+
 let h1 = document.createElement('h1');
 h1.id = 'title';
 h1.innerText = 'Paleta de Cores';
 document.body.appendChild(h1);
+
+// Requisito 2
+
+function createDivPai() {
+        let div = document.createElement('div');
+        div.id = 'color-palette';
+        document.body.appendChild(div);
+}
+
+createDivPai();
+
+function createDivFilhas() {
+    let arrayColors = ['black', 'blue', 'pink', 'yellow'];
+    for (let index = 0; index < 4; index += 1) {
+        let div = document.createElement('div');
+        div.className = 'color';
+        div.style.backgroundColor = arrayColors[index];
+        div.style.border = '1px solid black';
+        let divCores = document.querySelector('#color-palette');
+        divCores.appendChild(div);
+    }
+}
+
+createDivFilhas();
+
+// Exercício 3
+
+
+
