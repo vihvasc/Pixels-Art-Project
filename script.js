5f9cb2773b83f2e46cbf712c943ce6be97b896eb
vinicius23-dev <nicius.cadedes@gmail.com>
2021-08-13 16:16:11 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,18 @@
+/*let pblack = document.querySelector('#black');
+let pblack2 = document.getElementById('#black');
+
+
+let cor;
+
+//pblack.addEventListener('click', pintar) { }
+
+let ppixel = document.querySelector('.pixel');
+
+//window.onload = { }
+
+function pintar (event){
+    ppixel.style.backgroundColor = black;
+    
+}
+
+ppixel.addEventListener("change", pintar);*/
