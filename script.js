eeb6035d9ae40b4054c9eb51901ca718718f1e81
Caio <caiomgp22@gmail.com>
2021-08-13 18:45:27 -0300

    Adicionando botao de limpar

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-let square = document.body.querySelector('#pixel-board');
+const square = document.body.querySelector('#pixel-board');
 
 function squarePixel(numberOfElements) {
   for (let index = 1; index <= numberOfElements; index +=1) {
-    let pixels = document.createElement('li');
+    const pixels = document.createElement('li');
     pixels.className = 'pixel';
 
     square.appendChild(pixels);
   }
 }
 squarePixel(25);
+
+/* selectColor = document.body.getElementsByClassName('color');
+selectColor.addEvent
+
+function select() {
+  if 
+} */
+
+const button = document.body.querySelector('#clear-board');
+button.addEventListener('click', clearBoard);
+
+function clearBoard() {
+  const pixels = document.body.getElementsByClassName('pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    const pixel = pixels[index];
+    pixel.style.backgroundColor = 'white';
+  }
+}
