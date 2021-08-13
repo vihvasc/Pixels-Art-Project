2f56a4693992b0f4804a68f56399f1d95fe40098
henrique <hickm018@gmail.com>
2021-08-13 17:21:37 -0300

    Adiciona requisitos 4 e 5

---
+++ script.js
@@ -0,0 +1,23 @@
+// nDeLinhas(5);
+// nDePixelsLinha(5);
+
+// function nDePixelsLinha(n) {
+//   const childrenPixelBord = document.getElementsByClassName(
+//     'linhas-square-pixel'
+//   );
+
+//   for (let key = 1; key <= n; key += 1) {
+//     const createDiv = document.createElement('div');
+//     childrenPixelBord[key].appendChild(createDiv);
+//     createDiv.classList = 'pixel';
+//   }
+// }
+
+// function nDeLinhas(nPixels) {
+//   const pixelBoard = document.getElementById('pixel-board');
+//   for (let key = 0; key < nPixels; key += 1) {
+//     const createLines = document.createElement('div');
+//     pixelBoard.appendChild(createLines);
+//     createLines.classList = 'linhas-square-pixel';
+//   }
+// }
