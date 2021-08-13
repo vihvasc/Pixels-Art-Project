35ce4aa6ac947cd6eb173d49d6864b918b0009ab
davi <davisoarescoura@gmail.com>
2021-08-13 22:43:45 +0200

    adiciona o nono  requisito do projeto

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
+const colorOne = document.getElementById('color-one');
+const colorTwo = document.getElementById('color-two');
+const colorThree = document.getElementById('color-three');
+const colorFour = document.getElementById('color-four');
+const pixelBoard = document.getElementById('pixel-board');
+const allPixel = document.getElementsByClassName('pixel');
+const clearButton = document.getElementById('clear-board');
 
-const colorOne = document.getElementById("color-one");
-const colorTwo = document.getElementById("color-two");
-const colorThree = document.getElementById("color-three");
-const colorFour = document.getElementById("color-four");
-const pixelBoard = document.getElementById("pixel-board");
-
-colorOne.style.backgroundColor = "black";
-colorTwo.style.backgroundColor = "red";
-colorThree.style.backgroundColor = "green";
-colorFour.style.backgroundColor = "yellow";
+colorOne.style.backgroundColor = 'black';
+colorTwo.style.backgroundColor = 'red';
+colorThree.style.backgroundColor = 'green';
+colorFour.style.backgroundColor = 'yellow';
 
 colorOne.addEventListener('click', addSelectedClass);
 colorTwo.addEventListener('click', addSelectedClass);
 colorThree.addEventListener('click', addSelectedClass);
 colorFour.addEventListener('click', addSelectedClass);
 pixelBoard.addEventListener('click', changeColor);
+clearButton.addEventListener('click', clearColor);
 
 function addSelectedClass(event) {
-    let checkSelected = document.querySelector('.selected');
-    checkSelected.classList.remove('selected');
-    event.target.classList.add('selected');
-   }
+  let checkSelected = document.querySelector('.selected');
+  checkSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
 
 function changeColor(event) {
-    const colorSelected = document.querySelector(".selected").style.backgroundColor;
-    event.target.style.backgroundColor = colorSelected;
-}   
+  const colorSelected = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = colorSelected;
+}
+
+function clearColor() {
+  for (let index = 0; index < allPixel.length; index += 1) {
+    allPixel[index].style.backgroundColor = 'white';
+  }
+}
