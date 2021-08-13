6cb5907055c4f90ad30cc4cf2f8b400e0a6c000c
ArthurMoreira <arthurmcs96@gmail.com>
2021-08-13 18:40:14 -0300

    Adiciona o requisito 2

---
+++ script.js
@@ -0,0 +1,9 @@
+/* window.onload = function load() {
+  const sectionOne = document.createElement('section');
+  sectionOne.id = "color-palette";
+  for (let i = 0; i < 4; i += 1) {
+    const divColors = document.createElement('button');
+    divColors.className = 'color';
+  }
+};
+ */
