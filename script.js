a8974030a1ef0781a507495182c5bd3cb31f977b
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-13 15:37:05 -0300

    Requisito 3 completo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function colorPalette() {
   const paletaCores = document.querySelectorAll('.color');
-  const cores = ['green', 'purple', 'darkblue', 'darkred'];
+  const cores = ['black', 'purple', 'darkblue', 'darkred'];
   for (let key = 0; key < paletaCores.length; key += 1) {
     paletaCores[key].classList.add(cores[key]);
   }
