a09685873ba0e4c05b003c8b11156bac29e4b65f
DanielHott <daniel_hott@outlook.com>
2021-08-13 17:49:39 -0300

    Adicionando requisito numero 4

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ for (let i = 0; i < classe.length; i += 1) {
     let valor = classe[i].textContent
     classe[i].style.backgroundColor = valor
 }
+
+function criaQuadrado (string){
+    for(let i = 0; i < 5; i += 1){
+        let box = document.createElement('div')
+        box.className = 'pixel'
+        box.style.display = 'inline-block'
+        box.style.backgroundColor = 'white'
+        box.style.color = 'white'
+        let fileira = document.getElementsByClassName(string)[0]
+        fileira.appendChild(box)
+    }
+}
+criaQuadrado('fileira1')
+criaQuadrado('fileira2')
+criaQuadrado('fileira3')
+criaQuadrado('fileira4')
+criaQuadrado('fileira5')
