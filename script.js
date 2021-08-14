9ebb8d3fb420a06f332ab4b703cbf14d48337015
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-08-13 21:24:49 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+let elementBlack = document.getElementById('black');
+let elementColorPallete = document.getElementById('color-palette');
+let elementPixelBoard = document.getElementById('pixel-board')
+
 window.onload = function() {
     blackSelected()
-}
 
-let elementBlack = document.getElementById('black');
+}
 
 function blackSelected() {
     elementBlack.className = 'color ' + 'selected';
 }
-
-let elementColorPallete = document.getElementById('color-palette');
 
 function selectedColor(e) {
     let elementSelected = document.querySelector('.selected')
@@ -18,3 +19,28 @@ function selectedColor(e) {
     }
 }
 elementColorPallete.addEventListener('click', selectedColor);
+
+
+let elementColor = document.getElementsByClassName('color');
+
+function addPixelColor(e) {
+    let elementSelected = document.querySelector('.selected');
+    if (e.target.classList.contains('pixel')) {
+        if (elementColor[0] === elementSelected){
+            e.target.style.backgroundColor = 'black';
+
+        }
+        if (elementColor[1] === elementSelected){
+            e.target.style.backgroundColor = 'red';
+
+        }
+        if (elementColor[2] === elementSelected){
+            e.target.style.backgroundColor = 'blue';
+
+        }
+        if (elementColor[3] === elementSelected){
+            e.target.style.backgroundColor = 'darkgreen';
+        }          
+    }
+}
+elementPixelBoard.addEventListener('click', addPixelColor);
