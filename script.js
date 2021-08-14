b76e84557bd196ec8756979ba6b5ccae75208a07
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-08-14 16:53:17 -0300

    alguns ajustes

--- script.js
+++ script.js
@@ -1,91 +1,89 @@
-function colorPalette () {
-  let colors = ['black', 'purple', 'orange', 'aqua']
-  let paletteContainer = document.getElementById('color-palette');
+function colorPalette() {
+  const colors = ['black', 'purple', 'orange', 'aqua'];
+  const paletteContainer = document.getElementById('color-palette');
 
-  for ( let i = 0; i < 4; i += 1) {
-    var paletteColor = colors[i]
-    let paletteColorItem = document.createElement('li');
+  for (let i = 0; i < 4; i += 1) {
+    const paletteColor = colors[i];
+    const paletteColorItem = document.createElement('li');
     paletteColorItem.style.backgroundColor = paletteColor;
-    paletteColorItem.className = 'color'
+    paletteColorItem.className = 'color';
     paletteColorItem.innerHTML = paletteColor;
-    paletteColorItem.innerText = ''
+    paletteColorItem.innerText = '';
     paletteContainer.appendChild(paletteColorItem);
-
   }
-
 }
 
-colorPalette () 
+colorPalette();
 
-function board () {
-  let pixel = 5;
-  let wide = pixel;
-  let height = pixel
-  let lines = document.getElementsByClassName('line');
+function board() {
+  const pixel = 5;
+  const wide = pixel;
+  const lines = document.getElementsByClassName('line');
 
-  for (let i = 0; i < lines.length; i +=1){
-    for (let index = 0; index < wide; index += 1){
-      let square = document.createElement('div');
+  for (let i = 0; i < lines.length; i += 1) {
+    for (let index = 0; index < wide; index += 1) {
+      const square = document.createElement('div');
       lines[i].appendChild(square);
       square.className = 'pixel';
-    };
-  };
+    }
+  }
 }
 
-board ();
+board();
 
-function select () {
-  let selectColorBlack=document.getElementsByClassName('color')[0];
-  selectColorBlack.classList.add('selected')
+function select() {
+  const selectColorBlack = document.getElementsByClassName('color')[0];
+  selectColorBlack.classList.add('selected');
 }
 
 select();
 
-function selectedColor () {
-
-    let selectedColor = document.getElementById('color-palette')
-    let newColor = document.getElementsByClassName('color');
-      
-    selectedColor.addEventListener('click', newColors);
+function selectedColor() {
+  const selectColorPalette = document.getElementById('color-palette');
+  const colorselect = document.getElementsByClassName('color');
   //  material acessado :https://github.com/tryber/sd-08-project-pixels-art/blob/fabio-ferreira-pixels-art-project/script.js
-    function removeSelected() {
-        for (let i = 0; i < newColor.length; i += 1) {
-          newColor[i].className = 'color';
-        }
-      }
+  function removeSelected() {
+    for (let i = 0; i < colorselect.length; i += 1) {
+      colorselect[i].className = 'color';
+    }
+  }
+  //  ...........
+  // erro lint acessado:https://stackoverflow.com/questions/35637770/how-to-avoid-no-param-reassign-when-setting-a-property-on-a-dom-objec
+  function getBoardColors(event) {
+    const evitaError = event;
+    removeSelected();
+    evitaError.target.className = 'color selected';
+  }
+    // ....  
+  selectColorPalette.addEventListener('click', getBoardColors); 
+}
+selectedColor();
 
-      function newColors (newColor) {
-        removeSelected ();
-        newColor.target.className = 'color selected';
+function paintBoard() {
+  const boardPaint = document.getElementById('pixel-board');
+
+  function setColorBoard(event) {
+    // erro lint acessado:https://stackoverflow.com/questions/35637770/how-to-avoid-no-param-reassign-when-setting-a-property-on-a-dom-object
+    const evit = event;
+    // ....
+    evit.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor; // eslint-disable-line no-param-reassign
+  }
+  boardPaint.addEventListener('click', setColorBoard);
+}
+paintBoard();
+
+function clearButton() {
+  const getButton = document.querySelector('#clear-board');
+  const getPixels = document.querySelectorAll('.pixel');
+  const colorPixel = 'white';
+
+  getButton.addEventListener('click', function getwhite() {
+    for (let i = 0; i < getPixels.length; i += 1) {
+      if (getPixels[i].style.backgroundColor !== colorPixel) {
+        getPixels[i].style.backgroundColor = colorPixel;
       }
     }
-    selectedColor () ;
-  
-  
-    function paintBoard () {
-    
-      const board = document.getElementById('pixel-board');
-  
-      board.addEventListener('click', setColor);
+  });
+}
 
-      function setColor(event) {
-      event.target.style.backgroundColor = document.querySelector('.color.selected').style.backgroundColor;    
-        }
-        }
-
-      paintBoard ();
-
-      function clearButton (){
-        let getButton = document.querySelector('#clear-board');
-        let getPixels = document.querySelectorAll('.pixel')
-        let colorPixel = 'white';
-        getButton.addEventListener('click', function() {
-          for (let i = 0; i <getPixels.length; i +=1) {
-            if ( getPixels[i].style.backgroundColor !== colorPixel ) {
-              getPixels[i].style.backgroundColor = colorPixel
-            }
-          }
-        })
-
-      }
-      clearButton()
+clearButton();
