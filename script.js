db7466acd4b8af73aa5c673895ab1846adb33ea8
Paulo Flóra <tflora.2004@gmail.com>
2021-08-14 05:05:41 -0300

    Feito o requisito 12.

--- script.js
+++ script.js
@@ -11,7 +11,22 @@ for (let i = 0; i < numberOfColors; i += 1) {
 }
 
 const colorBox = document.getElementsByClassName('color');
-const colorSample = ['black', 'red', 'green', 'purple'];
+
+// Gera cores aleatórias
+function generateRandomColor() {
+  const values = '0123456789ABCDEF';
+  let randomColor = '#';
+  for (let i = 0; i < 6; i += 1) {
+    randomColor += values[Math.floor(Math.random() * 16)];
+  }
+  return randomColor;
+}
+
+const colorSample = ['Black'];
+
+for (let i = 0; i < 3; i += 1) {
+  colorSample.push(generateRandomColor());
+}
 
 for (let i = 0; i < colorBox.length; i += 1) {
   colorBox[i].style.background = colorSample[i];
