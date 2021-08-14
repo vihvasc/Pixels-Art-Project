568fefe966c8392b807321d6ed42a21e4b4f03bf
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-08-14 01:21:09 -0400

    Finaliza corretamente requisitos de 1 a 7

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
-const palleteItems = document.getElementsByClassName('color');
+const paletteItem = document.getElementsByClassName('color');
 const takeBody = document.getElementsByTagName('body');
-const listColor = ['#000', '#3df', '#F5D622', '#F52022'];
+const listColor = ['rgba(0,0,0)', 'rgba(51, 221, 255)', 'rgba(245, 214, 34)', 'rgba(245, 32, 34)'];
 const gridLength = 5;
+let currentColor = 'rgba(0,0,0)';
 
-function appendColor() {
-  for(let index = 0; index < palleteItems.length; index ++) {
-    palleteItems[index].style.backgroundColor = listColor[index];
+
+function pickColor(evento) {
+  const newColor = getComputedStyle(evento.target);
+  for (let item of paletteItem) {
+    item.classList.remove('selected');
   }
+  evento.target.classList.add('selected');
+  currentColor = newColor.backgroundColor;
+}
+
+function appendPaletteColor() {
+  paletteItem[0].classList.add('selected')
+  for (let index = 0; index < paletteItem.length; index += 1) {
+    paletteItem[index].style.backgroundColor = listColor[index];
+    paletteItem[index].addEventListener('click', pickColor);
+  }
+}
+
+function changeColor(evento) {
+  const fundo = evento.target;
+  console.log(fundo.style.backgroundColor)
+  fundo.style.backgroundColor = currentColor;
 }
 
 function generateGrid() {
   const containerGrid = document.createElement('div');
   containerGrid.id = 'pixel-board';
 
-  for (let linha = 0; linha < gridLength; linha ++) {
+  for (let linha = 0; linha < gridLength; linha += 1) {
     const contaienrLine = document.createElement('div');
     contaienrLine.className = 'grid-line';
-    for (let coluna = 0; coluna < gridLength; coluna ++) {
+    for (let coluna = 0; coluna < gridLength; coluna += 1) {
       const containerColumn = document.createElement('div');
       containerColumn.className = 'pixel';
+      containerColumn.addEventListener('click', changeColor);
 
       contaienrLine.appendChild(containerColumn);
     }
     containerGrid.appendChild(contaienrLine);
   }
-  takeBody[0].appendChild(containerGrid)
+  takeBody[0].appendChild(containerGrid);
 }
 
-window.onload = function() {
-  appendColor();
+function iniciando() {
+  appendPaletteColor();
   generateGrid();
 }
+
+window.onload = iniciando;
