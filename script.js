0a013f1c0e4d5a6bb7042975bda0590e3ca0323a
Allan Melo <allan-m-silva@hotmail.com>
2021-08-13 23:08:36 -0300

    adiciona requisito 8

--- script.js
+++ script.js
@@ -9,5 +9,19 @@ function changeSelected(trigger){
     element.classList.add("selected");
 }
 
+function changePixels (trigger){
+    let element = trigger.target
+    let selected = document.querySelector(".selected")
+    let style = window.getComputedStyle(selected)
+    let color = style.getPropertyValue("background-color")
+    element.style.setProperty("background-color", color)
+}
+
         let colorPalette = document.querySelector("#color-palette")
         colorPalette.addEventListener("click", changeSelected)
+
+        let pixel = document.getElementsByClassName("pixel")
+        for(let counter = 0; counter < pixel.length; counter += 1) {
+            let add = pixel[counter]
+            add.addEventListener("click", changePixels)
+        }
