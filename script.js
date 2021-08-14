46ffbe07c5b771395acd3fc2e832c82786fe8bdd
iuryroque <iury.roque@hotmail.com>
2021-08-14 19:21:27 -0300

    Exercicio 8 concluido

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const colorPalette = document.getElementById("color-palette");
+const pixelBoard = document.getElementById("pixel-board");
 
 colorPalette.children[0].style.backgroundColor = "black"
 colorPalette.children[1].style.backgroundColor = "red"
 colorPalette.children[2].style.backgroundColor = "green"
 colorPalette.children[3].style.backgroundColor = "blue"
 
-colorPalette.addEventListener("click", addClassSelected);
-
 //A box black começa com a class selected, quando clickar remove a class na antiga e coloca na nova
+
+colorPalette.addEventListener("click", addClassSelected);
 function addClassSelected(event) {
   let select = document.querySelector(".selected");
   select.classList.remove("selected");
   event.target.classList.add("selected")
 }
 
-colorPalette.addEventListener("click", addColor);
-document.querySelector(".selected").style.backgroundColor
-
+pixelBoard.addEventListener("click", addColor);
 function addColor(event) {
-  const pixelBoard = document.getElementById("pixel-board");
-  
+  let selectedCollor = document.querySelector(".selected").style.backgroundColor
+  event.target.style.setProperty("background-color", selectedCollor)
 }
-
-
