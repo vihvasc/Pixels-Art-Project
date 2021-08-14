591d56fdb828d339ff0625fc59b64fc1a64d21c4
1lucascq <1lucascq@gmail.com>
2021-08-14 08:58:53 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+window.onload = function(){
+  const blackColor = document.querySelector('#black');
+  blackColor.className = 'color selected';
+}
+
 const pixelBoard = document.querySelector('#pixel-board');
-for (let i = 0; i < 25; i += 1) { 
+for (let i = 0; i < 25; i += 1) {
   let pixel = document.createElement('div');
   pixel.className = 'pixel';
   pixelBoard.appendChild(pixel);
