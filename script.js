79de272f65812b8d304418edc3bbe55c8160b09b
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-14 14:51:32 -0300

    adiciona requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function selectColor() {
-  const colorPalette = document.querySelector('#color-palette');
+const pixelBoard = document.querySelector('#pixel-board');
+const clearButton = document.querySelector('#clear-board');
+const pixels = document.querySelectorAll('.pixel');
+const colorPalette = document.querySelector('#color-palette');
 
+function selectColor() {
   colorPalette.addEventListener('click', (event) => {
     if (event.target !== colorPalette) {
       const selected = document.querySelector('.selected');
@@ -9,18 +12,21 @@ function selectColor() {
     }
   });
 }
-
 selectColor();
 
-// function selectColor() {
-//   const colorPalette = document.querySelector('#color-palette');
-//   const paletteElement = document.querySelectorAll('.color')
 
-//   colorPalette.addEventListener('click', (event) => {
-//     const selected = document.querySelector('.selected');
-//     paletteElement.classList.add('selected');
-//     selected.classList.remove('selected');
-//   });
-// }
+pixelBoard.addEventListener('click', (event) => {
+  const selectedColor = document.querySelector('.selected');
+  const computedStyle = getComputedStyle(selectedColor);
+  const color = computedStyle.getPropertyValue('background-color');
 
-// selectColor();
+  if (event.target.classList.contains('pixel')) {
+    event.target.style.backgroundColor = color;
+  }
+});
+
+clearButton.addEventListener('click', (event)=>{
+for (const i of pixels) {
+  i.style.backgroundColor = 'white';
+}
+});
