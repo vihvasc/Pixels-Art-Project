b032f695a5bb3daf68fe87669a84c5b9b2ed5eb7
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-08-14 04:28:39 -0300

    resolucao requisito 8

--- script.js
+++ script.js
@@ -11,7 +11,21 @@ green.addEventListener('click', selected)
 function selected(event){
     let sel = document.querySelector('.selected')
     sel.classList.remove('selected')
-
+    
     event.target.classList.add('selected')
 }
+
+let li = document.getElementsByTagName('li');
+
+for(let key of li){
+    key.addEventListener('click', troca)
+}
+
+
+function troca(event){
+    let colorir = document.querySelector('.selected')
+    let colorir2 = window.getComputedStyle(colorir).backgroundColor;
+    event.target.style.backgroundColor = colorir2
+}
+
 
