82965f04e64245a370921316c4bddd228e31263d
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-08-14 03:07:27 -0300

    adiciona requisitos 6 e 7

---
+++ script.js
@@ -0,0 +1,17 @@
+let black = document.getElementById('black');
+let red = document.getElementById('red');
+let blue = document.getElementById('blue');
+let green = document.getElementById('green');
+
+black.addEventListener('click', selected)
+red.addEventListener('click', selected)
+blue.addEventListener('click', selected)
+green.addEventListener('click', selected)
+
+function selected(event){
+    let sel = document.querySelector('.selected')
+    sel.classList.remove('selected')
+
+    event.target.classList.add('selected')
+}
+
