d73f7697d47a7d518b3a81b155f97582ecd580c5
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-13 21:13:52 -0300

    adiciona requisito 6

---
+++ script.js
@@ -0,0 +1,20 @@
+// window.onload = function () {
+
+//   selectedColor();
+// }
+
+// function selectedColor() {
+//   let paletteColor = document.querySelector('.color');
+//   let selectedColor = document.querySelectorAll('.color');
+//    paletteColor.addEventListener('click', function(){
+//   for(let i = 0; i < paletteColor.length; i += 1) {
+//  if (selectedColor[i].classList.value !== 'color selected'){
+//    selectedColor[i].classList.add('selected')
+//  } else {
+//   selectedColor[i].className = 'color';
+// }
+// }
+//  })
+// }
+// console.log(selectedColor[0].classList.value)
+
