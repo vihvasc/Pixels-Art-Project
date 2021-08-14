5fb2253d3e57cc1b5f6537852888e98b0815d7a9
Flávio Bianchetti <fjb.developer@gmail.com>
2021-08-14 14:45:40 -0300

    refatorado o codigo, atendidos os requisitos 10 e 11 - validado via npm

--- script.js
+++ script.js
@@ -1,13 +1,142 @@
-function startPage() {
-  const parent = document.getElementById('color-palette').children;
-  parent[0].style.backgroundColor = 'black';
-  parent[1].style.backgroundColor = 'green';
-  parent[2].style.backgroundColor = 'gold';
-  parent[3].style.backgroundColor = 'blue';
-  parent[0].classList.add('selected');
+const id = ['title', 'color-palette', 'clear-board',
+  'board-size', 'generate-board', 'pixel-board'];
+const configHeader = {
+  elementType: 'body',
+  elementNum: 0,
+  newElementType: 'header',
+  newElementId: '',
+  newElementClass: '',
+  newElementText: '',
+};
+const configH1 = {
+  elementType: 'header',
+  elementNum: 0,
+  newElementType: 'h1',
+  newElementId: id[0],
+  newElementClass: '',
+  newElementText: 'Paleta de Cores',
+};
+const configColorPalette = {
+  elementType: 'body',
+  elementNum: 0,
+  newElementType: 'section',
+  newElementId: id[1],
+  newElementClass: '',
+  newElementText: '',
+};
+const configSectionButtons = {
+  elementType: 'body',
+  elementNum: 0,
+  newElementType: 'section',
+  newElementId: '',
+  newElementClass: '',
+  newElementText: '',
+};
+const configDivClear = {
+  elementType: 'section',
+  elementNum: 1,
+  newElementType: 'div',
+  newElementId: '',
+  newElementClass: '',
+  newElementText: '',
+};
+const configButtonClear = {
+  elementType: 'div',
+  elementNum: 5,
+  newElementType: 'button',
+  newElementId: id[2],
+  newElementClass: '',
+  newElementText: 'Limpar',
+};
+const configDivSize = {
+  elementType: 'section',
+  elementNum: 1,
+  newElementType: 'div',
+  newElementId: '',
+  newElementClass: '',
+  newElementText: '',
+};
+const configInputSize = {
+  elementType: 'div',
+  elementNum: 6,
+  newElementType: 'input',
+  newElementId: id[3],
+  newElementClass: '',
+  newElementText: '',
+};
+const configButtonSize = {
+  elementType: 'div',
+  elementNum: 6,
+  newElementType: 'button',
+  newElementId: id[4],
+  newElementClass: '',
+  newElementText: 'VQV',
+};
+const configPixelBoard = {
+  elementType: 'body',
+  elementNum: 0,
+  newElementType: 'section',
+  newElementId: id[5],
+  newElementClass: '',
+  newElementText: '',
+};
+
+function createElement(object) {
+  const myParent = document.getElementsByTagName(object.elementType)[object.elementNum];
+  const myNewElement = document.createElement(object.newElementType);
+  if (object.newElementId.length > 0) {
+    myNewElement.id = object.newElementId;
+  }
+  if (object.newElementClass.length > 0) {
+    myNewElement.className = object.newElementClass;
+  }
+  if (object.newElementText.length > 0) {
+    myNewElement.innerText = object.newElementText;
+  }
+  myParent.appendChild(myNewElement);
 }
 
-startPage();
+function createCustomElements(parentId, newElementClass, numElements) {
+  const myParent = document.getElementById(parentId);
+  const myDiv = document.createElement('div');
+  for (let index = 0; index < numElements; index += 1) {
+    const myDivPixel = document.createElement('div');
+    myDivPixel.className = newElementClass;
+    myDiv.appendChild(myDivPixel);
+  }
+  myParent.appendChild(myDiv);
+}
+
+function createPageStructure() {
+  createElement(configHeader);
+  createElement(configH1);
+  createElement(configColorPalette);
+  createCustomElements(id[1], 'color', 4);
+  createElement(configSectionButtons);
+  createElement(configDivClear);
+  createElement(configButtonClear);
+  createElement(configDivSize);
+  createElement(configInputSize);
+  createElement(configButtonSize);
+  createElement(configPixelBoard);
+  for (let index = 0; index < 5; index += 1) {
+    createCustomElements(id[5], 'pixel', 5);
+  }
+}
+
+createPageStructure();
+
+function startColorPalette(idValue) {
+  const parent = document.getElementById(idValue);
+  const myDiv = parent.children[0];
+  myDiv.childNodes[0].style.backgroundColor = 'black';
+  myDiv.childNodes[1].style.backgroundColor = 'green';
+  myDiv.childNodes[2].style.backgroundColor = 'gold';
+  myDiv.childNodes[3].style.backgroundColor = 'blue';
+  myDiv.childNodes[0].classList.add('selected');
+}
+
+startColorPalette(id[1]);
 
 function selectColorInPalette(event) {
   if (event.target.className === 'color') {
@@ -17,44 +146,86 @@ function selectColorInPalette(event) {
   }
 }
 
-function selectPalette() {
-  const parent = document.getElementById('color-palette');
+function selectPalette(idValue) {
+  const parent = document.getElementById(idValue).children[0];
   parent.addEventListener('click', selectColorInPalette);
 }
 
-selectPalette();
+selectPalette(id[1]);
 
 function paintSquareInBoard(event) {
-  let square = event;
+  const square = event;
   if (event.target.className === 'pixel') {
     const selectedColor = document.querySelector('.selected');
     square.target.style.backgroundColor = selectedColor.style.backgroundColor;
   }
 }
 
-function paintSquares() {
-  const parent = document.getElementById('pixel-board');
+function paintSquares(idValue) {
+  const parent = document.getElementById(idValue);
   parent.addEventListener('click', paintSquareInBoard);
 }
 
-paintSquares();
+paintSquares(id[5]);
 
 function cleanPixelBoard() {
-  const section = document.querySelector('#pixel-board').children;
-  for (let index of section) {
-    for (let index2 of index.children) {
-      index2.style.backgroundColor = 'white';
+  const parent = document.getElementById(id[5]).children;
+  for (let index = 0; index < parent.length; index += 1) {
+    for (let index2 = 0; index2 < parent[index].children.length; index2 += 1) {
+      parent[index].children[index2].style.backgroundColor = 'white';
     }
   }
 }
 
-function insertButton() {
-  const section = document.getElementsByTagName('body')[0].children[2];
-  const button = document.createElement('button');
-  button.innerText = 'Limpar';
-  button.id = 'clear-board';
+function startClearButton(idValue) {
+  const button = document.getElementById(idValue);
   button.addEventListener('click', cleanPixelBoard);
-  section.appendChild(button);
 }
 
-insertButton();
+startClearButton(id[2]);
+
+function removePixelsBoard() {
+  const parent = document.getElementById(id[5]);
+  const sizeOfParent = parent.childElementCount;
+  for (let index = 0; index < sizeOfParent; index += 1) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function createPixelsBoard(numPixels) {
+  let newNumPixels = numPixels;
+  if (numPixels > 50) {
+    newNumPixels = 50;
+  }
+  if (numPixels < 5) {
+    newNumPixels = 5;
+  }
+  for (let index = 0; index < newNumPixels; index += 1) {
+    createCustomElements('pixel-board', 'pixel', newNumPixels);
+  }
+}
+
+function newPixelsBoard() {
+  const input = document.getElementById(id[3]);
+  if (input.value !== '') {
+    removePixelsBoard();
+    createPixelsBoard(parseInt(input.value, 10));
+  } else {
+    window.alert('Board inválido!');
+  }
+}
+
+function startNewPixelBoard() {
+  const button = document.getElementById('generate-board');
+  button.addEventListener('click', newPixelsBoard);
+}
+
+startNewPixelBoard();
+
+function inputPixels() {
+  const input = document.getElementById('board-size');
+  input.setAttribute('min', '1');
+  input.setAttribute('type', 'number');
+}
+
+inputPixels();
