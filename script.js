817e8e92af9f1c012378fde436d0236223212eb8
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-08-14 13:58:20 -0300

    realizando requisito 4

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// aqui algumas variaveis que criei para
+// poder fazer o quadrado com 5 de largura e 5 da altura.
+let divPai = document.querySelector("#pixel-board")
+let divFilho1 = document.createElement("div");
+let divFilho2 = document.createElement("div");
+let divFilho3 = document.createElement("div");
+let divFilho4 = document.createElement("div");
+let divFilho5 = document.createElement("div");
+divPai.appendChild(divFilho1) && divPai.appendChild(divFilho2) && divPai.appendChild(divFilho3) && divPai.appendChild(divFilho4) && divPai.appendChild(divFilho5);
+
+
+
 function coresDasPaletas() {
     let cor1 = document.getElementById('cor1');
     let cor2 = document.getElementById('cor2');
@@ -10,3 +22,21 @@ function coresDasPaletas() {
     cor4.style.backgroundColor = '#FAC114';
 }
 coresDasPaletas();
+
+
+
+
+function criandoQuadradoDivs() {
+    let arrayDeDivs = [divFilho1, divFilho2, divFilho3, divFilho4, divFilho5];
+
+    for (let index = 0; index < arrayDeDivs.length; index += 1) {
+        arrayDeDivs[index].classList.add('pixel');
+    }
+    for (let index = 1; index <= 20; index += 1) {
+        divPai.innerHTML += '<div class="pixel"></div>';
+    }
+
+    // let corInicialPixels = document.getElementsByClassName('pixel')
+    
+}
+criandoQuadradoDivs();
