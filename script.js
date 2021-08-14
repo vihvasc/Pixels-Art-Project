60dc89ff69029dd173b2956b74bc1c8a4e1eadf4
Samuel Pereira <gmr.quest.10@gmail.com>
2021-08-14 17:01:47 -0300

    Requisito 2 concluido

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  let palette = document.getElementsByClassName('color');
+
+  palette[0].style.backgroundColor = '#efea5a';
+  palette[1].style.backgroundColor = '#16db93';
+  palette[2].style.backgroundColor = '#048ba8';
+  palette[3].style.backgroundColor = '#a4036f';
+}
