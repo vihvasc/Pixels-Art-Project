8e30bf5cd7a22939a1d2723a779215dceb3931e1
Jamez Mayke <jamezmayke@gmail.com>
2021-08-14 14:07:11 -0300

    Requisito 5 feito

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const getPixel = document.querySelectorAll(".pixel");
+const getPixel = document.querySelectorAll('.pixel');
 
 // inicializa com a cor de todos os quadrados em branco
 for (let index = 0; index < getPixel.length; index += 1) {
-  getPixel[index].style.backgroundColor = "white";
+  getPixel[index].style.backgroundColor = 'white';
 }
