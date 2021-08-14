26d41e7234adf6e63840ac5ef11748229d366a56
LeonardoMonti <contatoleomonti@gmail.com>
2021-08-14 11:41:10 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-function createPixelDivs() {
-  const test = document.querySelector('#pixel-board');
-  let lengthDivs;
+const test = document.querySelector('#pixel-board');
+const firstColor = document.getElementById('firstColor');
+const secondColor = document.getElementById('secondColor');
+const thirdColor = document.getElementById('thirdColor');
+const fourthColor = document.getElementById('fourthColor');
 
-  const yHeight = 5;
-  const xWidth = 5;
+function createPixelDivs(param1) {
+  let lengthDivs;
+  const yHeight = param1;
+  const xWidth = param1;
+
   for (let index = 0; index < yHeight * xWidth; index += 1) {
     lengthDivs = document.createElement('div');
     test.appendChild(lengthDivs);
     lengthDivs.classList = 'pixel';
   }
 }
-createPixelDivs();
+createPixelDivs(5);
+
+/* https://github.com/LeonardoMonti/trybe-exercicios/blob/exercicios/5.3/01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/para-fixar/main.js */
+function addAndRemoveClassSelected(event) {
+  firstColor.classList.remove('selected');
+  secondColor.classList.remove('selected');
+  thirdColor.classList.remove('selected');
+  fourthColor.classList.remove('selected');
+
+  event.target.classList.add('selected');
+}
+firstColor.addEventListener('click', addAndRemoveClassSelected);
+secondColor.addEventListener('click', addAndRemoveClassSelected);
+thirdColor.addEventListener('click', addAndRemoveClassSelected);
+fourthColor.addEventListener('click', addAndRemoveClassSelected);
