a8ceabee973d51e155860b58cab09e4662d069a5
Joao Marco <joaomarco.matta@gmail.com>
2021-08-13 22:38:03 -0300

    Implementa requisito 8.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Criar função que aumenta o tamanho do quadrado da paleta de cores quando selecionada (utilizar a classe .selected-color do CSS).
-
-// Inicia Scripts:
+// Seleciona um quadrado da paleta de cores e adiciona a classe 'selected', excluindo esta classe de todos os outros quadrados.
 const getColorPalette = document.getElementsByClassName('color');
 
 function handleColorPaletteSelection(event) {
@@ -10,7 +8,28 @@ function handleColorPaletteSelection(event) {
   }
   targetEvent.target.classList.add('selected');
 }
-
-for (let i = 0; i < getColorPalette.length; i += 1) {
-  getColorPalette[i].addEventListener('click', handleColorPaletteSelection);
+function addPaletteEventListener() {
+  for (let i = 0; i < getColorPalette.length; i += 1) {
+    getColorPalette[i].addEventListener('click', handleColorPaletteSelection);
+  }
 }
+
+// Requisito 8 - Pintar quadrados do pixel board.
+function handlePaintingPixelBoard(event) {
+  const selectedColor = document.querySelector('.selected');
+  const newColor = window.getComputedStyle(selectedColor).backgroundColor;
+  const targetEvent = event;
+  targetEvent.target.style.backgroundColor = newColor;
+}
+
+function addPixelEventListener() {
+  const allPixels = document.getElementsByClassName('pixel');
+  for (let i = 0; i < allPixels.length; i += 1) {
+    allPixels[i].addEventListener('click', handlePaintingPixelBoard);
+  }
+}
+
+window.onload = function () {
+  addPaletteEventListener();
+  addPixelEventListener();
+};
