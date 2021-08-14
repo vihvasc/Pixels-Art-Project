9c27867758ec094c8e424ef3e652d34b843bfe3f
iuryroque <iury.roque@hotmail.com>
2021-08-14 20:05:16 -0300

    Exercicio 9 concluido e o exercicio 8 refatorado

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const colorPalette = document.getElementById("color-palette");
 const pixelBoard = document.getElementById("pixel-board");
+const buttonClear = document.getElementById("clear-board");
+let pixels = document.getElementsByClassName("pixel");
 
 colorPalette.children[0].style.backgroundColor = "black"
 colorPalette.children[1].style.backgroundColor = "red"
@@ -12,11 +14,20 @@ colorPalette.addEventListener("click", addClassSelected);
 function addClassSelected(event) {
   let select = document.querySelector(".selected");
   select.classList.remove("selected");
-  event.target.classList.add("selected")
+  event.target.classList.add("selected");
 }
 
-pixelBoard.addEventListener("click", addColor);
+for (const pixel of pixels) {
+  pixel.addEventListener("click", addColor);
+}
 function addColor(event) {
-  let selectedCollor = document.querySelector(".selected").style.backgroundColor
-  event.target.style.setProperty("background-color", selectedCollor)
+  let selectedCollor = document.querySelector(".selected").style.backgroundColor;
+  event.target.style.setProperty("background-color", selectedCollor);
+}
+
+buttonClear.addEventListener("click", clearPixels);
+function clearPixels() {
+  for (const pixel of pixels) {
+    pixel.style.setProperty("background-color", "white");
+  }
 }
