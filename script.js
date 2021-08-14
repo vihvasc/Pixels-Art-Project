6e9e812629477b87d84499738653726065f0e107
José Erivam <erivamdev@gmail.com>
2021-08-14 18:00:14 -0300

    Adicionando requisito 9

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// adicionando cores as paletas de cores e add a uma variavel
 let boxBlackA = document.getElementsByClassName('color')[0];
 boxBlackA.style.backgroundColor = 'black';
 
@@ -13,7 +13,7 @@ let boxBlackD = document.getElementsByClassName('color')[3];
 boxBlackD.style.backgroundColor = 'MediumSpringGreen';
 
 let pixelBoard = document.getElementById('pixel-board');
-
+// criando a tela com as boxs
 function creatBox() {
     for (let i = 0; i < 5; i += 1) {
         let divLineBox = document.createElement('div');
@@ -27,7 +27,7 @@ function creatBox() {
 }
 
 creatBox();
-
+// função para selecionar a cor
 function selectColor(evento) {
 
     let selecionado = document.querySelector('.selected');
@@ -40,10 +40,26 @@ boxBlackB.addEventListener('click', selectColor);
 boxBlackC.addEventListener('click', selectColor);
 boxBlackD.addEventListener('click', selectColor);
 
-
 let main = document.querySelector('main');
-main.addEventListener('click', function (evento) {
+
+main.addEventListener('click', colorPrint);
+// funação para pintar as boxs
+function colorPrint(evento) {
     let corSelecionada = document.querySelector('.selected');
     evento.target.style.backgroundColor = corSelecionada.style.backgroundColor;
+}
+//adicionando o texto limpar no botão
+let botao = document.querySelector('button');
+botao.innerText = 'Limpar';
+// criando a função para limpar
+botao.addEventListener('click', clearColor);
 
-})
+function clearColor(evento) {
+    for (let i = 0; i < 25; i += 1) {
+        let limpar = document.querySelectorAll('.pixel')[i];
+        limpar.style.backgroundColor = 'white';
+        evento.target.style.backgroundColor = limpar;
+    }
+    
+
+}
