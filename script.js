2e6a6245180a3d6d03fb233d5090ad67903c4055
Samuel Pereira <gmr.quest.10@gmail.com>
2021-08-14 17:07:08 -0300

    Requisito 3 concluido

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-  let palette = document.getElementsByClassName('color');
+  const palette = document.getElementsByClassName('color');
 
-  palette[0].style.backgroundColor = '#efea5a';
+  palette[0].style.backgroundColor = '#000000';
   palette[1].style.backgroundColor = '#16db93';
   palette[2].style.backgroundColor = '#048ba8';
   palette[3].style.backgroundColor = '#a4036f';
-}
+};
