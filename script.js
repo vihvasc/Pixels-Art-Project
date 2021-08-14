f5a7a7a1e768fbb9b4ed32c4f572027c33fdf541
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-14 17:13:03 -0300

    Adiciona funções de randomização da palheta de cores

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-
-//chamada das funções
 createBoard();
-firstColor();
-fillPalette();
-// createEvent(pixelBoard);
-// createEvents();
 
 //variáveis globais
 var paletteList = document.querySelectorAll(".color");
 var pixelBoard = document.querySelectorAll(".pixel");
+var board = document.getElementById("pixel-board");
 var selectedColor = 'Black';
 var button = document.getElementById("clear-board");
+var boardButton = document.getElementById("generate-board");
+var boardInput = document.getElementById("board-size");
+var boardColumns = 5;
 
 
-//função que cria os grids para o pixel-board
+// função que cria os grids para o pixel-board
 function createBoard(){
   for(let i = 0; i < 25; i ++){
     let box = document.createElement("div");
@@ -25,47 +23,46 @@ function createBoard(){
   }
 }
 
+// boardButton.addEventListener('click', resizeBoard);
+// boardInput.addEventListener('change', getValue);
+
+// function getValue(){
+//   boardColumns = event.target.value;
+  
+// }
+// function resetSize(){
+//   pixelBoard = document.querySelectorAll(".pixel");
+// for(let i = 0; i < pixelBoard.length; i ++){
+//   board.removeChild(pixelBoard[i]);
+// }
+// console.log(pixelBoard);
+// }
+
+// function resizeBoard(){
+//   resetSize();
+//   var boardSize = boardColumns*boardColumns;
+//   let templateGrid = document.getElementById("pixel-board");
+//   let stringGrid = "repeat(" + boardColumns + ", 40px)";
+//   templateGrid.style.gridTemplateColumns = stringGrid;
+
+//   for(let i = 0; i < boardSize; i ++){
+//     let box = document.createElement("div");
+//     box.classList.add("pixel");
+//     box.classList.add("box");
+
+//     board.appendChild(box);
+//   }
+// }
+
+
 //seta a primeira cor da paleta como preto
+firstColor();
 function firstColor(){
   let firstBox = document.querySelector(".color");
   firstBox.style.backgroundColor = "black";
   firstBox.className = "selected color box";
 }
 
-function fillPalette(){
-  let boxes = document.querySelectorAll(".color");
-  boxes[1].style.backgroundColor = 'blue';
-  boxes[2].style.backgroundColor = 'red';
-  boxes[3].style.backgroundColor = 'violet'
-}
-
-// //gera as cores para preencher a palheta
-// function generateRGB(){
-//   let max = 256;
-//   let min = 35;
-// let redValue = Math.round(Math.random(min, max)*1000);
-// let greenValue = Math.round(Math.random(min, max)*1000);
-// let blueValue = Math.round(Math.random(min, max)*1000);
-
-// let rgbValue = [redValue, greenValue, blueValue];
-// return rgbValue;
-// }
-
-// //cria um string com o formato de declaração rgb do CSS
-// function createRGBString(color){
-//   let rgbString = 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')';
-//   return rgbString;
-// }
-// fillPalette();
-// //preenche a palheta de cores com as cores aleatórias geradas
-// function fillPalette(second, third, fourth){
-//   let boxesPalette = document.querySelectorAll(".color");
-//   let numericColor = generateRGB();
-  
-//   for(let i = 1; i < 4; i ++){
-//     boxesPalette[i].style.backgroundColor = createRGBString(numericColor);
-//   }
-// }
 function selectColor(event){
   for(let i = 0; i < paletteList.length; i ++){
     if(event.target === paletteList[i]){
@@ -84,7 +81,7 @@ function selectColor(event){
 function paint(){
   event.target.style.backgroundColor = selectedColor;
 }
-createEvent(pixelBoard, paletteList);
+
 function createEvent(pixelBoard, paletteList){
   for(let i =0; i < pixelBoard.length; i++){
     pixelBoard[i].addEventListener('click', paint);
@@ -93,6 +90,7 @@ function createEvent(pixelBoard, paletteList){
   for(let i =0; i < paletteList.length; i ++){
     paletteList[i].addEventListener('click', selectColor);
   }
+  console.log(pixelBoard.length);
 }
 
 button.addEventListener('click', clearBoard);
@@ -101,5 +99,41 @@ function clearBoard(){
   
   for(let key of pixelBoard){
     key.style.backgroundColor = 'white';
+  }
+}
+
+createEvent(pixelBoard, paletteList);
+
+//randomizar cores da palheta
+//criar função que gera um número aleatório
+function randomNumber(){
+let number = Math.random()*100;
+if(number > 255 || number < 0){
+  number = Math.random()*100;
+}
+return number;
+}
+//criar função que monta uma string rgg
+function rgbString(value){
+  let red = randomNumber();
+  let green = randomNumber();
+  let blue = randomNumber();
+
+  let string = "rgb(" + red + ", " + green + ", " + blue + ")";
+  console.log(string);
+  return string;
+}
+
+fillPalette();
+function fillPalette(){
+  let boxes = document.querySelectorAll(".color");
+  // boxes[1].style.backgroundColor = 'blue';
+  // boxes[2].style.backgroundColor = 'red';
+  // boxes[3].style.backgroundColor = 'green'
+  for(let i = 1; i < boxes.length; i ++){
+    boxes[i].style.backgroundColor = rgbString();
+    if(boxes[i].style.backgroundColor === 'black'){
+      boxes[i].style.backgroundColor = rgbString();
+    }
   }
 }
