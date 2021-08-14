cf24ecef5d0da61b0f2b5e7ef623ab2fbfd6f4b8
Jamez Mayke <jamezmayke@gmail.com>
2021-08-14 15:04:22 -0300

    Requisito 6 e 7 feitos

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+const getColor = document.querySelectorAll('.color');
 const getPixel = document.querySelectorAll('.pixel');
 
 // inicializa com a cor de todos os quadrados em branco
 for (let index = 0; index < getPixel.length; index += 1) {
   getPixel[index].style.backgroundColor = 'white';
+}
+
+for (let index = 0; index < getColor.length; index += 1) {
+  getColor[index].addEventListener('click', selectedClass);
+}
+
+function selectedClass(event) {
+  for (let index = 0; index < getColor.length; index += 1) {
+    getColor[index].classList.remove('selected')
+  }
+  event.target.classList.add('selected')
 }
