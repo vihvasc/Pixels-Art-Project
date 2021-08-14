e13bb659e3e64b061d6221900bb1e7b411bb24ba
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2021-08-14 11:51:04 -0300

    [Projeto] Ajusta espaçamentos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Gera cor aleatória para o Desafio 2
 function colorGenerator() {
-	const req1 = (Math.random() * 256) - 1;
+  const req1 = (Math.random() * 256) - 1;
   const req2 = (Math.random() * 256) - 1;
   const req3 = (Math.random() * 256) - 1;
 
@@ -14,13 +14,13 @@ function colorGenerator() {
 
 // Verifica se a função acima não tem cor repetida
 function colorRepeat() {
-  let cores = document.querySelectorAll('.color');
-  let newColor = colorGenerator();
+  const cores = document.querySelectorAll('.color');
+  const newColor = colorGenerator();
 
   if (cores.length === 0) {
     return newColor;
   }
-  for (let i = 0; i < cores.length; i += 1) {
+  for (let i = 0; i <= cores.length; i += 1) {
     if (newColor === cores[i]) {
       colorGenerator();
     }
@@ -46,8 +46,8 @@ fourDistinctColorPalette(4);
 
 // Desafio 3
 function firstColorBlack() {
-  let cores = document.querySelectorAll('.color');
-  let tornaPreto = cores[0];
+  const cores = document.querySelectorAll('.color');
+  const tornaPreto = cores[0];
 
   tornaPreto.style.backgroundColor = 'black';
 }
@@ -56,47 +56,43 @@ firstColorBlack();
 
 // Desafio 4
 function quadroDePixels(tamanho) {
-  let quadroPixel = document.querySelector('#pixel-board');
+  const quadroPixel = document.querySelector('#pixel-board');
 
   for (let index = 1; index <= tamanho; index += 1) {
-    let linhaPixel = document.createElement('div');
-    let contador = 1;
+    const linhaPixel = document.createElement('div');
     linhaPixel.className = 'newDiv';
 
     for (let i = 1; i <= tamanho; i += 1) {
-      let pixelL = document.createElement('div');
-
+      const pixelL = document.createElement('div');
       pixelL.className = 'pixel';
       pixelL.style.backgroundColor = 'white';
       linhaPixel.appendChild(pixelL);
     }
-
-    contador += 1;
     quadroPixel.appendChild(linhaPixel);
     firstPick();
     paintPixels();
-	}
+  }
 }
 
 quadroDePixels(5);
 
 // Desafio 6
 function firstPick() {
-  let standardColor = document.querySelector('.color');
+  const standardColor = document.querySelector('.color');
 
   standardColor.classList.add('selected');
 }
 
 // Desafio 7
 function selectColor() {
-  let colorList = document.querySelectorAll('.color');
+  const colorList = document.querySelectorAll('.color');
 
-  for (let z = 0; z <colorList.length; z += 1) {
-		let button = colorList[z];
+  for (let z = 0; z < colorList.length; z += 1) {
+    const button = colorList[z];
 
     button.addEventListener('click', function semuso1() {
-			for (let i = 0; i < colorList.length; i += 1) {
-      colorList[i].classList.remove('selected');
+      for (let i = 0; i < colorList.length; i += 1) {
+        colorList[i].classList.remove('selected');
       }
       button.classList.add('selected');
     });
@@ -107,14 +103,14 @@ selectColor();
 
 // Desafio 8
 function paintPixels() {
-  let pixelList = document.querySelectorAll('.pixel');
+  const pixelList = document.querySelectorAll('.pixel');
 
   for (let i = 0; i < pixelList.length; i += 1) {
-		let pixelSelected = pixelList[i];
+    const pixelSelected = pixelList[i];
 
-    pixelSelected.addEventListener('click', function semuso2(){
-      let colorItem = document.querySelector('.selected');
-      let colorSelected = colorItem.style.backgroundColor;
+    pixelSelected.addEventListener('click', function semuso2() {
+      const colorItem = document.querySelector('.selected');
+      const colorSelected = colorItem.style.backgroundColor;
 
       pixelSelected.style.backgroundColor = colorSelected;
     });
@@ -126,37 +122,35 @@ function clearButton() {
   let button = document.getElementById('clear-board');
   let board = document.querySelectorAll('.pixel');
 
-	button.addEventListener('click', function(){
-		for (let i = 0; i < board.length; i += 1) {
-			board[i].style.backgroundColor = 'white';
-		}
-	});
+  button.addEventListener('click', function semuso4() {
+    for (let i = 0; i < board.length; i += 1) {
+      board[i].style.backgroundColor = 'white';
+    }
+  });
 }
 
 clearButton();
 
 // Desafio 10
 function resizeBoard() {
-  let input = document.getElementById('board-size');
-  let button = document.getElementById('generate-board');
-
-  button.addEventListener('click', function semuso3(){
+  const input = document.getElementById('board-size');
+  const button = document.getElementById('generate-board');
+  button.addEventListener('click', function semuso4() {
     const entrada = input.value;
-    let size = document.getElementById('pixel-board')
-
+    const size = document.getElementById('pixel-board');
     if (entrada === '') {
       alert('Board inválido!');
-		} else if(entrada >= 1 && entrada <= 5) {
-			size.innerHTML = '';
-			quadroDePixels(5);
-		} else if(entrada >= 50) {
-			size.innerHTML = '';
-			quadroDePixels(50);
-		} else if(entrada > 5 && entrada < 50) {
-			size.innerHTML = '';
-			quadroDePixels(entrada);
-		}
-	});
+    } else if (entrada >= 1 && entrada <= 5) {
+      size.innerHTML = '';
+      quadroDePixels(5);
+    } else if (entrada >= 50) {
+      size.innerHTML = '';
+      quadroDePixels(50);
+    } else if (entrada > 5 && entrada < 50) {
+      size.innerHTML = '';
+      quadroDePixels(entrada);
+    }
+  });
 }
 
 resizeBoard();
