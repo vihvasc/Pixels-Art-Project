565c4ec4dc3df3a32f6606340182640e1985de92
Arthur <turmessias@gmail.com>
2021-08-14 15:34:45 -0300

    cumpre requisito 4

---
+++ script.js
@@ -0,0 +1,57 @@
+window.onload = function () {
+  function criaLinha() {
+    let elementoOriginal = document.getElementById('pixel-board');
+    let elementoCriado = document.createElement('div');
+    elementoCriado.className = 'pixel';
+    elementoOriginal.appendChild(elementoCriado);
+  }
+
+  criaLinha();
+  criaLinha();
+  criaLinha();
+  criaLinha();
+
+  function criaLinha2() {
+    let elementoOriginal = document.getElementById('pixel-board2');
+    let elementoCriado = document.createElement('div');
+    elementoCriado.className = 'pixel';
+    elementoOriginal.appendChild(elementoCriado);
+  }
+  criaLinha2();
+  criaLinha2();
+  criaLinha2();
+  criaLinha2();
+
+  function criaLinha3() {
+    let elementoOriginal = document.getElementById('pixel-board3');
+    let elementoCriado = document.createElement('div');
+    elementoCriado.className = 'pixel';
+    elementoOriginal.appendChild(elementoCriado);
+  }
+  criaLinha3();
+  criaLinha3();
+  criaLinha3();
+  criaLinha3();
+
+  function criaLinha4() {
+    let elementoOriginal = document.getElementById('pixel-board4');
+    let elementoCriado = document.createElement('div');
+    elementoCriado.className = 'pixel';
+    elementoOriginal.appendChild(elementoCriado);
+  }
+  criaLinha4();
+  criaLinha4();
+  criaLinha4();
+  criaLinha4();
+
+  function criaLinha5() {
+    let elementoOriginal = document.getElementById('pixel-board5');
+    let elementoCriado = document.createElement('div');
+    elementoCriado.className = 'pixel';
+    elementoOriginal.appendChild(elementoCriado);
+  }
+  criaLinha5();
+  criaLinha5();
+  criaLinha5();
+  criaLinha5();
+};
