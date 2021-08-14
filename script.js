c3d811e026ba17e90878083c37b0dc9d933951b5
Adriana <adrianam754@gmail.com>
2021-08-14 00:48:20 -0300

    Adicionada exercios 01 a 06

--- script.js
+++ script.js
@@ -8,19 +8,50 @@
 // }
 // titulo1()
 
-// desafio 2
+// desafio 2, 3
+
 let paletteDiv = document.querySelector('#color-palette')
 
 function paletteList() {
 
   for (let index = 0; index < 4; index += 1) {
     let paletteSon = document.createElement('div');
-    let colors = ['#000', '#4169E1', '#c8a2c8', '#bde0ff',];
-
+    let colors = ['#000', '#e8f7d9', '#fcdec9', '#d3d5e4'];
     paletteSon.className = 'color';
     paletteSon.style.backgroundColor = colors[index];
+    paletteSon.style.border = '1px solid #000';
     paletteDiv.appendChild(paletteSon);
   }
 
 }
 paletteList()
+
+
+// desafio 4 e 5- id pixel-board - class pixel
+
+let pixelsColor = document.querySelector('#color-palette')
+
+function createPixels() {
+  let pixelDiv = document.createElement('div');
+  pixelDiv.id = 'pixel-board';
+  document.body.appendChild(pixelDiv);
+
+  for (let index = 0; index < 25; index += 1) {
+    let pixelLi = document.createElement('li');
+    pixelLi.classList.add('pixel');
+    pixelLi.style.background = 'white';
+    pixelDiv.appendChild(pixelLi)
+  }
+}
+createPixels();
+
+// desafio 6 
+
+let firstChild = document.querySelector('.color');
+firstChild.classList.add('selected');
+
+
+
+
+
+
