ccb6ee14b47f765f328dbdec5b6debd168994910
Ge <gmarcoscardoso@gmail.com>
2021-08-14 16:58:40 -0300

    implmentando funçao que troca de class

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
  const lineBase = 5
  const colunmBase = 5
 
+
+
 function crateSquare() {
   for(let index = 0; index < lineBase; index += 1){
     for (let main = 1; main < colunmBase; main += 1) {
@@ -15,14 +17,25 @@ function crateSquare() {
 crateSquare ()
 
 function getColorButton() {
-//
+  const selectColor = document.querySelectorAll('.color')
+  selectColor[0].classList.add('selected');
+
+  for(let color of selectColor){
+    color.addEventListener('click', function(event) { 
+        const selectColor = document.querySelector('.selected');
+        selectColor.classList.remove('selected');
+        event.target.classList.add('selected');
+    })
+  }
 }
+getColorButton()
 
 function colorTransfer() {
   const selectDiv = document.querySelectorAll('.pixel')
+
   for (let pixel of selectDiv){
     pixel.addEventListener('click', function() {
-      let changecolor = 'red'
+      let changecolor = colordefol
       let colorStandard = document.querySelector('.pixel').style.backgroundColor
       if (pixel.style.backgroundColor === colorStandard) {
         pixel.style.backgroundColor = changecolor
@@ -34,3 +47,4 @@ function colorTransfer() {
 };
 
 colorTransfer()
+
