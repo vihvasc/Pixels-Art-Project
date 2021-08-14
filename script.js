b6acdc2ae45766e6befd2c5233021025a8eff75c
José Erivam <erivamdev@gmail.com>
2021-08-14 10:53:13 -0300

    Adicionando requisito 2 e 3

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+let boxBlackA = document.getElementsByClassName('color')[0];
+boxBlackA.style.backgroundColor = 'black';
+
+let boxBlackB = document.getElementsByClassName('color')[1];
+boxBlackB.style.backgroundColor = 'red';
+
+let boxBlackC = document.getElementsByClassName('color')[2];
+boxBlackC.style.backgroundColor = 'blue';
+
+let boxBlackD = document.getElementsByClassName('color')[3];
+boxBlackD.style.backgroundColor = 'green';
