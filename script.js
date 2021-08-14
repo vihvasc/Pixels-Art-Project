cd588d8802d8baed81d0d98eff8ff8d136b23996
Samuel Pereira <gmr.quest.10@gmail.com>
2021-08-14 17:54:40 -0300

    Requsito 7 concluido

--- script.js
+++ script.js
@@ -5,4 +5,32 @@ window.onload = function() {
   palette[1].style.backgroundColor = '#16db93';
   palette[2].style.backgroundColor = '#048ba8';
   palette[3].style.backgroundColor = '#a4036f';
+
+  palette[0].addEventListener('click', function() {
+    palette[0].className = 'color selected';
+    palette[1].className = 'color'
+    palette[2].className = 'color'
+    palette[3].className = 'color'
+  });
+
+  palette[1].addEventListener('click', function() {
+    palette[1].className += ' selected';
+    palette[0].className = 'color';
+    palette[2].className = 'color';
+    palette[3].className = 'color';
+  });
+
+  palette[2].addEventListener('click', function() {
+    palette[2].className += ' selected';
+    palette[0].className = 'color';
+    palette[1].className = 'color';
+    palette[3].className = 'color';
+  });
+
+  palette[3].addEventListener('click', function() {
+    palette[3].className += ' selected';
+    palette[0].className = 'color';
+    palette[1].className = 'color';
+    palette[2].className = 'color';
+  });
 };
