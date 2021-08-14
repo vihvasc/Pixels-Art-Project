f0126d75cb845c7e0f1842666ce7cd29b08d983b
Lucas <theluks21@live.com>
2021-08-13 22:35:29 -0300

    Adiciona o setimo e oitavo requisito

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-window.onload = function() {
-  console.log('teste')
+window.onload = function windowOnload() {
+  console.log('teste');
 
-  let selector = document.querySelector('.color')
-  selector.className += 'selected'
+  const black = document.querySelector('.black');
+  const red = document.querySelector('.red');
+  const orange = document.querySelector('.orange');
+  const yellow = document.querySelector('.yellow');
+  const pixelBoard = document.getElementById('pixel-board');
+  black.className += 'selected';
+
+  black.addEventListener('click', handleClassChange);
+  red.addEventListener('click', handleClassChange);
+  orange.addEventListener('click', handleClassChange);
+  yellow.addEventListener('click', handleClassChange);
+  pixelBoard.addEventListener('click', handleColorChange);
+
+  // console.log(window.getComputedStyle(orange).getPropertyValue('background-color'))
+
+};
+
+function handleClassChange(event) {
+  if (event.target.className === 'selected') {
+    console.log(event.target.className)
+  } else {
+    const newColor = document.querySelector('.selected');
+    newColor.classList.remove('selected');
+    event.target.className += ' selected';
+  }
+}
+
+function handleColorChange(event) {
+  const colorSelected = document.querySelector('.selected'); 
+  const getColor = window.getComputedStyle(colorSelected).getPropertyValue('background-color');
+  event.target.style.backgroundColor = getColor;
 }
