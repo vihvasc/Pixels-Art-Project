06299317fbf8eeebe579dea40b77e8e94808eae2
Jamez Mayke <jamezmayke@gmail.com>
2021-08-14 15:15:38 -0300

    atualizando e comentando algumas partes do script

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ for (let index = 0; index < getPixel.length; index += 1) {
   getPixel[index].style.backgroundColor = 'white';
 }
 
+// itera sobre o NodeList da classe color, adicionando um evento em cada objeto, chamando uma função
 for (let index = 0; index < getColor.length; index += 1) {
   getColor[index].addEventListener('click', selectedClass);
 }
 
+// event é para acessarmos o target do objeto chamado
 function selectedClass(event) {
+  // itera sobre os elementos, removendo a classe 'selected'
   for (let index = 0; index < getColor.length; index += 1) {
-    getColor[index].classList.remove('selected')
+    getColor[index].classList.remove('selected');
   }
-  event.target.classList.add('selected')
+  // event.target é uma propriedade do objeto para adicionarmos alguma coisa nele, nesse caso uma classe
+  event.target.classList.add('selected');
 }
