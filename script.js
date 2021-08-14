339d620aec6f648506ccec36c3d191c0d174a4cb
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-14 16:22:28 -0300

    Cumprido o requisito 8

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-
 const blackFirst = document.querySelector('.color-green');
 blackFirst.style.backgroundColor = 'rgb(0,0,0)';
 blackFirst.classList.add('selected');
 
+let selectedBox = document.querySelector('.selected');
 let colorsPalette = document.querySelectorAll('.color')
 
 for (let index = 0; index < colorsPalette.length; index += 1) {
-  colorsPalette[index].addEventListener('click', getClick);
+  colorsPalette[index].addEventListener('click', selectColor);
 }
 
-function getClick (originEvent) {
-  // let classes =  originEvent.target.classList;
-  let selectedBox = document.querySelector('.selected');
-  selectedBox.classList.remove('selected');
-  originEvent.target.classList.add('selected');
-  console.log(originEvent.target.classList);
-  console.log(originEvent.target.style.backgroundColor);
+function selectColor (originEvent) {
+  let eventTarget = originEvent.target;
+  for (let index = 0; index < colorsPalette.length; index += 1) {
+    colorsPalette[index].classList.remove('selected');
+  }
+  eventTarget.classList.add('selected');
+}
+
+
+let whiteBoxes = document.querySelectorAll('.pixel');
+for (let index = 0; index < whiteBoxes.length; index += 1) {
+  whiteBoxes[index].addEventListener('click', listenWhiteBoxes);
+}
+
+function listenWhiteBoxes (originBox) {
+  let selectedColor = document.querySelector('.selected');
+  let colorToFill = getComputedStyle(selectedColor).getPropertyValue('background-color');
+  originBox.target.style.backgroundColor = colorToFill;
 }
