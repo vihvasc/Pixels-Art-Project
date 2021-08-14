ee3d7f02483fe8a920a9d132e1f706f31ec0ae2b
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-14 12:08:47 -0300

    Fix lint issues

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Requisito 01
 function createTitle() {
-  let h1 = document.createElement('h1');
+  const h1 = document.createElement('h1');
   h1.id = 'title';
   h1.innerText = 'Paleta de Cores';
   document.querySelector('body').appendChild(h1);
@@ -9,17 +9,17 @@ createTitle();
 
 // Requsito 02 e 03
 function createPalette() {
-  let createList = document.createElement('div');
+  const createList = document.createElement('div');
   createList.id = 'color-palette';
   document.querySelector('body').appendChild(createList);
   addColor();
 }
 
-let colors = ['#000000', '#ffb3ff', '#b3ffcc', '#ccb3ff'];
+const colors = ['#000000', '#ffb3ff', '#b3ffcc', '#ccb3ff'];
 
 function addColor() {
   for (let index = 0; index < colors.length; index += 1) {
-    let colorPallete = document.createElement('div');
+    const colorPallete = document.createElement('div');
     colorPallete.className = 'color';
     colorPallete.style.border = '1px solid black';
     colorPallete.style.display = 'inline-block';
@@ -32,27 +32,26 @@ function addColor() {
 }
 createPalette();
 
-//Requisito 04 e 05
+// Requisito 04 e 05
 
 let gridSize = 5;
 
 function pixelBoard() {
-  let createBoard = document.createElement('div')
-  createBoard.id = 'pixel-board'
+  const createBoard = document.createElement('div');
+  createBoard.id = 'pixel-board';
   document.querySelector('body').appendChild(createBoard);
   createPixels();
 }
 
 function createPixels() {
-  for (let line = 0; line < gridSize; line++) {
-    let createLine = document.createElement("div");
-      createLine.id = 'pixel-line';
-      createLine.style.height = '40px';
-      document.getElementById('pixel-board').appendChild(createLine);
-    for (let cols2 = 0; cols2 < gridSize / gridSize; cols2++) {
-      for (let cols = 0; cols < gridSize; cols++) {
-        let createColumn = document.createElement("div");
-        createColumn.id = 'pixel-column';
+  for (let line = 0; line < gridSize; line += 1) {
+    const createLine = document.createElement('div');
+    createLine.id = 'pixel-line';
+    createLine.style.height = '40px';
+    document.getElementById('pixel-board').appendChild(createLine);
+    for (let cols2 = 0; cols2 < 1; cols2 += 1) {
+      for (let cols = 0; cols < gridSize; cols += 1) {
+        const createColumn = document.createElement('div');
         createColumn.className = 'pixel';
         createColumn.style.border = '1px solid black';
         createColumn.style.display = 'inline-block';
