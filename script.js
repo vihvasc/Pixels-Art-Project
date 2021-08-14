a129bcb0b489d560a5dffd25c155b13bfae42014
Allan Melo <allan-m-silva@hotmail.com>
2021-08-13 23:21:23 -0300

    testes do requisito 9

--- script.js
+++ script.js
@@ -17,6 +17,15 @@ function changePixels (trigger){
     element.style.setProperty("background-color", color)
 }
 
+// function clearBoard(trigger){
+//        let element = trigger.target
+//     let pixels = document.getElementsByClassName('pixel')
+//     for(let counter = 0; counter < pixel.length; counter += 1)
+//     let change = pixel[counter]
+//     change.style.backgroundColor = "white"
+// }
+
+
         let colorPalette = document.querySelector("#color-palette")
         colorPalette.addEventListener("click", changeSelected)
 
@@ -25,3 +34,6 @@ function changePixels (trigger){
             let add = pixel[counter]
             add.addEventListener("click", changePixels)
         }
+
+        // let clearButton = document.querySelector("#clear-board")
+        // clearButton.addEventListener('click', clearBoard)
