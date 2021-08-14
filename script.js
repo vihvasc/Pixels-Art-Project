8214b528015f5ce26845c39adbe23fcd807aeeac
Ronald_D_Bezerra <rebulgame@gmail.com>
2021-08-13 21:13:10 -0300

    requisito 7 e alinhamento dos itens

--- script.js
+++ script.js
@@ -1,22 +1,82 @@
 window.onload = function () { 
 corbranca();
+corpreta()
+cliquei;
+cliquei2;
+cliquei3;
+cliquei4;
 }
 
 function corbranca (){
 let pixels = document.getElementsByClassName("pixel")
 for(i = 0; i < pixels.length; i +=1) {
     pixels[i].style.backgroundColor = "rgb(255, 255, 255)"
+    }
 }
+
+function corpreta (){      
+    cor1.classList.add("selected");
 }
-   
+
+function cliquei(){
+    corArmazenada = cor1.style.backgroundColor
+    if(cor1.className !== "color selected") {
+        cor1.classList.add("selected")
+        cor2.className = "color"
+        cor3.className = "color"
+        cor4.className = "color"
+    }
+    console.log(corArmazenada) 
+}
+
+function cliquei2(){
+    corArmazenada = cor2.style.backgroundColor
+    if(cor2.className !== "color selected") {
+        cor2.classList.add("selected")
+        cor1.className = "color"
+        cor3.className = "color"
+        cor4.className = "color"
+    }
+    console.log(corArmazenada) 
+}
+
+function cliquei3(){
+    corArmazenada = cor3.style.backgroundColor
+    if(cor3.className !== "color selected") {
+        cor3.classList.add("selected")
+        cor1.className = "color"
+        cor2.className = "color"
+        cor4.className = "color"
+    }
+    console.log(corArmazenada) 
+}
+
+function cliquei4(){
+    corArmazenada = cor4.style.backgroundColor
+    if(cor4.className !== "color selected") {
+        cor4.classList.add("selected")
+        cor1.className = "color";
+        cor2.className = "color";
+        cor3.className = "color";
+    }
+    console.log(corArmazenada) 
+}  
+
+let corArmazenada = "";
+
 let cor1 = document.getElementsByClassName("color")[0]
+cor1.addEventListener("click", cliquei)
 cor1.style.backgroundColor = "black";
 
 let cor2 = document.getElementsByClassName("color")[1]
+cor2.addEventListener("click", cliquei2)
 cor2.style.backgroundColor = "red";
 
 let cor3 = document.getElementsByClassName("color")[2]
+cor3.addEventListener("click", cliquei3)
 cor3.style.backgroundColor = "blue";
 
 let cor4 = document.getElementsByClassName("color")[3]
+cor4.addEventListener("click", cliquei4)
 cor4.style.backgroundColor = "purple";
+
