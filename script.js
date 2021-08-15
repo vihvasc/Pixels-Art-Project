a3a555ef396428b977fb5e68175b878c6adfbb56
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-08-15 11:35:41 -0300

    adiciona sexto requisito

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 window.onload = function() {
   let board = document.getElementById('pixel-board')
-  let pixels = document.querySelectorAll('.pixel')
-
   
+  for (let i = 0; i < 25; i++) {
+    let pixel = document.createElement('div')
+    pixel.className = 'pixel'
+    pixel.style.display = 'inline-block'
+    board.appendChild(pixel)
+  }
 
+  function firstColor() {
+    let black = document.querySelector('#black')
+    black.classList.toggle('selected')
+  }
+
+  firstColor()
 }
