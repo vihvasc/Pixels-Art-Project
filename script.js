cbbf09148bfd75fd3e6d4ce07cb38d44fbf0aa9f
iuryroque <iury.roque@hotmail.com>
2021-08-14 22:30:25 -0300

    Exercicio 12 concluido

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const colorPalette = document.getElementById("color-palette");
-const pixelBoard = document.getElementById("pixel-board");
 const buttonClear = document.getElementById("clear-board");
 let pixels = document.getElementsByClassName("pixel");
+const buttonAplic = document.getElementById("button-aplication")
+const styleSheet = document.styleSheets[0];
 
 colorPalette.children[0].style.backgroundColor = "black"
-colorPalette.children[1].style.backgroundColor = "red"
-colorPalette.children[2].style.backgroundColor = "green"
-colorPalette.children[3].style.backgroundColor = "blue"
+colorPalette.children[1].style.backgroundColor = colorRandom();
+colorPalette.children[2].style.backgroundColor = colorRandom();
+colorPalette.children[3].style.backgroundColor = colorRandom();
 
 //A box black começa com a class selected, quando clickar remove a class na antiga e coloca na nova
 
@@ -17,6 +18,7 @@ function addClassSelected(event) {
   event.target.classList.add("selected");
 }
 
+//Adiciona cor aos pixels
 for (const pixel of pixels) {
   pixel.addEventListener("click", addColor);
 }
@@ -25,9 +27,29 @@ function addColor(event) {
   event.target.style.setProperty("background-color", selectedCollor);
 }
 
+//Limpa as cores dos pixels
 buttonClear.addEventListener("click", clearPixels);
 function clearPixels() {
   for (const pixel of pixels) {
     pixel.style.setProperty("background-color", "white");
   }
 }
+
+//Cor aleatoria
+function colorRandom() {
+  let r = Math.floor(Math.random()*255);
+  let g = Math.floor(Math.random()*255);
+  let b = Math.floor(Math.random()*255);
+  let cor = r+g+b;
+  if (cor != 0 || cor != 765)return `rgb(${r},${g},${b})`
+}
+
+// buttonAplic.addEventListener("click", sizePixel);
+// function sizePixel() {
+//   let valueInput = document.getElementById("sizePixel").value + "px";
+//   document.
+
+//   for (const pixel of pixels) {
+    
+//   }
+// }
