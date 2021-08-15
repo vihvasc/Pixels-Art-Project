e17176699df9b41147a7146a218dc89a8f4d4433
BrenoRCBrito <brenorcbrito@gmail.com>
2021-08-15 17:22:57 -0300

    adiciona requisito 4 comppleto

--- script.js
+++ script.js
@@ -10,5 +10,15 @@ window.onload = function() {
         }
     }
     createColorPalette();
+
+    function createPixelBoard() {
+        for (let i = 1; i <= 25; i += 1) {
+            let PixelBoard = document.querySelector('#pixel-board');
+            let pixelDiv = document.createElement('div');
+            pixelDiv.className = 'pixel';
+            PixelBoard.appendChild(pixelDiv);
+        }
+    }
+    createPixelBoard();
 }
 
