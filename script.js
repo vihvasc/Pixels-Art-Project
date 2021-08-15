aa02590cb969d56bc8bf12e390f511402fb5f1ed
Valdênio <valdeniomartinho@gmail.com>
2021-08-15 15:37:41 -0300

    adiciona requisito 8

--- script.js
+++ script.js
@@ -3,19 +3,31 @@ window.onload = function (){
     }
     
     let divColor = document.querySelectorAll(".color");;
-    for (index = 0; index < divColor.length; index += 1){
-        //let divColorSelected = divColor[index];         
+    for (index = 0; index < divColor.length; index += 1){              
         divColor[index].addEventListener('click', verificaClasse);
        
    }  
    function verificaClasse(event){  
-        let selectedClass = document.querySelector('.selected') ;              
+  let selectedClass = document.querySelector('.selected');              
           selectedClass.classList.remove('selected');
-          event.target.classList.add('selected')
+          event.target .classList.add('selected')
        
    }
      
     
+   let selecionaDiv = document.querySelectorAll(".pixel");
+   for (i = 0; i < selecionaDiv.length; i += 1){
+    selecionaDiv[i].addEventListener('click', alteraCor)
+   }
+   
+    function alteraCor(obj){
+        let selectedClass = document.querySelector('.selected');       
+         
+            obj.target.style.backgroundColor = selectedClass.style.backgroundColor; 
+          }
+         
+      
+
+
     
-
-
+    
