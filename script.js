62bf2a0a09988915007e8d2c20d3fe626cb49603
Gabriel Borges <igblborges@gmail.com>
2021-08-15 16:25:23 -0300

    adiciono ex-7

--- script.js
+++ script.js
@@ -21,10 +21,6 @@ function changeColorOfColor(){
 		
 }
 changeColorOfColor();
-
-
-
-
 /*
 ### 4 - Adicione à página um quadro de pixels, com 25 pixels.
 
@@ -48,13 +44,13 @@ changeColorOfColor();
 
 */
 function creatPixel(){
-	let npixel = 25;
+	let nPixel = 25;
 	let pixelBoard = document.querySelector("#pixel-board");
-	for(let cont = 1;cont <= 25; cont += 1){
+	for(let cont = 1;cont <= nPixel; cont += 1){
 	let pixel = document.createElement("div");
 	pixel.classList.add("pixel")
 	pixelBoard.appendChild(pixel);
-				
+			
 	}
 
 }
@@ -80,5 +76,73 @@ function selectedColorBlack(){
 
 }
 selectedColorBlack();
+
+/*
+7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
+
+A classe selected deve ser adicionada à cor selecionada na paleta, ao mesmo tempo em que é removida da cor anteriormente selecionada;
+Somente uma das cores da paleta deve ter a classe selected de cada vez;
+Note que os elementos que deverão receber a classe selected devem ser os mesmos elementos que possuem a classe color, como especificado no requisito 2.
+
+O que será verificado:
+
+Verifica se somente uma cor da paleta de cores tem a classe selected de cada vez
+
+Verifica se os pixels dentro do quadro não têm a classe selected quando são clicados
+
+*/
+
+	 let arrClassColor = document.getElementsByClassName("color");
+	   let arrClassColor0 = arrClassColor[0]; 
+	   let arrClassColor1 = arrClassColor[1]; 
+		 let arrClassColor2 = arrClassColor[2]; 
+	   let arrClassColor3 = arrClassColor[3]; 
+
+		arrClassColor0.addEventListener("click",selectedClassName0);
+		arrClassColor1.addEventListener("click",selectedClassName1);
+		arrClassColor2.addEventListener("click",selectedClassName2);
+		arrClassColor3.addEventListener("click",selectedClassName3);
+
+
+		
+
+
+	function selectedClassName0(){
+		let arrClassColor = document.getElementsByClassName("color");
+		arrClassColor[0].classList.add("selected");
+		arrClassColor[1].classList.remove("selected")
+		arrClassColor[2].classList.remove("selected")
+		arrClassColor[3].classList.remove("selected")
+	}
+		
+	function selectedClassName1(){
+	  let arrClassColor = document.getElementsByClassName("color");
+	  arrClassColor[1].classList.add("selected");
+		arrClassColor[0].classList.remove("selected")
+		arrClassColor[2].classList.remove("selected")
+		arrClassColor[3].classList.remove("selected")
+		
+}
+  function selectedClassName2(){
+		let arrClassColor = document.getElementsByClassName("color");
+		arrClassColor[2].classList.add("selected");
+		arrClassColor[0].classList.remove("selected")
+		arrClassColor[1].classList.remove("selected")
+		arrClassColor[3].classList.remove("selected")
+		
+}
+  function selectedClassName3(){
+		let arrClassColor = document.getElementsByClassName("color");
+		arrClassColor[3].classList.add("selected");
+		arrClassColor[1].classList.remove("selected")
+		arrClassColor[2].classList.remove("selected")
+		arrClassColor[0].classList.remove("selected")
+}
+
+
+
+
+
+
 
 
