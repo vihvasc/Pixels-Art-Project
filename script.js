7b3de0e93bdfccb450cb04ce43f5f6fa71b73865
Flávia Labanca <flavialabanca@gmail.com>
2021-08-15 15:49:06 -0300

    requisto 2

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const paletaDeCores = document.createElement('div');
-paletaDeCores.id = 'color-palette';
-document.body.appendChild(paletaDeCores);
+// const paletaDeCores = document.createElement('div');
+// paletaDeCores.id = 'color-palette';
+// document.body.appendChild(paletaDeCores);
 
 const cores = ['pink', 'yellow', 'brown', 'green'];
 
-for (let index = 0; index < cores.length; index += 1) {
-  const novaCor = document.createElement('div');
-  novaCor.className = 'color';
-  paletaDeCores.appendChild(novaCor);
-}
+// for (let index = 0; index < cores.length; index += 1) {
+//   const novaCor = document.createElement('div');
+//   novaCor.className = 'color';
+//   paletaDeCores.appendChild(novaCor);
+// }
 
-function atribuiCores(cores) {
+function atribuiCores() {
+  const quadrado = document.getElementsByClassName('color');
   for (let index = 0; index < cores.length; index += 1) {
-    const quadrado = document.getElementsByClassName('color');
-    quadrado[index] = cores[index];
+    quadrado[index].style.backgroundColor = cores[index];
+    console.log(quadrado[index]);
   }
 }
+
+atribuiCores();
