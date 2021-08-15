10ec87fb6d351be1130f3d8729fe276597102193
Gabriel Borges <igblborges@gmail.com>
2021-08-15 19:15:30 -0300

    adiciono ex-9

--- script.js
+++ script.js
@@ -160,6 +160,35 @@ function changeColorSelected(click){
 	let color = document.querySelector(".selected").style.backgroundColor
 	click.target.style.backgroundColor = color;
 }
+
+/* 
+9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
+O que será verificado:
+
+Verifica se o botão tem o id denominado clear-board
+
+Verifica se o botão está posicionado entre a paleta de cores e o quadro de pixels
+
+Verifica se o texto do botão é 'Limpar'
+
+Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
+*/
+
+
+let resetButton = document.getElementById("clear-board")
+resetButton.innerText = "Limpar";
+resetButton.addEventListener("click",makeWhiteAgain);
+
+function makeWhiteAgain (){
+	for(let cont = 0;cont <pixels.length;cont += 1 ){
+		pixels[cont].style.backgroundColor = "white";
+	}
+}
+
+
+
+
+
 	
  
 
