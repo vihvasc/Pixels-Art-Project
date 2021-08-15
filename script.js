6af32f3901d405387d3f9ad2f82273a57c8afd8e
israel jeronimo <silvaij16@gmail.com>
2021-08-15 00:08:21 -0300

    4 - Adiciona à página um quadro de pixels, com 25 pixels.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 window.onload = function() {
-  const colorPalette = document.querySelector("#color-palette");
-  const boardLines = document.querySelectorAll(".line");
+  const colorPalette = document.querySelector('#color-palette');
+  const boardLines = document.querySelectorAll('.line');
   const colors = ['black', 'green', 'yellow', 'red'];
-  const initialColor = "white";
-  
+  const initialColor = 'white';
   paletteCreator(colorPalette);
   fillBoard(boardLines);
 
   // paleta de cores
   function paletteCreator(linha) {
     for (let i = 0; i < colors.length; i += 1) {
-      let colorBox = boxCreator("color");
+      let colorBox = boxCreator('color');
 
       colorBox.style.backgroundColor = colors[i];
       linha.appendChild(colorBox);
@@ -19,10 +18,11 @@ window.onload = function() {
 
   function boxCreator(classe) {
     let box = document.createElement("div");
-    box.className = classe; 
+    box.className = classe;
     return box;
   }
-  //  Quadro de pixels
+  // Quadro de pixels
+ 
   function fillBoard(linhas) {
     for(let i = 0; i < linhas.length; i += 1) {
       fillLine(linhas[i]);//  para cada linha cria 5 divs
@@ -31,11 +31,18 @@ window.onload = function() {
 
   function fillLine(boardLine) {
     for (let i = 0; i < boardLines.length; i += 1) {
-      let box = boxCreator("pixel");
+      let box = boxCreator('pixel');
 
       box.style.backgroundColor = initialColor;
       boardLine.appendChild(box);
     }
   }
+};
+// function fillBoard(boardLine) {
+//   for (let i = 0; i < 25; i += 1) {
+//     let box = boxCreator('pixel');
 
-}
+//     box.style.backgroundColor = initialColor;
+//     boardLine.appendChild(box);
+//   }
+// }
