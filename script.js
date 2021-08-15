241e310dc32320a8d12de066fc3cc1ca559a7192
Flávia Labanca <flavialabanca@gmail.com>
2021-08-15 17:15:35 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -2,20 +2,43 @@
 // paletaDeCores.id = 'color-palette';
 // document.body.appendChild(paletaDeCores);
 
-const cores = ['black', 'yellow', 'brown', 'green'];
-
 // for (let index = 0; index < cores.length; index += 1) {
 //   const novaCor = document.createElement('div');
 //   novaCor.className = 'color';
 //   paletaDeCores.appendChild(novaCor);
 // }
 
+const cores = ['black', 'yellow', 'pink', 'brown'];
+
 function atribuiCores() {
   const quadrado = document.getElementsByClassName('color');
   for (let index = 0; index < cores.length; index += 1) {
     quadrado[index].style.backgroundColor = cores[index];
-    console.log(quadrado[index]);
   }
 }
 
-atribuiCores();
+const pegaTudo = document.getElementsByTagName('body');
+const boardSize = 5;
+
+function geraQuadradao() {
+  const board = document.createElement('div');
+  board.id = 'pixel-board';
+
+  for (let linha = 0; linha < boardSize; linha += 1) {
+    const contLinha = document.createElement('div');
+    contLinha.className = 'grid-line';
+    for (let coluna = 0; coluna < boardSize; coluna += 1) {
+      const contColuna = document.createElement('div');
+      contColuna.className = 'pixel';
+
+      contLinha.appendChild(contColuna);
+    }
+    board.appendChild(contLinha);
+  }
+  pegaTudo[0].appendChild(board);
+}
+
+window.onload = function () {
+  atribuiCores();
+  geraQuadradao();
+};
