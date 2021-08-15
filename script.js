fbe999d1c6a819cbb27ac4b79e00ab4b37779d6d
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-15 11:45:33 -0300

    Adiciona comentários e remove linhas desnecessárias

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const root = document.documentElement;
+
+const colors = document.querySelectorAll('.color');
+const pixelBoard = document.querySelector('#pixel-board');
+const clearButton = document.querySelector('#clear-board');
+const input = document.querySelector('#board-size');
+const gridButton = document.querySelector('#generate-board');
+
+const colorsArray = Array.from(colors);
+const colorsArrayLength = colorsArray.length;
+
+// setting black as default selected
+const blackColor = colorsArray[0];
+blackColor.classList.add('selected');
+
 function paintSquare(originEvent) {
   const selectedColorElement = document.querySelector('.selected');
   const selectedColor = getComputedStyle(selectedColorElement).backgroundColor;
@@ -5,30 +20,12 @@ function paintSquare(originEvent) {
   myPixel.style.background = selectedColor;
 }
 
-const root = document.documentElement;
-const pixelBoard = document.querySelector('#pixel-board'); // board
-const gridSizePxCSS = '--grid-size-px';
-const pixelSize = getComputedStyle(root).getPropertyValue('--user-input');
-const pixelSizeNumber = Number(pixelSize);
-const gridSize = pixelSizeNumber * pixelSizeNumber;
-const gridSizeString = `${String(gridSize)}px`;
-const pixels = document.querySelectorAll('.pixel');
-const pixelsArray = Array.from(pixels);
-const pixelsArrayLength = pixelsArray.length;
-const colors = document.querySelectorAll('.color');
-const colorsArray = Array.from(colors);
-const colorsArrayLength = colorsArray.length;
-const blackColor = colorsArray[0];
-const clearButton = document.querySelector('#clear-board');
-const input = document.querySelector('#board-size');
-const gridButton = document.querySelector('#generate-board');
-blackColor.classList.add('selected');
-
+// make 5x5 grid, pixel size = 40px
 const exerciseSize = '40px';
 const sideExerciseNumber = parseInt(exerciseSize, 10); // 40
 const sideGrid = sideExerciseNumber * 5; // 200
 const sideGridString = `${sideGrid}px`; // 200px
-root.style.setProperty(gridSizePxCSS, sideGridString);
+root.style.setProperty('--grid-size-px', sideGridString);
 root.style.setProperty('--user-input-px', exerciseSize);
 for (let index = 1; index <= 25; index += 1) {
   const pixel = document.createElement('div');
@@ -36,6 +33,9 @@ for (let index = 1; index <= 25; index += 1) {
   pixelBoard.appendChild(pixel);
   pixel.addEventListener('click', paintSquare);
 }
+const pixels = document.querySelectorAll('.pixel');
+const pixelsArray = Array.from(pixels);
+const pixelsArrayLength = pixelsArray.length;
 for (let index = 0; index < pixelsArrayLength - 1; index += 1) {
   pixelsArray[index].style.width = exerciseSize;
   pixelsArray[index].style.height = exerciseSize;
@@ -51,11 +51,12 @@ function addSelected(originEvent) {
   element.classList.add('selected');
 }
 
-
-
 function clearBoard() {
-  for (let index = 0; index < pixelsArrayLength; index += 1) {
-    pixelsArray[index].style.background = 'white';
+  const pixelsClearBoard = document.querySelectorAll('.pixel');
+  const pixelsArrayClearBoard = Array.from(pixelsClearBoard);
+  const pixelsArrayLengthClearBoard = pixelsArrayClearBoard.length;
+  for (let index = 0; index < pixelsArrayLengthClearBoard; index += 1) {
+    pixelsArrayClearBoard[index].style.background = 'white';
   }
 }
 
@@ -65,7 +66,7 @@ function addPixels() {
   const newGridSizeNumber = newPixelSizeNumber * newPixelSizeNumber;
   const newGridSizeString = `${String(newGridSizeNumber)}px`;
   root.style.setProperty('--grid-size', String(newGridSizeNumber));
-  root.style.setProperty(gridSizePxCSS, newGridSizeString);
+  root.style.setProperty('--grid-size-px', newGridSizeString);
   root.style.setProperty('--user-input', String(newPixelSizeNumber));
   root.style.setProperty('--user-input-px', newPixelSizeString);
   pixelBoard.innerHTML = '';
@@ -99,7 +100,5 @@ function checkInput() {
 for (let index = 0; index < colorsArrayLength; index += 1) {
   colorsArray[index].addEventListener('click', addSelected);
 }
-
 clearButton.addEventListener('click', clearBoard);
-
 gridButton.addEventListener('click', checkInput);
