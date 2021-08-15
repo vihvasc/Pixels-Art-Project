0bda37b7a41209adfd1913c68784680d37d7ac5c
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-08-15 19:31:50 -0300

    itens 4 e 5 executados

--- script.js
+++ script.js
@@ -16,5 +16,53 @@ return Math.floor(Math.random() * number);
 // transferir numeros aleatorios para gerar as cores
 window.onload = coresAleatorias;
 
-
+// função para definir tamanho do quadrado
+function criandoLinhasColuna() {
+	const input = document.getElementById('board-size');
+	const pixelBoard = document.getElementById('pixel-board');
 	
+	for (let index = 0; index < input.value; index ++) {
+		const tr = document.createElement('div');
+		tr.className = 'tr';
+		pixelBoard.appendChild(tr);
+	
+		for (let count = 0; count < input.value; count ++) {
+			const td = document.createElement('div');
+			td.className = 'pixel td';
+			tr.appendChild(td);
+		}
+	}
+}
+	
+function createTable() {
+	deletePixels();
+	
+	const input = document.getElementById('board-size');
+	
+	if (input.value === '') {
+		alert('Board inválido!');
+	} else if (input.value < 5) {
+		input.value = 5;
+	} else if (input.value > 50) {
+		input.value = 50;
+	}
+	
+	criandoLinhasColuna();
+}
+
+	const generateTable = document.getElementById('generate-board');
+
+	generateTable.addEventListener('click', createTable);
+
+	// função para colorir pixels de acordo com a cor da paleta escolhida
+	const selectPixel = document.querySelectorAll('.pixel');
+
+function changeColor(event) {
+	const colorSelected = document.querySelector('.selected');
+
+	event.target.style.backgroundColor = colorSelected.style.backgroundColor;
+}
+	
+	for (let index = 0; index < selectPixel.length; index += 1) {
+	selectPixel[index].addEventListener('click', changeColor);
+	}
