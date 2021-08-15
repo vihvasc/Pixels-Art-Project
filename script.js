13b81c74324f005eb20f48e2749b3e7cfdd7ed10
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-15 19:50:27 -0300

    Refatora codigo inteiro

--- script.js
+++ script.js
@@ -11,12 +11,15 @@ window.onload = function(){
     selected.style.backgroundColor = "black";
 };
 
+// função retirada do site: https://www.ti-enxame.com/pt/javascript/gerador-de-cores-aleatorias/967183954/;
 function getRandomColor() {
-  let letters = "0123456789ABCDEF";
+  let letters = "0123456789ABCDEF"; 
   let color = "#";
   for (let i = 0; i < 6; i++) {
-   color += letters[Math.floor(Math.random() * 16)];
-  }
+    let randomNumber = Math.random() * letters.length
+    let numberFloor = Math.floor(randomNumber);
+    color += letters[numberFloor];
+  };
   return color;
 };
 
@@ -25,30 +28,18 @@ function createColorPalett (){
     let randomColor = document.createElement("div");
     colorPalett.appendChild(randomColor);
     randomColor.classList.add("color");
+    randomColor.addEventListener("click", selectColor);
     if (i === 0){
       randomColor.style.backgroundColor = "black";
       randomColor.classList.add("selected");
       randomColor.setAttribute("id", "black")
-    } else if (i === 1){
+    } else {
       randomColor.style.backgroundColor = getRandomColor();
-      randomColor.setAttribute("id", "color-two")
-    } else if (i === 2){
-      randomColor.style.backgroundColor = getRandomColor();
-      randomColor.setAttribute("id", "color-three")
-    } else if (i === 3){
-      randomColor.style.backgroundColor = getRandomColor();
-      randomColor.setAttribute("id", "color-four")
     };     
   }; 
 };
 
 createColorPalett();
-
-// parte 4 - Adicione à página um quadro de pixels, com 25 pixels.
-// - O quadro de "pixels" deve ter 5 elementos de largura e 5 elementos de comprimento;
-// - O quadro de "pixels" deve possuir o `id` denominado `pixel-board`, ao passo que cada "pixel" individual dentro do quadro deve possuir a `classe` denominada `pixel`;
-// - A cor inicial dos "pixels" dentro do quadro, ao abrir a página, deve ser branca;
-// - O quadro de "pixels" deve aparecer abaixo da paleta de cores.
 
 function createPixels(){
   for (let i = 0; i < 25; i += 1){
@@ -61,41 +52,16 @@ function createPixels(){
 
 createPixels();
 
-// 7 - Clicar em uma das cores da paleta, faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
-let blackColor = document.getElementById("black");
-let secondColor = document.getElementById("color-two");
-let thirdColor = document.getElementById("color-three");
-let fourthColor = document.getElementById("color-four");
-let backgroundColorOne = blackColor.style.backgroundColor;
-let backgroundColorTwo = secondColor.style.backgroundColor;
-let backgroundColorThree = thirdColor.style.backgroundColor;
-let backgroundColorFour = fourthColor.style.backgroundColor;
-
-blackColor.addEventListener("click", selectColor);
-secondColor.addEventListener("click", selectColor);
-thirdColor.addEventListener("click", selectColor);
-fourthColor.addEventListener("click", selectColor);
-
 function selectColor(event){
-  blackColor.classList.remove("selected");
-  secondColor.classList.remove("selected");
-  thirdColor.classList.remove("selected");
-  fourthColor.classList.remove("selected");
+  document.querySelector(".selected").classList.remove("selected");
   
   event.target.classList.add("selected");
   selected = document.querySelector(".selected");
-  selected.style.backgroundColor = event.target.style.backgroundColor;
 };
-
-//  8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta, faz com que o pixel seja preenchido com a cor selecionada.
 
 function paintPixel(event){
   event.target.style.backgroundColor = selected.style.backgroundColor
 };
-
-// 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
-// 1) Verifica se o botão tem o `id` denominado `clear-board`
-// 2) Verifica se o botão está posicionado entre a paleta de cores e o quadro de pixels
 
 let button = document.querySelector("#clear-board");
 button.addEventListener("click", clearBoard)
@@ -104,5 +70,5 @@ function clearBoard (){
   let allPixels = document.getElementsByClassName("pixel");
   for (let i = 0; i < allPixels.length; i += 1){
     allPixels[i].style.backgroundColor = "white";
-  };  
+  };
 };
