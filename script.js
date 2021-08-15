36ad045c981d93c1fa9c052df02789ca09da7958
ArthurMoreira <arthurmcs96@gmail.com>
2021-08-15 19:09:39 -0300

    Adiciona o requisito 9

--- script.js
+++ script.js
@@ -15,12 +15,20 @@ function setPixelColor(id) {
   pixel.style.backgroundColor = colorSelected.id;
 }
 
+function clearButton() {
+  const buttonClear = document.createElement('button');
+  buttonClear.innerText = 'Limpar';
+  buttonClear.id = 'clear-board';
+  document.body.appendChild(buttonClear);
+  buttonClear.addEventListener('click', clear);
+}
+clearButton();
+
 function squareOfPixels() {
   for (let x = 0; x < 5; x += 1) {
     const line = document.createElement('div');
     line.id = 'pixel-board';
     document.body.appendChild(line);
-    console.log(line);
 
     for (let y = 0; y < 5; y += 1) {
       const column = document.createElement('div');
@@ -29,8 +37,15 @@ function squareOfPixels() {
       column.style.backgroundColor = 'white';
       line.appendChild(column);
       column.addEventListener('click', () => { setPixelColor(column.id); });
-      console.log(column);
     }
   }
 }
 squareOfPixels();
+
+function clear() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    const pixel = pixels[i];
+    pixel.style.backgroundColor = 'white';
+  }
+}
