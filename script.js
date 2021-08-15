10035a0eee76de77752fcb8684262d10cf90ec07
Rafael Colombo <rcolombodev@gmail.com>
2021-08-15 00:45:41 -0300

    Adiciona finalmente requesito 7

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 function colorPalette() {
-  const firstPallete = document.getElementsByClassName('color')[0];
-  const secondPallete = document.getElementsByClassName('color')[1];
-  const thirdPallete = document.getElementsByClassName('color')[2];
-  const fourthPallete = document.getElementsByClassName('color')[3];
-  firstPallete.style.backgroundColor = 'black';
-  secondPallete.style.backgroundColor = '#003F63';
-  thirdPallete.style.backgroundColor = '#F2B138';
-  fourthPallete.style.backgroundColor = '#A1A5A6';
+  const colors = ['black', 'yellow', 'blue', 'grey'];
+  const colorsList = document.getElementsByClassName('color');
+
+  for (let index = 0; index < colorsList.length; index += 1) {
+    const currentColor = colors[index];
+    const currentColorList = colorsList[index];
+    currentColorList.style.backgroundColor = currentColor;
+  }
+}
+
+const pixelGrid = document.body.querySelector('#pixel-board');
+function insertPixels(numberOfPixels) {
+  for (let index = 1; index <= numberOfPixels; index += 1) {
+    const createPixelsList = document.createElement('li');
+    createPixelsList.className = 'pixel';
+    pixelGrid.appendChild(createPixelsList);
+  }
+}
+insertPixels(25);
+
+function selectedColor() {
+  const selectColor = document.querySelectorAll('.color');
+  for (let index = 0; index < selectColor.length; index += 1) {
+    selectColor[index].addEventListener('click', (event) => {
+      const getSelected = document.querySelector('.selected');
+      getSelected.classList.remove('selected');
+      event.target.classList.add('selected');
+    });
+  }
 }
 
 function pixelsArt() {
   colorPalette();
-  const startColor = document.getElementsByClassName('selected')[0];
-  startColor.style.backgroundColor = 'black';
+  selectedColor();
 }
-
 window.onload = pixelsArt;
