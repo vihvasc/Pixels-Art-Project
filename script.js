f9b170a841216b7d52303243941113f133b8c7bb
israel jeronimo <silvaij16@gmail.com>
2021-08-14 22:06:27 -0300

    4 - Adiciona à página um quadro de pixels, com 25 pixels.

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-  window.onload = function() {
-  const boxPaleta = 4;
-  const linhaPaleta = document.querySelector("#color-palette");
-  //const colorBoxs = document.querySelectorAll(".color");
-  // const colors = {cor: 'black', cor: 'green', cor: 'yellow', cor: 'red'}
+window.onload = function() {
+  const colorPalette = document.querySelector("#color-palette");
+  const boardLines = document.querySelectorAll(".line");
+  const colors = ['black', 'green', 'yellow', 'red'];
+  const initialColor = "white";
   
-  lineCreator(linhaPaleta);
-  //toColor();
+  paletteCreator(colorPalette);
+  fillBoard(boardLines);
 
+  // paleta de cores
+  function paletteCreator(linha) {
+    for (let i = 0; i < colors.length; i += 1) {
+      let colorBox = boxCreator("color");
+
+      colorBox.style.backgroundColor = colors[i];
+      linha.appendChild(colorBox);
+    }
+  }
 
   function boxCreator(classe) {
     let box = document.createElement("div");
     box.className = classe; 
     return box;
   }
-  
-  function lineCreator(linha) {
-    const colors = ['black', 'green', 'yellow', 'red'];
-    for (let i = 0; i < colors.length; i += 1) {
-      let colorBox = boxCreator("color");
-
-      colorBox.style.backgroundColor =colors[i];
-      linha.appendChild(colorBox);
+  //  Quadro de pixels
+  function fillBoard(linhas) {
+    for(let i = 0; i < linhas.length; i += 1) {
+      fillLine(linhas[i]);//  para cada linha cria 5 divs
     }
   }
 
-  // function toColor() {
-  //   const colors = ['black', 'green', 'yellow', 'red'];
-  //   const colorBoxs = document.querySelectorAll(".color");
-  //     for (let i = 0; i <= colors.length; i += 1) {
-  //     colorBoxs[i].style.backgroundColor =colors[i];  
-  //   }
-  // }
+  function fillLine(boardLine) {
+    for (let i = 0; i < boardLines.length; i += 1) {
+      let box = boxCreator("pixel");
+
+      box.style.backgroundColor = initialColor;
+      boardLine.appendChild(box);
+    }
+  }
+
 }
