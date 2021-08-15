fdd1fbc4c13359e868a322bfc9689417f931a421
Danilo <dcalado.fisio@yahoo.com.br>
2021-08-15 18:13:56 -0300

    feito requesito 8 e 9 e centralizado a tabela

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let cores = ["black", "yellow", "green", "purple"]
 let corSelecionada = cores[0];
 
 
+
 function createColor(colorName) {
   const color = document.createElement("div")
   color.classList.add("color")
+  if(colorName === corSelecionada){
+    color.classList.add("selected")
+  }
   color.style.backgroundColor = colorName
   color.onclick = function () {
     corSelecionada = colorName
@@ -19,12 +23,26 @@ function createColor(colorName) {
 
 function createPixel() {
   const pixel = document.createElement("div")
+  pixel.style.backgroundColor = "white"
   pixel.classList.add("pixel")
   pixel.onclick = function () {
-    pixel.style.backgroundColor = corSelecionada
+    if(pixel.style.backgroundColor === corSelecionada){
+      pixel.style.backgroundColor = "white"
+    } else{
+      pixel.style.backgroundColor = corSelecionada
+    }
   }
-  return pixel
+     return pixel
 }
+
+function generateBoard(numero){
+  const pixelBoard = document.getElementById("pixel-board")
+  pixelBoard.style.width = `${numero * 42}px` 
+  
+  for (let i = 0; i < numero*numero; i++) {
+    pixelBoard.appendChild(createPixel())
+  }
+  }
 
 
 const colorPallete = document.getElementById("color-palette")
@@ -32,12 +50,17 @@ for (let i = 0; i < cores.length; i++) {
   colorPallete.appendChild(createColor(cores[i]))
 }
 
-const pixelBoard = document.getElementById("pixel-board")
+generateBoard(5)
 
-for (let i = 0; i < 25; i++) {
-  pixelBoard.appendChild(createPixel())
+
+
+const clearButton = document.getElementById("clear-board")
+clearButton.onclick = function(){
+  let pixels = document.getElementsByClassName("pixel")
+  for(let i=0;i < pixels.length;i++){
+    pixels[i].style.backgroundColor = "white"
+  }
 }
-
 
 
 
