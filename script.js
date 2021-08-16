709cf04f255f171528671c53a448b066238bffd9
1lucascq <1lucascq@gmail.com>
2021-08-16 13:52:27 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-window.onload = function () {
+window.onload = function load() {
   const blackColor = document.querySelector('#black');
   blackColor.className = 'color selected';
+
+  function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+  }
+  const colorPalette = document.getElementsByClassName('color');
+  for (let i = 1; i < colorPalette.length; i += 1) {
+    colorPalette[i].style.backgroundColor = randomRGB();
+  }
+  randomRGB();
 };
 
 const pixelBoard = document.querySelector('#pixel-board');
 for (let i = 0; i < 25; i += 1) {
-  let pixel = document.createElement('div');
+  const pixel = document.createElement('div');
   pixel.className = 'pixel';
   pixelBoard.appendChild(pixel);
 }
 
 const colorPalette = document.getElementsByClassName('color');
-for (let i = 0; i < colorPalette.length; i += 1) {
-  colorPalette[i].addEventListener('click', selectionFunction);
-}
 function selectionFunction(e) {
   for (let i = 0; i < colorPalette.length; i += 1) {
     if (colorPalette[i].classList.contains('selected')) {
@@ -22,22 +31,23 @@ function selectionFunction(e) {
   }
   e.target.classList.toggle('selected');
 }
+for (let i = 0; i < colorPalette.length; i += 1) {
+  colorPalette[i].addEventListener('click', selectionFunction);
+}
 
 function paint(e) {
   const selectedColor = document.querySelector('.selected');
   e.target.style.backgroundColor = window.getComputedStyle(selectedColor).backgroundColor;
-  console.log(selectedColor);
 }
 const pixels = document.getElementsByClassName('pixel');
 for (let i = 0; i < pixels.length; i += 1) {
   pixels[i].addEventListener('click', paint);
 }
 
-const button = document.querySelector('#clear-board');
-button.addEventListener('click', clearBoard);
-
 function clearBoard() {
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
   }
 }
+const button = document.querySelector('#clear-board');
+button.addEventListener('click', clearBoard);
