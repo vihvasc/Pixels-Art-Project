7901752341ebecb854a76387bbf1ac40aea5f681
Gustavo <fariaguga@hotmail.com>
2021-08-16 13:23:10 -0300

    Adiciona Requisito 8

--- script.js
+++ script.js
@@ -20,10 +20,34 @@ previousColor.classList.remove("selected");
 event.target.classList.add("selected");
 }
 
+
+
+
+document.querySelector(".pixel").classList.add("selectedP");
+
+let selectedPixel = document.querySelectorAll(".pixel");
+for(let index2 = 0 ; index2 < selectedPixel.length; index2 +=1){
+    selectedPixel[index2].addEventListener("click",clickTablePixel);
+    selectedPixel[index2].addEventListener("click",changeColor);
+}
+
 let colors = document.getElementsByClassName("color");
 for(let index = 0 ; index < colors.length ; index += 1){
     colors[index].addEventListener("click", clickSelectedPixel);
 }
 
 
+function clickTablePixel(event){
+    document.querySelector(".pixel").classList.add(".selectedP");
+    let pixelSelected = document.querySelector(".selectedP");
+    pixelSelected.classList.remove("selectedP");
+    event.target.classList.add("selectedP");
+}
+
+
+function changeColor(){
+    let pallet = document.querySelector(".selected").id;
+    let pixel2 = document.querySelector(".selectedP");
+    pixel2.style.backgroundColor = pallet;
+}
 
