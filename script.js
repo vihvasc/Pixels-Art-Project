e8fb4b208c786edf3213a3a5329f37176e0da047
raphael <raphabarzotto@gmail.com>
2021-08-16 17:26:27 -0300

    entrega os requisitos 2, 3 e 12

--- script.js
+++ script.js
@@ -3,7 +3,50 @@ function criarH1() {
   const novoH1 = document.createElement('h1');
   novoH1.textContent = 'Paleta de Cores';
   novoH1.id = 'title';
-  document.body.appendChild(novoH1);
+  document.getElementById('paleta-de-cores').appendChild(novoH1);
 }
 criarH1();
 
+// 2 - Adicione à página uma paleta de quatro cores distintas.
+
+// referência: https://css-tricks.com/snippets/javascript/random-hex-color/
+function randomColor() {
+  let randomColorString = 'FFFFFF';
+  while (randomColorString === 'FFFFFF') {
+    randomColorString = Math.floor(Math.random()*16777215).toString(16);
+    while (randomColorString.length < 6) {
+      randomColorString = Math.floor(Math.random()*16777215).toString(16);
+    }
+  }
+  return ('#' + randomColorString);
+}
+
+function criarPaleta() {
+  let numeroDeCores = 4;
+  const paletaDeCores = document.createElement('div');
+  paletaDeCores.id = 'color-palette';
+  document.getElementById('paleta-de-cores').appendChild(paletaDeCores);
+
+   let cores = [];
+   cores[0] = '#000000';
+
+  for(let index = 1; index < numeroDeCores; index += 1) {
+    cores[index] = randomColor();
+  }
+
+  for(let index = 0; index < numeroDeCores; index += 1) {
+    const divColorida = document.createElement('div');
+    divColorida.className = 'color';
+
+    divColorida.style.borderColor = 'black';
+    divColorida.style.borderStyle = 'solid';
+    divColorida.style.borderWidth = '1px';
+    divColorida.style.display = 'inline-block';
+    divColorida.style.backgroundColor = cores[index];
+    divColorida.style.width = '30px';
+    divColorida.style.height = '30px';
+
+    paletaDeCores.appendChild(divColorida);
+  }
+}
+criarPaleta();
