9f5ab164eaf5aa94cb436cd5ac6d5e2bb852483c
JoaoVMont <joaovmt1905@gmail.com>
2021-08-16 18:20:21 -0300

    Requisitos 4, 5, 6, 7, 8 e 9

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
 function colorPaletteSquares() {
-    const colors = ['red', 'blue', 'black', 'green']
-    let colorPalette = document.getElementById('color-palette')
+    const colors = ['black', 'blue', 'red', 'green'];
+    let colorPalette = document.getElementById('color-palette');
     for (let index = 0; index < colors.length; index += 1) {
-        let colorsDiv = document.createElement('div')
-        colorsDiv.className = 'color'
-        colorPalette.appendChild(colorsDiv)
-        colorsDiv.style.display = 'inline-block'
-        colorsDiv.style.backgroundColor = colors[index]   
+        let colorsDiv = document.createElement('button');
+        colorsDiv.className = 'color';
+        colorPalette.appendChild(colorsDiv);
+        colorsDiv.style.display = 'inline-block';
+        colorsDiv.style.backgroundColor = colors[index]; 
     }
 }
 colorPaletteSquares()
+
+function whitePixels() {
+    let pixels = document.getElementsByClassName('pixel')
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].addEventListener('onload', function(){
+            pixels[index].style.backgroundColor = 'white'
+        })
+    }
+}
+whitePixels()
+
+function onloadSelected() {
+    let colors = document.querySelectorAll('.color')
+    let blackColor = colors[0]
+    blackColor.classList.add('selected')
+}
+
+onloadSelected()
+
+function selectColor() {
+    const colors = document.getElementsByClassName('color')
+    for (let index = 0; index < colors.length; index += 1) {
+        colors[index].addEventListener('click', function(color) {
+            for (let index = 0; index < colors.length; index += 1) {
+                if (colors[index].classList.contains('selected')){
+                    colors[index].classList.toggle('selected')
+                }
+            }
+        color.target.classList.toggle('selected')
+        })
+    }
+}
+selectColor()
+
+function paintPixel(){
+    let pixels = document.getElementsByClassName('pixel')
+    let selectedColor = document.getElementsByClassName('selected')
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].addEventListener('click', function() {
+            pixels[index].style.backgroundColor = selectedColor[0].style.backgroundColor
+        })
+    }
+}
+
+paintPixel()
+
+let clearButton = document.getElementById('clear-board')
+
+clearButton.addEventListener('click', function() {
+    let pixels = document.getElementsByClassName('pixel')
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].style.backgroundColor = 'white'
+    }
+})
