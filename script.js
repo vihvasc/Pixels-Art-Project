724b4d857510fb5005ecc9be8faac6341365a3ef
Richard Leal <rwsleal@gmail.com>
2021-08-16 18:01:51 -0300

    Requisitos 8 e 9 finalizados

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const pixelBoard = document.getElementById('pixel-board');
 let maxLines = 5;
 let maxColumns = 5;
+var newColor = "black";
+var colorResset = "white";
 
 window.onload = function() {
     document.getElementById('color1').classList.add('selected');
-    for (let index = 0; index < maxLines; index += 1) {
-        for (let i =0; i < maxLines; i +=1) {
-            let pixel = document.createElement('div');
-            pixelBoard.appendChild(pixel).className = "pixel";
-        }
-    }
 };
+
+for (let index = 0; index < maxLines; index += 1) {
+    for (let i =0; i < maxLines; i +=1) {
+        let pixel = document.createElement('div');
+        pixelBoard.appendChild(pixel).className = "pixel";
+    }
+}
 
 let buttons = document.getElementsByTagName('button');
 
@@ -18,14 +21,33 @@ for (index = 0; index < buttons.length; index += 1) {
     buttons[index].addEventListener('click', colorSelection);
 }
 
-console.log(buttons.length)
-
-
-
-function colorSelection() {
+function colorSelection(event) {
+    const newSelection = event.target;
+    newColor = window.getComputedStyle(newSelection).backgroundColor;
     const selectedColor = document.querySelector('.selected');
-    const newColor = event.target
 
     selectedColor.classList.remove('selected');
-    newColor.classList.add('selected');
+    newSelection.classList.add('selected');
 }
+
+function colorAply(obj) {
+    obj.target.style.backgroundColor = newColor;
+}
+
+let pixels = document.getElementsByClassName("pixel");
+for (index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', colorAply);
+}
+
+console.log(typeof(pixels));
+
+function clearBoard () {
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].style.backgroundColor = colorResset;
+    }
+    newColor = "black";
+}
+
+let clearButton = document.getElementById("clear-board");
+clearButton.addEventListener('click', clearBoard);
+
