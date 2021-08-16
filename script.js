839f9fa8f24850dddf1dbe4276678ad2feda52e2
DanMondaini <danielmondaini@outlook.com>
2021-08-16 18:56:35 -0300

    cria funcao que altera cor dos pixels

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ const pixelBoard = document.getElementById('pixel-board')
 const clearButton = document.getElementById('clear-board')
 let backgroundColorSelected = 'black'
 
+function alterarCor(colors){
+    for(let i = 0; i < colors.length; i += 1){
+    let li = document.createElement('li')
+    if (i === 0) {
+    li.classList.add('selected')
+}
+    li.classList.add('color')
+    li.style.backgroundColor = colors[i]
+    colorsUl.appendChild(li)
+  }
+}
+
 clearButton.addEventListener('click', function() {
     let pixelsList = document.getElementsByClassName('pixel')
     for(pixel of pixelsList){
     pixel.style.backgroundColor = 'white'
     }
-    })
+})
+
+alterarCor(colorsPalette)
