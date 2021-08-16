e3c95c02c0bd070873024a6a6ac7ebcb520e3c81
fernandamichetti <f.michetti@hotmail.com>
2021-08-16 14:05:55 -0300

    corrigindo erros de código

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 /*Variaveis */
-const pixelBoard = document.querySelector('#pixel-board');
-let amount = 25;
-const pickingDarkPink = document.getElementById('dark-pink');
-const pickingBrightPink = document.getElementById('bright-pink');
-const pickingDarkBlue = document.getElementById('dark-blue');
-const pickingDarkBlack = document.getElementById('dark-black');
 const clearBoard = document.getElementById('clear-board');
+const pixelBoard = document.querySelector('#pixel-board');
 const allPixels = document.getElementsByClassName('pixel');
+const sectionbuttons = document.getElementById('extras');
+const inputBoard = document.getElementById('board-size');
 const buttonValue = document.getElementById('generate-board');
-const count = document.getElementById('board-size');
-const countValue = count.value;
-const valueBoard = countValue * countValue;
 
 /* Cores aleatórios */
+const secondColor = document.getElementById('secondColor');
+const thirdColor = document.getElementById('thirdColor');
+const firstColor = document.getElementById('firstColor');
+const black = document.getElementById('blackColor');
+
 function randomColors() {
   const randomColors = Math.floor(Math.random() * 255);
   return randomColors;
 }
-
-pickingBrightPink.style.backgroundColor = `rgb(${randomColors()}, ${randomColors()}, ${randomColors()})`;
-pickingDarkPink.style.backgroundColor = `rgb(${randomColors()}, ${randomColors()}, ${randomColors()})`;
-pickingDarkBlue.style.backgroundColor = `rgb(${randomColors()}, ${randomColors()}, ${randomColors()})`;
-pickingDarkBlack.style.backgroundColor = 'black';
+firstColor.style.backgroundColor = `rgb(${randomColors()}, ${randomColors()}, ${randomColors()})`;
+secondColor.style.backgroundColor = `rgb(${randomColors()}, ${randomColors()}, ${randomColors()})`;
+thirdColor.style.backgroundColor = `rgb(${randomColors()}, ${randomColors()}, ${randomColors()})`;
+black.style.backgroundColor = 'black';
 
 /*botão VQV*/
 buttonValue.addEventListener('click', function () {
-  if (countValue === 0) {
-    alert('Board inválido!');
-    return;
+  if (!inputBoard.value) {
+    window.alert('Board inválido!');
   }
-  if (countValue < 5) {
+  if (inputBoard.value >= 5) {
     countValue = 5;
   }
-  if (countValue > 50) {
+  if (countValue < 50) {
     countValue = 50;
   }
 });
 
 /*Criando o board com 25 pixels*/
+
+let amount = 25;
 for (let i = 0; i < amount; i += 1) {
   const pixel = document.createElement('div');
   pixel.className = 'pixel';
@@ -50,37 +49,36 @@ for (let index = 0; index < allPixels.length; index += 1) {
 
 /*Página sempre inicia com o preto selecionado*/
 window.onload = function () {
-  let black = document.getElementById('dark-black');
   black.className = 'color' + ' selected';
 };
 
 /*Mudando a classe de cada cor */
-pickingDarkPink.addEventListener('click', function () {
-  pickingDarkPink.className = 'color' + ' selected';
-  pickingBrightPink.className = 'color';
-  pickingDarkBlue.className = 'color';
-  pickingDarkBlack.className = 'color';
+firstColor.addEventListener('click', function () {
+  firstColor.className = 'color' + ' selected';
+  secondColor.className = 'color';
+  thirdColor.className = 'color';
+  black.className = 'color';
 });
 
-pickingBrightPink.addEventListener('click', function () {
-  pickingBrightPink.className = 'color' + ' selected';
-  pickingDarkPink.className = 'color';
-  pickingDarkBlue.className = 'color';
-  pickingDarkBlack.className = 'color';
+secondColor.addEventListener('click', function () {
+  firstColor.className = 'color';
+  secondColor.className = 'color' + ' selected';
+  thirdColor.className = 'color';
+  black.className = 'color';
 });
 
-pickingDarkBlue.addEventListener('click', function () {
-  pickingDarkBlue.className = 'color' + ' selected';
-  pickingBrightPink.className = 'color';
-  pickingDarkPink.className = 'color';
-  pickingDarkBlack.className = 'color';
+thirdColor.addEventListener('click', function () {
+  firstColor.className = 'color';
+  thirdColor.className = 'color' + ' selected';
+  secondColor.className = 'color';
+  black.className = 'color';
 });
 
-pickingDarkBlack.addEventListener('click', function () {
-  pickingDarkBlack.className = 'color' + ' selected';
-  pickingBrightPink.className = 'color';
-  pickingDarkPink.className = 'color';
-  pickingDarkBlue.className = 'color';
+black.addEventListener('click', function () {
+  black.className = 'color' + ' selected';
+  firstColor.className = 'color';
+  secondColor.className = 'color';
+  thirdColor.className = 'color';
 });
 
 /*Cor clicada é a cor do pixel*/
