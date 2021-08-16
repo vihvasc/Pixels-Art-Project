a93f06890c00425172d5709e8a19ce4b9375b8f7
Arthur <turmessias@gmail.com>
2021-08-16 10:33:18 -0300

    Refatora o codigo para criar pixelboard de forma dinamica

--- script.js
+++ script.js
@@ -1,61 +1,20 @@
 window.onload = function () {
 
-  function criaQuadrado () {
-  function criaLinha() {
-    let elementoOriginal = document.getElementById('pixel-board');
-    let elementoCriado = document.createElement('div');
-    elementoCriado.className = 'pixel';
-    elementoOriginal.appendChild(elementoCriado);
+  function makeBoardLines() {
+    let lines = document.getElementsByClassName('pixel-line');
+
+    for (let i = 0; i < lines.length; i += 1) {
+      createPixels(lines[i]);
+    }
   }
 
-  criaLinha();
-  criaLinha();
-  criaLinha();
-  criaLinha();
+  function createPixels(line) {
+    for (let i = 0; i < 5; i += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      line.appendChild(pixel);
+    }
+  }
 
-  function criaLinha2() {
-    let elementoOriginal = document.getElementById('pixel-board2');
-    let elementoCriado = document.createElement('div');
-    elementoCriado.className = 'pixel';
-    elementoOriginal.appendChild(elementoCriado);
-  }
-  criaLinha2();
-  criaLinha2();
-  criaLinha2();
-  criaLinha2();
-
-  function criaLinha3() {
-    let elementoOriginal = document.getElementById('pixel-board3');
-    let elementoCriado = document.createElement('div');
-    elementoCriado.className = 'pixel';
-    elementoOriginal.appendChild(elementoCriado);
-  }
-  criaLinha3();
-  criaLinha3();
-  criaLinha3();
-  criaLinha3();
-
-  function criaLinha4() {
-    let elementoOriginal = document.getElementById('pixel-board4');
-    let elementoCriado = document.createElement('div');
-    elementoCriado.className = 'pixel';
-    elementoOriginal.appendChild(elementoCriado);
-  }
-  criaLinha4();
-  criaLinha4();
-  criaLinha4();
-  criaLinha4();
-
-  function criaLinha5() {
-    let elementoOriginal = document.getElementById('pixel-board5');
-    let elementoCriado = document.createElement('div');
-    elementoCriado.className = 'pixel';
-    elementoOriginal.appendChild(elementoCriado);
-  }
-  criaLinha5();
-  criaLinha5();
-  criaLinha5();
-  criaLinha5();
-  }
-criaQuadrado()
+  makeBoardLines();
 };
