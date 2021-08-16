1830c816ee97678bf1ed8f849c6783779f880f31
assisMeneghetti <assismfabri@hotmail.com>
2021-08-16 11:16:40 -0300

    adiciona requisto 8

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function colorSelect() {
   }
 }
 colorSelect();
+
+function paintPixels() {
+  const pixels = document.getElementsByClassName("pixel");
+  let previousColor = document.getElementsByClassName("selected");
+  //console.log(previousColor[0]);
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener("click", function (event) {
+      if (previousColor[0].classList.contains("red")) {
+        pixels[index].classList.remove("black", "blue", "green");
+        event.target.classList.add("red");
+      } else if (previousColor[0].classList.contains("blue")) {
+        pixels[index].classList.remove("black", "red", "green");
+        event.target.classList.add("blue");
+      } else if (previousColor[0].classList.contains("green")) {
+        pixels[index].classList.remove("black", "blue", "red");
+        event.target.classList.add("green");
+      } else {
+        pixels[index].classList.remove("red", "blue", "green");
+        event.target.classList.add("black")
+      }
+    });
+  }
+}
+paintPixels();
