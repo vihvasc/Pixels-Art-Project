222dead62c6ad1f6667b3d26f84fcc7579e32f0d
Valdênio <valdeniomartinho@gmail.com>
2021-08-16 11:34:08 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function (){
     
     }
-    
+    // Seleciona cor com a classe selected
     let divColor = document.querySelectorAll(".color");;
     for (index = 0; index < divColor.length; index += 1){              
         divColor[index].addEventListener('click', verificaClasse);
@@ -10,11 +10,11 @@ window.onload = function (){
    function verificaClasse(event){  
   let selectedClass = document.querySelector('.selected');              
           selectedClass.classList.remove('selected');
-          event.target .classList.add('selected')
+          event.target.classList.add('selected')
        
    }
      
-    
+   // Preenche pixel com a cor desejada 
    let selecionaDiv = document.querySelectorAll(".pixel");
    for (i = 0; i < selecionaDiv.length; i += 1){
     selecionaDiv[i].addEventListener('click', alteraCor)
@@ -24,9 +24,21 @@ window.onload = function (){
         let selectedClass = document.querySelector('.selected');       
          
             obj.target.style.backgroundColor = selectedClass.style.backgroundColor; 
+                         
+            
           }
-         
-      
+              
+      // Limpa a cor
+     function limpar(){
+        let pixels = document.querySelectorAll('.pixel');
+        for (let pixel of pixels){
+            pixel.style.backgroundColor = 'white';
+           }
+        console.log(pixels)
+     }
+             
+            
+          
 
 
     
