eedc241f2f356b947639b8446daaf3e141d6dfa2
Arthur <turmessias@gmail.com>
2021-08-16 11:29:45 -0300

    Adiciona evento escutador na paleta para trocar a classe selected de acordo com a cor clicada e outro evento no quadro de pixel para mudar o backgroundcolor para o mesmo do elemento que possui a classe selected, cumprindo requisitos 6, 7 e 8

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-
   function makeBoardLines() {
     let lines = document.getElementsByClassName('pixel-line');
 
@@ -12,9 +11,51 @@ window.onload = function () {
     for (let i = 0; i < 5; i += 1) {
       let pixel = document.createElement('div');
       pixel.className = 'pixel';
-      line.appendChild(pixel);
+
+    pixel.addEventListener('click', function () {
+      // pega elemento classe selected
+      let colorSelected = document.querySelector('.selected');
+      
+      // a cor de fundo do pixel deve se tornar a cor de fundo da classe selected
+      pixel.style.background = colorSelected.style.background;
+    })
+    line.appendChild(pixel);
     }
   }
 
   makeBoardLines();
+
+  function palletListener() {
+    let palletColors = document.getElementsByClassName('color');
+    let currentColor = palletColors[0];
+    let blueColor = palletColors[1];
+    let greenColor = palletColors[2];
+    let yellowColor = palletColors[3];
+
+    currentColor.style.background = 'black';
+    blueColor.style.background = "blue";
+    greenColor.style.background = "green";
+    yellowColor.style.background = "yellow";
+
+    for (let i = 0; i < palletColors.length; i += 1) {
+      let color = palletColors[i];
+
+      color.addEventListener('click', function () {
+        // preciso remover a classe selected da cor atual
+        currentColor.className = currentColor.className.replace(
+          ' selected',
+          ''
+        );
+        // preciso adicionar a classe selected na cor clicada 
+        color.className += ' selected';
+
+        // a cor que eu clicar precisa ser a nova cor atual
+        currentColor = color;
+      });
+    }
+  }
+  palletListener();
+
+
+
 };
