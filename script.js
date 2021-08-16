fa9eeffed28524b703c54d9f9c1de8717aed7454
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-08-16 15:29:00 -0300

    adiciona oitavo requisito

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 window.onload = function () {
   let board = document.getElementById('pixel-board')
-  let pixels = document.getElementsByClassName('color')
+  let palette = document.getElementsByClassName('color')
+  
+  let blackPixel = document.getElementById('black')
+  blackPixel.style.backgroundColor = '#000000'
+  let greenPixel = document.getElementById('green')
+  greenPixel.style.backgroundColor = '#4E6E5D'
+  let whitePixel = document.getElementById('white')
+  whitePixel.style.backgroundColor = '#C9D5B5'
+  let goldPixel = document.getElementById('gold')
+  goldPixel.style.backgroundColor = '#B1740F'
+
 
   for (let i = 0; i < 25; i++) {
-    let pixel = document.createElement('div')
-    pixel.className = 'pixel'
+    var pixel = document.createElement('div')
     pixel.style.display = 'inline-block'
+    pixel.className = 'pixel'
+    pixel.addEventListener('click', changeColor)
     board.appendChild(pixel)
   }
 
@@ -16,18 +27,24 @@ window.onload = function () {
 
   firstColor()
 
-  for (let i = 0; i < pixels.length; i++) {
-    pixels[i].addEventListener('click', handleBoard)
+  for (let i = 0; i < palette.length; i++) {
+    palette[i].addEventListener('click', handleBoard)
   }
 
   function handleBoard(color) {
-    for (let i = 0; i < pixels.length; i++) {
-      if (pixels[i].classList.contains('selected')) {
-        pixels[i].classList.toggle('selected')
+    for (let i = 0; i < palette.length; i++) {
+      if (palette[i].classList.contains('selected')) {
+        palette[i].classList.toggle('selected')
       }
     }
     color.target.classList.toggle('selected')
   }
-  
-  
+
+  function changeColor(pixel) {
+    for (let i = 0; i < palette.length; i++) {
+      if (palette[i].classList.contains("selected")) {
+        pixel.target.style.backgroundColor = palette[i].style.backgroundColor
+      }
+    }
+  }
 }
