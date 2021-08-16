84fd6178ad7e06c0407b9c2cd9fe3c92e97bfdc0
Alex Menezes <alexmenezes.business@outlook.com>
2021-08-16 10:05:42 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ function makePixels(ondeGuarda) {
   }
 }
 
+//  Requisito 7
+
+/* Primeiro passo = dar a classe select dinamicamente para o botão que for clicado
+obs preto começa com a classe select 
+- sei que vai ser um evento  pois quero que isso aconteça quando o usuário faça uma interação
+nesse evento terá uma função que dará a classe select  para o botão
+
+
+*/
+let selected = document.querySelector('.color');
+selected.classList.add('selected');
+
+let buttonSelect = document.querySelectorAll('.color');
+function addClassSelected() {
+  for (let index = 0; index < buttonSelect.length; index += 1) {
+    buttonSelect[index].addEventListener('click', function (event) {
+      selected.classList.remove('selected');
+      event.target.classList.add('selected');
+      selected = event.target;
+    });
+  }
+}
+addClassSelected();
