96cd485967d3e5261d9e58076c4eb2934d4a66c7
1lucascq <1lucascq@gmail.com>
2021-08-16 12:56:14 -0300

    Requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const blackColor = document.querySelector('#black');
   blackColor.className = 'color selected';
-}
+};
 
 const pixelBoard = document.querySelector('#pixel-board');
 for (let i = 0; i < 25; i += 1) {
@@ -21,4 +21,23 @@ function selectionFunction(e) {
     }
   }
   e.target.classList.toggle('selected');
+}
+
+function paint(e) {
+  const selectedColor = document.querySelector('.selected');
+  e.target.style.backgroundColor = window.getComputedStyle(selectedColor).backgroundColor;
+  console.log(selectedColor);
+}
+const pixels = document.getElementsByClassName('pixel');
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', paint);
+}
+
+const button = document.querySelector('#clear-board');
+button.addEventListener('click', clearBoard);
+
+function clearBoard() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
 }
