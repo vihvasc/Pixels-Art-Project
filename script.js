d1720197ef9ddb0353057def40ce8bd2edf37307
Flávia Labanca <flavialabanca@gmail.com>
2021-08-16 11:11:01 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-// const paletaDeCores = document.createElement('div');
-// paletaDeCores.id = 'color-palette';
-// document.body.appendChild(paletaDeCores);
-
-// for (let index = 0; index < cores.length; index += 1) {
-//   const novaCor = document.createElement('div');
-//   novaCor.className = 'color';
-//   paletaDeCores.appendChild(novaCor);
-// }
-
 const cores = ['black', 'yellow', 'pink', 'brown'];
 
+// coloca cores nos quadrados criados
+// mantém o preto como selecionado
+const quadrado = document.getElementsByClassName('color');
+
 function atribuiCores() {
-  const quadrado = document.getElementsByClassName('color');
   for (let index = 0; index < cores.length; index += 1) {
     quadrado[index].style.backgroundColor = cores[index];
   }
@@ -21,25 +14,46 @@ function atribuiCores() {
 const pegaTudo = document.getElementsByTagName('body');
 const boardSize = 5;
 
+// monta o quadradão
 function geraQuadradao() {
   const board = document.createElement('div');
   board.id = 'pixel-board';
-
-  for (let linha = 0; linha < boardSize; linha += 1) {
-    const contLinha = document.createElement('div');
-    contLinha.className = 'grid-line';
+  for (let index = 0; index < boardSize; index += 1) {
+    const linhaQuadradao = document.createElement('div');
+    linhaQuadradao.className = 'grid-line';
     for (let coluna = 0; coluna < boardSize; coluna += 1) {
-      const contColuna = document.createElement('div');
-      contColuna.className = 'pixel';
-
-      contLinha.appendChild(contColuna);
+      const colunaQuadradao = document.createElement('div');
+      colunaQuadradao.className = 'pixel';
+      linhaQuadradao.appendChild(colunaQuadradao);
     }
-    board.appendChild(contLinha);
+    board.appendChild(linhaQuadradao);
   }
   pegaTudo[0].appendChild(board);
+}
+
+let corEscolhida = 'rgba(0,0,0)';
+quadrado[0].style.backgroundColor = corEscolhida;
+
+function escolheCor(event) {
+  const novaCor = getComputedStyle(event.target);
+  for (let index = 0; index < quadrado.length; index += 1) {
+    const item = quadrado[index];
+    item.classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+  corEscolhida = novaCor.backgroundColor;
+}
+
+function adicionaCorNaPaleta() {
+  quadrado[0].classList.add('selected');
+  for (let index = 0; index < quadrado.length; index += 1) {
+    quadrado[index].style.backgroundColor = cores[index];
+    quadrado[index].addEventListener('click', escolheCor);
+  }
 }
 
 window.onload = function () {
   atribuiCores();
   geraQuadradao();
+  adicionaCorNaPaleta();
 };
