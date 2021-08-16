82793015058067763bcd7b54b70d6166852df651
DanMondaini <danielmondaini@outlook.com>
2021-08-16 18:54:55 -0300

    transfere cores para script e deixa preto como cor padrao

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const colorsPalette = ['black', 'red', 'blue', 'green']
+const colorsUl = document.querySelector('.colors-ul')
+const pixelBoard = document.getElementById('pixel-board')
+const clearButton = document.getElementById('clear-board')
+let backgroundColorSelected = 'black'
+
 clearButton.addEventListener('click', function() {
     let pixelsList = document.getElementsByClassName('pixel')
     for(pixel of pixelsList){
