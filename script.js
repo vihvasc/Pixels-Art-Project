0b08dcc262f88513a2130904272814df38ec00cf
Toineto <augusto.vilhena4@gmail.com>
2021-08-17 17:46:38 -0300

    adiciona requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const selectColor = document.getElementsByClassName('color');
+const pixels = document.getElementsByClassName('pixel');
 
 // criar o evento de click//
 function colorPaint(event) {
@@ -10,4 +11,14 @@ function colorPaint(event) {
 }
 for (let i = 0; i < selectColor.length; i += 1) {
   selectColor[i].addEventListener('click', colorPaint);
+}
+function pintar(eventPaint) {
+  const selecionado = document.querySelector('.selected');
+  const cor = window.getComputedStyle(selecionado);
+  const event = eventPaint.target;
+  event.style.backgroundColor = cor.backgroundColor;
+  console.log(cor.backgroundColor);
+}
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', pintar);
 }
