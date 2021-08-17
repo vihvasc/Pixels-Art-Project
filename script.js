7b83b9f086298e288ee4f74955b57f94b35299e5
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-08-17 17:07:49 -0300

    Realiza o segundo requisito

---
+++ script.js
@@ -0,0 +1,17 @@
+const paleta_de_cores = ['black', 'blue', 'red', 'yellow'];
+
+function cores_da_paleta () {
+    const color_divs = document.getElementsByClassName('color');
+
+    for (index=0; index < color_divs.length; index +=1) {
+        const cor_atual = paleta_de_cores[index];
+        const cor_atual_da_div = color_divs[index];
+        cor_atual_da_div.style.backgroundColor = cor_atual;
+
+    }
+}
+
+window.onload = function () {
+    cores_da_paleta ();
+    
+}
