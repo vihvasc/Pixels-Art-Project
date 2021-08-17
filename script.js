beeaff51b4bb2c96c780a4e11f08767fc15b83c2
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-17 20:41:52 -0300

    Adiciona requisito bonus 10

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ let secondColor = document.getElementById('color-palette').children[1];
 let thirdColor = document.getElementById('color-palette').children[2];
 let fourthColor = document.getElementById('color-palette').children[3];
 
+let pixelBoard = document.getElementById('pixel-board');
+
 let buttonClear = document.getElementById('clear-board');
+let buttonAdd = document.getElementById('generate-board');
 
 let arrayColors = [firstColor, secondColor, thirdColor, fourthColor];
 
@@ -23,6 +26,39 @@ function handleSelectColor(event) {
   elementSelectorClass.classList.remove('selected');
   event.target.classList.add('selected');
 }
+
+function handleCreateBoard(number) {
+  for (let count = 0; count < number ** 2; count += 1) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixelBoard.appendChild(pixel);
+
+    pixelBoard.style.width = 42 * number + 'px';
+    pixelBoard.style.height = 42 * number + 'px';
+  }
+}
+let initialNumber = 5;
+handleCreateBoard(initialNumber);
+
+function handleDeleteBoard() {
+  pixelBoard.innerHTML = '';
+}
+
+function handleInput() {
+  let inputValue = parseInt(document.getElementById('board-size').value, 10);
+  if (inputValue > 4 && inputValue < 51) {
+    handleDeleteBoard();
+    handleCreateBoard(inputValue);
+
+    arrayPixels = document.getElementsByClassName('pixel');
+    for (pixel of arrayPixels) {
+      pixel.addEventListener('click', handlePaint);
+    }
+  } else {
+    alert('Board inválido!');
+  }
+}
+buttonAdd.addEventListener('click', handleInput);
 
 for (color of arrayColors) {
   color.addEventListener('click', handleSelectColor);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,15 @@
-console.log(256/64)
+var width = 7,
+    height = 7, // for example
+    boxElem = document.getElementById("box"); // this is the element which will hold your box
+
+for(var i = 0; i < height; i++) {
+    for(var j = 0; j < width; j++) {
+        if(i === 0 || i === height - 1 || j === 0 || j === width - 1 || (i === Math.floor(height / 2) && j === Math.floor(width / 2))) {
+            // check if it's the first or last row or column, or if it's the centre of the box
+            boxElem.innerHTML += "*";
+        } else {
+            boxElem.innerHTML += " "; // otherwise, don't add an asterisk but an empty space
+        }
+    }
+    boxElem.innerHTML += "\n"; // new line
+}
