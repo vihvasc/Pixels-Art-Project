8b8d9aafcb2e70fd5e0ea9fb8d766955b97a0c62
Kdulima <carlosedu151@gmail.com>
2021-08-17 12:35:05 -0300

    Adicionado Requisito 4

---
+++ script.js
@@ -0,0 +1,11 @@
+// Requisito 4
+
+let corpoDoQuadro = document.querySelector("#pixel-board");
+let numberPixel = 25;
+
+for (let index = 0; index < numberPixel; index += 1) {
+    let pixelContainer = document.createElement("div");
+    pixelContainer.className = "pixel";
+    corpoDoQuadro.appendChild(pixelContainer);
+}
+
