fc3bc584f2b3b7d6086840b6ad22af4989b912e1
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-08-17 22:25:12 -0300

    add 11

--- script.js
+++ script.js
@@ -10,20 +10,19 @@ paletaDeCores[3].style.backgroundColor = 'green';
 
 paletaDeCores[0].classList.add('selected');
 
-for (let index = 0; index < 5; index += 1) {
-    createPixel();
-}
-
-function createPixel() {
-    for (let index2 = 0; index2 < 5; index2 += 1) {
+function createPixel(quantidadeDeQuadrados = 5) {
+    telaDePintura.style.width = `${quantidadeDeQuadrados * 42}px`
+    for (let index2 = 0; index2 < quantidadeDeQuadrados * quantidadeDeQuadrados; index2 += 1) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
         telaDePintura.appendChild(pixel);
-    }
+        
+        }
 }
+window.onload = createPixel()
 
 function trocaDoSeletor(origin) {
-let corSelecionada = document.querySelector('.selected')
+    let corSelecionada = document.querySelector('.selected')
     corSelecionada.classList.remove('selected')
     origin.target.classList.add('selected');
 
@@ -38,20 +37,51 @@ for (let i = 0; i < paletaDeCores.length; i += 1) {
 
 let quadroDePixel = document.querySelector('#pixel-board');
 
-function tinta(origem){
+function tinta(origem) {
     let corSelecionada = document.querySelector('.selected').style.backgroundColor
     if (origem.target.classList.contains('pixel')) {
         origem.target.style.backgroundColor = corSelecionada;
-}
+    }
 };
 
 quadroDePixel.addEventListener('click', tinta);
 
-let limpezaDePixel= document.querySelectorAll('.pixel');
 
-function limpar(){
-    for(let i =0 ; i< limpezaDePixel.length; i += 1){
-    limpezaDePixel[i].style.backgroundColor = 'white'
-}
+
+function limpar() {
+    let limpezaDePixel = document.querySelectorAll('.pixel');
+    for (let i = 0; i < limpezaDePixel.length; i += 1) {
+        limpezaDePixel[i].style.backgroundColor = 'white'
+    }
 }
 botãoDeLimpeza.addEventListener('click', limpar)
+
+let valorImput = document.querySelector('#board-size')
+
+
+function apagarBord(){
+    telaDePintura.innerHTML = ""
+}
+
+function alertaBotao() {
+    alert("Board inválido!");
+}
+
+function montar() { 
+   if (valorImput.value <= 50 && valorImput.value >= 5) {
+        apagarBord()
+        quantidadeDeQuadrados = valorImput.value
+        createPixel(quantidadeDeQuadrados)
+        telaDePintura.style.width = `${quantidadeDeQuadrados * 42}px`
+    }else if (valorImput.value >= 51){
+        quantidadeDeQuadrados = 50
+        createPixel(quantidadeDeQuadrados)
+        telaDePintura.style.width = `${quantidadeDeQuadrados * 42}px`
+    } else {
+        alertaBotao()
+    }
+}
+
+let botaoDeAtualização = document.querySelector('#generate-board');
+
+botaoDeAtualização.addEventListener('click', montar)
