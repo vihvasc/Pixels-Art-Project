2d8214f856e20e234273b309752a93134055a989
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-08-17 22:17:00 -0300

    Realiza o segundo e terceiro requisito

---
+++ script.js
@@ -0,0 +1,9 @@
+let cor1 = document.getElementById('color1');
+let cor2 = document.getElementById('color2');
+let cor3 = document.getElementById('color3');
+let cor4 = document.getElementById('color4');
+
+cor1.style.backgroundColor = 'black';
+cor2.style.backgroundColor = 'blue';
+cor3.style.backgroundColor = 'red';
+cor4.style.backgroundColor = 'yellow';
