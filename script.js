f65336577cc65bbedb33f6a8e1f0c85838bc9928
samuel <samuelsoutosilva14@gmail.com>
2021-08-18 11:46:58 -0300

    corrige cores - Requisito 2

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const coresPaleta = document.querySelectorAll(".color")
-const lista = ["#000","#f00","#00f","#008000" ];
+const lista = ["rgba(0,0,0)","rgba(22,45,34)","rgba(189,66,25)","rgba(40,45,20)"];
 
+
+// (Requisito 2)
 function coresCria() {
     for(let index = 0; index < coresPaleta.length; index+= 1) {
-        coresPaleta[index].style.backgroundColor = lista[index];
+        coresPaleta[index].style.backgroundColor = lista[index];   
     }
 }
 
 coresCria()
+
