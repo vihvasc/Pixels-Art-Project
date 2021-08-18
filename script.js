8a6d5bd7e88fd65969525efce7dfe52c6a1396cc
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-08-18 14:22:36 -0300

    Terceiro, Quarto e Quinto requisito concluido

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+  defineLineAndColumns();
+  createPixelsInBoardLine(5, 5);
+};
+
+function defineLineAndColumns() {
+  let button = document.querySelector('#pick-size-btn');
+
+  button.addEventListener('click', function () {
+    columnSize = document.querySelector('#column').value;
+    lineSize = document.querySelector('#line').value;
+
+    createPixelsInBoardLine(columnSize, lineSize);
+  });
+}
+
+function createPixelsInBoardLine(columnSize, lineSize) {
+  let pixelBoard = document.querySelector('#pixel-board');
+  let sizeOfBoard = columnSize * lineSize;
+
+  for (let index = 0; index < sizeOfBoard; index += 1) {
+    let pixels = document.createElement('div');
+
+    if (index % columnSize === 0) {
+      let breakPoint = document.createElement('br');
+      pixelBoard.appendChild(breakPoint);
+    }
+
+    pixels.className = 'pixel';
+    pixelBoard.appendChild(pixels);
+  }
+}
