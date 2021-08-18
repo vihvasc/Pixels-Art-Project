8038fdd726a8e0316d66e42e264f8eb4e06e1e44
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-08-18 19:19:13 -0300

    Sexto e Decimo requisito concluido

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 window.onload = function () {
-  defineLineAndColumns();
-  createPixelsInBoardLine(5, 5);
+  defineSizeOfBoard();
+  createPixelsInBoard(5);
 };
 
-function defineLineAndColumns() {
-  let button = document.querySelector('#pick-size-btn');
+function createPixelsInBoard(pixelsByTheUser) {
+  let pixelBoard = document.querySelector('#pixel-board');
+
+  for (let line = 0; line < pixelsByTheUser; line += 1) {
+    let pixelLine = document.createElement('div');
+    pixelLine.className = 'line';
+    pixelBoard.appendChild(pixelLine);
+
+    for (let column = 0; column < pixelsByTheUser; column += 1) {
+      let pixel = document.createElement('div');
+
+      pixel.className = 'pixel';
+      pixelLine.appendChild(pixel);
+    }
+  }
+}
+
+function defineSizeOfBoard() {
+  let button = document.querySelector('#generate-board');
 
   button.addEventListener('click', function () {
-    columnSize = document.querySelector('#column').value;
-    lineSize = document.querySelector('#line').value;
+    let pixelNumber = document.querySelector('#board-size').value;
+    let pixelBoard = document.querySelector('#pixel-board');
 
-    createPixelsInBoardLine(columnSize, lineSize);
+    if (pixelNumber == '') {
+      alert('Board inválido!');
+    }
+
+    if (pixelNumber < 5) {
+      return;
+    }
+
+    if (pixelNumber > 50) {
+      pixelBoard.innerHTML = ' ';
+      createPixelsInBoard(50);
+      return;
+    }
+
+    pixelBoard.innerHTML = ' ';
+
+    createPixelsInBoard(pixelNumber);
   });
 }
 
-function createPixelsInBoardLine(columnSize, lineSize) {
-  let pixelBoard = document.querySelector('#pixel-board');
-  let sizeOfBoard = columnSize * lineSize;
-
-  for (let index = 0; index < sizeOfBoard; index += 1) {
-    let pixels = document.createElement('div');
-
-    if (index % columnSize === 0) {
-      let breakPoint = document.createElement('br');
-      pixelBoard.appendChild(breakPoint);
-    }
-
-    pixels.className = 'pixel';
-    pixelBoard.appendChild(pixels);
-  }
-}
+function paintPixels() {}
