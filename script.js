e990b6a51c2b8f3aa61ed7d2ce96a7d4f876ddca
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-08-18 01:01:01 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colors = document.getElementsByClassName('color');
+const pixels = document.getElementsByClassName('pixel');
 const black = document.getElementById('black');
 const red = document.getElementById('red');
 const green = document.getElementById('green');
@@ -17,3 +18,14 @@ black.addEventListener('click', handleColors);
 red.addEventListener('click', handleColors);
 green.addEventListener('click', handleColors);
 blue.addEventListener('click', handleColors);
+
+function paint(eventPaint) {
+  const paintpixel = eventPaint.target;
+  const selector = document.querySelector('.selected');
+  const currentColor = window.getComputedStyle(selector);
+  paintpixel.style.backgroundColor = currentColor.backgroundColor;
+  console.log(currentColor.backgroundColor);
+}
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', paint);
+}
