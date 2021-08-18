ff0692e6b62d69c428859e175669ce580f81a823
Antônio Campos <aclcampos@live.com>
2021-08-17 22:02:11 -0300

    Faltando apenas 1 ponto do requisito 5

--- script.js
+++ script.js
@@ -1,45 +1,154 @@
-const pixels = document.getElementsByClassName('pixel');
-let c1 = document.getElementById('cor1');
-c1.style.backgroundColor = 'black'
-let c2 = document.getElementById('cor2');
-c2.style.backgroundColor = 'blue'
-let c3 = document.getElementById('cor3');
-c3.style.backgroundColor = 'green'
-let c4 = document.getElementById('cor4');
-c4.style.backgroundColor = 'yellow'
-let cores = document.getElementsByClassName('color');
-document.getElementById('cor1').classList.add('selected');
+const colorPalette = document.getElementById('color-palette');
+const buttonVQV = document.getElementById('generate-board');
+const buttonClear = document.getElementById('clear-board');
+const inputPixelBoard = document.getElementById('board-size');
+const pixelBoard = document.getElementById('pixel-board');
 
-
-function selectedColor(cor) {
-    for (let i = 0; i < 4; i += 1) {
-    cores[i].classList.remove('selected')
-    }
-  let c = cor.target;
-  c.classList.add('selected');
-   
-}
-
-function pintarPixel2 () {
-    const selected2 = document.querySelector('.selected')
-    const corr = selected2.style.backgroundColor
-    event.target.style.backgroundColor = corr
-}
-
-function pintarPixel () {
-
-  for (let i = 0; i < pixels.length; i += 1) {
-    pixels[i].addEventListener('click', pintarPixel2);
+function createColorPalette() {
+  for (let i = 0; i < 4; i += 1) {
+    const createElement = document.createElement('div');
+    colorPalette.appendChild(createElement);
+    createElement.classList.add('color');
   }
-    
+}
+
+function paletteColors() {
+  const palette = document.getElementsByClassName('color');
+  for (let i = 0; i < palette.length; i += 1) {
+    if (i === 0) {
+      palette[i].style.backgroundColor = 'black';
+      palette[i].classList.add('selected');
+      palette[i].addEventListener('click', getColor)
+    } else {
+      let color = '#';
+      for (let i2 = 1; i2 < 7; i2 += 1) {
+        color += Math.floor(Math.random() * 10);
+      }
+      palette[i].style.backgroundColor = color;
+      palette[i].addEventListener('click', getColor)
+    }
+  }
+}
+
+function getColor() {
+  const palette = document.getElementsByClassName('color');
+  for (let i = 0; i < palette.length; i += 1) {
+    palette[i].classList.remove('selected');
+  }
+  const colorSelected = event.target;
+  colorSelected.classList.add('selected');
+}
+
+function paintPixel() {
+  const colorSelected = document.getElementsByClassName('selected');
+  event.target.style.backgroundColor = colorSelected[0].style.backgroundColor;  
+  console.log(colorSelected[0].style.backgroundColor)
 
 }
 
-pintarPixel ()
-
-
-
-for (let i = 0; i < cores.length; i += 1) {
-  cores[i].addEventListener('click', selectedColor)
+function createBoardInitial() {
+  const n = 5;
+  for (let i = 0; i < n; i += 1) {
+    const createElement = document.createElement('div');
+    pixelBoard.appendChild(createElement);
+    createElement.classList.add('lines');
+  }
+  const lines = document.getElementsByClassName('lines');
+  for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      const createElement = document.createElement('div');
+      lines[i].appendChild(createElement);
+      createElement.classList.add('pixel');
+      createElement.style.backgroundColor = 'white';
+      createElement.addEventListener('click', paintPixel);
+    }
+  }
 }
 
+function createBoard() {
+  const n = inputPixelBoard.value;
+  for (let i = 0; i < n; i += 1) {
+    const createElement = document.createElement('div');
+    pixelBoard.appendChild(createElement);
+    createElement.classList.add('lines');
+  }
+  const lines = document.getElementsByClassName('lines');
+  for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      const createElement = document.createElement('div');
+      lines[i].appendChild(createElement);
+      createElement.classList.add('pixel');
+      createElement.style.backgroundColor = 'white';
+      createElement.addEventListener('click', paintPixel);
+    }
+  }
+}
+
+function createBoard5() {
+  const n = 5;
+  for (let i = 0; i < n; i += 1) {
+    const createElement = document.createElement('div');
+    pixelBoard.appendChild(createElement);
+    createElement.classList.add('lines');
+  }
+  const lines = document.getElementsByClassName('lines');
+  for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      const createElement = document.createElement('div');
+      lines[i].appendChild(createElement);
+      createElement.classList.add('pixel');
+      createElement.style.backgroundColor = 'white';
+      createElement.addEventListener('click', paintPixel);
+    }
+  }
+}
+
+function createBoard50() {
+  const n = 50;
+  for (let i = 0; i < n; i += 1) {
+    const createElement = document.createElement('div');
+    pixelBoard.appendChild(createElement);
+    createElement.classList.add('lines');
+  }
+  const lines = document.getElementsByClassName('lines');
+  for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      const createElement = document.createElement('div');
+      lines[i].appendChild(createElement);
+      createElement.classList.add('pixel');
+      createElement.style.backgroundColor = 'white';
+      createElement.addEventListener('click', paintPixel);
+    }
+  }
+}
+
+function createBoardByButton() {
+  console.log(inputPixelBoard.value);
+  if (inputPixelBoard.value > 0) {
+    pixelBoard.innerHTML = '';
+    if (inputPixelBoard.value < 5) {
+      createBoard5();
+    } else if (inputPixelBoard.value > 50) {
+      createBoard50();
+    } else {
+      createBoard();
+    }
+  } else {
+    alert('Board inválido!');
+  }
+}
+
+function clear() {
+  const pixels = document.getElementsByClassName('pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+
+createBoard();
+createBoardInitial();
+createColorPalette();
+paletteColors();
+
+buttonVQV.addEventListener('click', createBoardByButton);
+buttonClear.addEventListener('click', clear);
