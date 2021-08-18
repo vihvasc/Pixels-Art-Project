161bf0f4335934cef03dd1ce27bab4d2af28ef4d
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-08-17 23:23:00 -0300

    Modifica Requisito 6 e Adiciona Requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const initialColor = document.querySelector('.color');
-initialColor.classList.add('selected');
+const colors = document.getElementsByClassName('color');
+const black = document.getElementById('black');
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
+
+function handleColors(event) {
+  const selectedColor = event.target;
+
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].classList.remove('selected');
+  }
+  selectedColor.classList.add('selected');
+}
+
+black.addEventListener('click', handleColors);
+red.addEventListener('click', handleColors);
+green.addEventListener('click', handleColors);
+blue.addEventListener('click', handleColors);
