fb9bf8e2efe133ba3d6f874f6151a569e978a100
Murilo <murilocarrijoadm@gmail.com>
2021-08-19 20:08:43 -0300

    Adiciona 8º requisito

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 window.onload = function(){
-let colorPalette = document.getElementsByClassName("color");
-for(let index = 0; index < colorPalette.length; index +=1){
-    colorPalette[index].addEventListener("click", recebeClick);
+
 }
 
-function recebeClick(color){
+let colorPalette = document.getElementsByClassName("color");
+let pixel = document.getElementsByClassName("pixel");
+let clear = document.getElementById("clear");
+
+
+for(let index = 0; index < colorPalette.length; index +=1){
+    colorPalette[index].addEventListener("click", receiveClick);
+}
+
+function receiveClick(color){
     for(let index =0; index < colorPalette.length; index += 1){
         if(colorPalette[index].classList.contains("selected")){
-            colorPalette[index].classList.toggle("selected")
+            colorPalette[index].classList.toggle("selected");     
         }
     }
     color.target.classList.toggle("selected")
 }
+
+for(let index = 0; index < pixel.length; index += 1){
+    pixel[index].addEventListener("mouseover", selectColor);
+}
+
+function selectColor(color){
+    if(colorPalette[0].classList.contains("selected")){
+        color.target.style.backgroundColor = "rgb(0, 0, 0)"
+    } else if(colorPalette[1].classList.contains("selected")){
+        color.target.style.backgroundColor = "rgb(212, 14, 14)"
+    } else if(colorPalette[2].classList.contains("selected")){
+        color.target.style.backgroundColor = "rgb(0, 110, 255)"
+    }else if(colorPalette[3].classList.contains("selected")){
+        color.target.style.backgroundColor = "rgb(5, 71, 5)"
+    }
 }
