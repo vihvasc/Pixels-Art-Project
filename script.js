801d1b60eebf5393c0bf7ae6cd35cc90944346d2
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-08-19 12:49:00 -0300

    Adiciona Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 let buttonVQV = document.getElementById('vqv');
 let pixelBoard = document.getElementById('pixel-board');
+console.log(pixelBoard);
+let colorPalette = document.getElementsByClassName('color');
+
+function initialPixelBoard() {
+  for (let index = 0; index < 5; index += 1) {
+    let line = document.createElement('div');
+    line.className = 'line';
+    pixelBoard.appendChild(line);
+    for (let index = 0; index < 5; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.addEventListener('click', paintPixels);
+      line.appendChild(pixel);
+    }
+    //pixelBoard.appendChild(document.createElement('br'));
+  }
+}
+
+function selectColor(event) {
+  // console.log('cheguei na selectcolor');
+  for (let index = 0; index < colorPalette.length; index += 1) {
+    colorPalette[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+for (let index = 0; index < colorPalette.length; index += 1) {
+  colorPalette[index].addEventListener('click', selectColor);
+}
+// pixelBoard.addEventListener('click', paintPixels); Ideia de colocar o escutador na pixel board foi boa, mas ao clicar fora dos pixels tbm pinta a area.
+
+function paintPixels(event) {
+  let selectColor = document.querySelector('.selected');
+  let newColor = window.getComputedStyle(selectColor).backgroundColor;
+  let targetEvent = event;
+  targetEvent.target.style.backgroundColor = newColor;
+}
 
 function changePixelBoarSize() {
   let value = inputBoard.value;
@@ -8,12 +45,13 @@ function changePixelBoarSize() {
 
   if (value > 4 && value < 51) {
     for (let index = 0; index < value; index += 1) {
-        let line = document.createElement('div');
-        line.className = 'line';
-        pixelBoard.appendChild(line);
+      let line = document.createElement('div');
+      line.className = 'line';
+      pixelBoard.appendChild(line);
       for (let index = 0; index < value; index += 1) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
+        pixel.addEventListener('click', paintPixels);
         line.appendChild(pixel);
       }
       //pixelBoard.appendChild(document.createElement('br'));
@@ -24,6 +62,7 @@ function changePixelBoarSize() {
       for (let index = 0; index < 5; index += 1) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
+        pixel.addEventListener('click', paintPixels);
         pixelBoard.appendChild(pixel);
       }
       pixelBoard.appendChild(document.createElement('br'));
@@ -38,5 +77,6 @@ function listenerButtonVQV() {
 window.onload = callAllFunctions;
 
 function callAllFunctions() {
+  initialPixelBoard();
   listenerButtonVQV();
 }
