ac2adf1cddb814d96eb0d57b11b6168ba2043891
dfernan6 <danilo.afernandes@outlook.com>
2021-08-19 11:33:37 -0300

    Verificar resposta #7

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-let tintaPreta = document.getElementById("iCorSelecionada");
-function corInicial(){
-    tintaPreta.style.backgroundColor= "black";
+let corPreto = document.getElementById("iPrimeiraCor");
+let corAzul = document.getElementById("iSegundaCor");
+let corVermelho = document.getElementById("iTerceiraCor");
+let corAmarelo = document.getElementById("iQuartaCor");
+
+function addCorAzul(){  
+corPreto.classList.remove("selected");
+corVermelho.classList.remove("selected");
+corAmarelo.classList.remove("selected");
+corAzul.classList.add("selected");
+}
+function addCorPreto(){
+    corAzul.classList.remove("selected");
+    corVermelho.classList.remove("selected");
+    corAmarelo.classList.remove("selected");
+    corPreto.classList.add("selected");
+}
+function addCorVermelho(){
+    corPreto.classList.remove("selected");
+    corAzul.classList.remove("selected");
+    corAmarelo.classList.remove("selected");
+    corVermelho.classList.add("selected");
+}
+function addCorAmarelo(){
+    corPreto.classList.remove("selected");
+    corAzul.classList.remove("selected");
+    corVermelho.classList.remove("selected");
+    corAmarelo.classList.add("selected");
 }
