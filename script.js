198e35bb069253a493f6bdd5a30940757aea9016
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-08-19 16:05:42 -0300

    Adiciona Requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let buttonVQV = document.getElementById('vqv');
+let buttonVQV = document.getElementById('generate-board');
 let pixelBoard = document.getElementById('pixel-board');
 console.log(pixelBoard);
 let colorPalette = document.getElementsByClassName('color');
+let inputSize = document.getElementById('board-size');
 
 function initialPixelBoard() {
   for (let index = 0; index < 5; index += 1) {
@@ -38,35 +39,82 @@ function paintPixels(event) {
   targetEvent.target.style.backgroundColor = newColor;
 }
 
-function changePixelBoarSize() {
-  let value = inputBoard.value;
+function createPixelBoard() {
+  const { value } = inputSize;
+  for (let index = 0; index < value; index += 1) {
+    let line = document.createElement('div');
+    line.className = 'line';
+    pixelBoard.appendChild(line);
+    for (let index = 0; index < value; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.addEventListener('click', paintPixels);
+      line.appendChild(pixel);
+    }
+    //pixelBoard.appendChild(document.createElement('br'));
+  }
+}
 
+function invalidBoard() {
+  alert('Board Inválido!');
+  for (let index = 0; index < 5; index += 1) {
+    for (let index = 0; index < 5; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.addEventListener('click', paintPixels);
+      pixelBoard.appendChild(pixel);
+    }
+    pixelBoard.appendChild(document.createElement('br'));
+  }
+}
+
+function boardChecks() {
+  let value = inputSize.value;
+
+  if (value < 5) {
+    boardSize5();
+  }
+  if (value > 50) {
+    boardSize50();
+  }
+  if (value >= 5 && value <= 50) {
+    createPixelBoard();
+  }
+}
+
+function boardSize5() {
+  for (let index = 0; index < 5; index += 1) {
+    for (let index = 0; index < 5; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.addEventListener('click', paintPixels);
+      pixelBoard.appendChild(pixel);
+    }
+    pixelBoard.appendChild(document.createElement('br'));
+  }
+}
+
+function boardSize50() {
+  for (let index = 0; index < 50; index += 1) {
+    for (let index = 0; index < 50; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.addEventListener('click', paintPixels);
+      pixelBoard.appendChild(pixel);
+    }
+    pixelBoard.appendChild(document.createElement('br'));
+  }
+}
+
+function changePixelBoarSize() {
+  let value = inputSize.value;
+  console.log('este é o value: ' + value);
   pixelBoard.innerHTML = '';
 
-  if (value > 4 && value < 51) {
-    for (let index = 0; index < value; index += 1) {
-      let line = document.createElement('div');
-      line.className = 'line';
-      pixelBoard.appendChild(line);
-      for (let index = 0; index < value; index += 1) {
-        let pixel = document.createElement('div');
-        pixel.className = 'pixel';
-        pixel.addEventListener('click', paintPixels);
-        line.appendChild(pixel);
-      }
-      //pixelBoard.appendChild(document.createElement('br'));
-    }
+  if (inputSize.value === '') {
+    invalidBoard();
   } else {
-    alert('Tamanho Inválido!');
-    for (let index = 0; index < 5; index += 1) {
-      for (let index = 0; index < 5; index += 1) {
-        let pixel = document.createElement('div');
-        pixel.className = 'pixel';
-        pixel.addEventListener('click', paintPixels);
-        pixelBoard.appendChild(pixel);
-      }
-      pixelBoard.appendChild(document.createElement('br'));
-    }
+    boardChecks();
   }
 }
 
