fc72c002a140e095f5736146d171ee97a4398f10
Miguel_Reis <guelsouzar@gmail.com>
2021-08-19 13:12:49 -0300

    requisto 7

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 // fiz a tabela baseada nesse video https://www.youtube.com/watch?v=-dGWaHA3ud8
+let botao = document.querySelectorAll('button')
+let color = document.querySelectorAll('.color')
+let selecinada = color[0] // pegando o primeiro color
 
 
-
-
- function criarLinhas() {
- 
+function criarLinhas() {
   for (let index = 0; index < 5; index++) {
-    let tabela = document.getElementById('pixel-board');
-    let divs = document.createElement('div')
-    tabela.appendChild(divs)
-       
+    const tabela = document.getElementById('pixel-board');
+    const divs = document.createElement('div');
+    tabela.appendChild(divs);
+
     for (let i = 0; i < 5; i++) {
-    let criar =  document.createElement('section')
-       criar.classList.add('pixel')
-      divs.appendChild(criar)
-      
+      const sessao = document.createElement('section');
+      sessao.classList.add('pixel');
+      divs.appendChild(sessao);
     }
-
   }
+   
+    
+    
+    
+    /* .addEventListener('click',function(){
+    document.querySelectorAll('.pixel').style.color= 'rgb(0,0,0)'
+    }) */
+} 
+
+function selecionar () {
+
+  for (let index = 0; index < color.length; index++) {
+    color[index].addEventListener('click',function(event){
+     selecinada.classList.remove('selected')
+     event.target.classList.add('selected')
+     selecinada = event.target
+     
+   
+    })
+    
+   }
   
-
 }
- criarLinhas()
 
- 
+
+criarLinhas() 
+selecionar()
