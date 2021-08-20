6d6035149b66efb0b40f0eaf3cc3f8f1bcc8b215
JoaoVMont <joaovmt1905@gmail.com>
2021-08-20 20:43:54 -0300

    Adiciona hover no cursor

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
 function colorPaletteSquares() {
-  const colors = ['black', 'blue', 'red', 'green'];
-  const colorPalette = document.getElementById('color-palette');
-  for (let index = 0; index < colors.length; index += 1) {
-    const colorsDiv = document.createElement('button');
-    colorsDiv.className = 'color';
-    colorPalette.appendChild(colorsDiv);
-    colorsDiv.style.display = 'inline-block';
-    colorsDiv.style.backgroundColor = colors[index];
-  }
+    const colors = ['black', 'blue', 'red', 'green'];
+    const colorPalette = document.getElementById('color-palette');
+    for (let index = 0; index < colors.length; index += 1) {
+        const colorsDiv = document.createElement('button');
+        colorsDiv.className = 'color';
+        colorPalette.appendChild(colorsDiv);
+        colorsDiv.style.display = 'inline-block';
+        colorsDiv.style.backgroundColor = colors[index]; 
+    }
 }
 colorPaletteSquares();
 
 function whitePixels() {
-  const pixels = document.getElementsByClassName('pixel');
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('load', () => {
-      pixels[index].style.backgroundColor = 'white';
-    });
-  }
+    let pixels = document.getElementsByClassName('pixel');
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].addEventListener('load', function(){
+            pixels[index].style.backgroundColor = 'white';
+        })
+    }
 }
 whitePixels();
 
 function onloadSelected() {
-  const colors = document.querySelectorAll('.color');
-  const blackColor = colors[0];
-  blackColor.classList.add('selected');
+    let colors = document.querySelectorAll('.color');
+    let blackColor = colors[0];
+    blackColor.classList.add('selected');
 }
 
 onloadSelected();
 
 function selectColor() {
-  const colors = document.getElementsByClassName('color');
-  for (let index = 0; index < colors.length; index += 1) {
-    colors[index].addEventListener('click', (color) => {
-      for (let index = 0; index < colors.length; index += 1) {
-        if (colors[index].classList.contains('selected')) {
-          colors[index].classList.toggle('selected');
-        }
-      }
-      color.target.classList.toggle('selected');
-    });
-  }
+    const colors = document.getElementsByClassName('color');
+    for (let index = 0; index < colors.length; index += 1) {
+        colors[index].addEventListener('click', function(color) {
+            for (let index = 0; index < colors.length; index += 1) {
+                if (colors[index].classList.contains('selected')){
+                    colors[index].classList.toggle('selected');
+                }
+            }
+        color.target.classList.toggle('selected');
+        })
+    }
 }
 selectColor();
 
-function paintPixel() {
-  const pixels = document.getElementsByClassName('pixel');
-  const selectedColor = document.getElementsByClassName('selected');
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('click', () => {
-      pixels[index].style.backgroundColor = selectedColor[0].style.backgroundColor;
-    });
-  }
+function paintPixel(){
+    let pixels = document.getElementsByClassName('pixel');
+    let selectedColor = document.getElementsByClassName('selected');
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].addEventListener('click', function() {
+            pixels[index].style.backgroundColor = selectedColor[0].style.backgroundColor;
+        })
+    }
 }
 
 paintPixel();
 
-const clearButton = document.getElementById('clear-board');
+let clearButton = document.getElementById('clear-board');
 
-clearButton.addEventListener('click', () => {
-  const pixels = document.getElementsByClassName('pixel');
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = 'white';
-  }
-});
+clearButton.addEventListener('click', function() {
+    const pixels = document.getElementsByClassName('pixel');
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].style.backgroundColor = 'white';
+    }
+})
