5516824185200a61d93e7e2a27e87b9e14a60378
Kdulima <carlosedu151@gmail.com>
2021-08-21 01:16:08 -0300

    Correções do Lint e Nova Estilização

--- script.js
+++ script.js
@@ -5,30 +5,30 @@ const numberPixel = 5;
 
 for (let index = 0; index < numberPixel; index += 1) {
   const linePixel = document.createElement('div');
-  linePixel.className = 'line'
-  corpoDoQuadro.appendChild(linePixel);  
-  for (let index = 0; index < numberPixel; index += 1) {
+  linePixel.className = 'line';
+  corpoDoQuadro.appendChild(linePixel);
+  for (let index1 = 0; index1 < numberPixel; index1 += 1) {
     const pixelContainer = document.createElement('div');
     pixelContainer.className = 'pixel';
     linePixel.appendChild(pixelContainer);
-}
+  }
 }
 
 // Requisito 6
 
-let primeiraCor = document.querySelector('.color');
+const primeiraCor = document.querySelector('.color');
 primeiraCor.style.backgroundColor = 'black';
 primeiraCor.className = 'color selected black-color';
 
 // Requisito 7
 
-let coresDaPaleta = document.getElementsByClassName('color');
+const coresDaPaleta = document.getElementsByClassName('color');
   for (let index = 0; index < coresDaPaleta.length; index += 1) {
-    coresDaPaleta[index].addEventListener('click', selecionarCor);
-  }
+  coresDaPaleta[index].addEventListener('click', selecionarCor);
+}
 
 function selecionarCor(evento) {
-  let classeSelected = document.querySelector('.selected');
+  const classeSelected = document.querySelector('.selected');
   classeSelected.classList.remove('selected');
   evento.target.classList.add('selected');
 }
