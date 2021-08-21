1c47d147e17983033f532e327114c5997098122e
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-08-21 15:33:52 -0300

    welckson valentim - finaliza setimo  requisito, referente ao projeto pixels art, apos aprovaçao local

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-window.onload = function() {
-    let blackSelect = document.querySelector('#black');
-    blackSelect.classList.add('selected');
-}
-
-//Referência: requisito desenvolvido com ajuda da Adriana Martins.
 let colorSelect = document.querySelectorAll('.color');
-function selecionaPixel () {
+let paletteColors = document.querySelector('#color-palette');
+function selecionaPixel(event) {
     for (let index = 0; index < colorSelect.length; index += 1) {
-        colorSelect[index].addEventListener('click', function(event) {
-            event.target.classList.add('selected');
-            colorSelect.classList.remove('selected');
-        })
-    }
+            colorSelect[index].classList.remove('selected');
+      }
+    event.target.classList.add('selected'); 
 }
-selecionaPixel()
+paletteColors.addEventListener('click', selecionaPixel);
+
 
 //digesan 9 gotas 
 //enterogermina plus uma vez por dia
