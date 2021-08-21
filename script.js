7dcd14d30cc9b85ba5971e734c78aa4941a7c190
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-20 23:10:31 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function defineTamanhoDaGrade(tamanho){
+function criarGrade(tamanho){
 let grade = document.querySelector("#pixel-board");
 
 for (i = 0; i < tamanho; i += 1){
@@ -6,6 +6,33 @@ for (i = 0; i < tamanho; i += 1){
     pixel.className = "pixel";
     grade.appendChild(pixel);
     pixel.style.backgroundColor = "white"
+    pixel.addEventListener("click", colorirPixel)
 }
 }
-defineTamanhoDaGrade(25)
+criarGrade(25)
+let color = document.getElementsByClassName("color");
+
+
+
+function selecionaCor(event) {
+    const corSelecionada = document.querySelector(".selected");
+    corSelecionada.classList.remove("selected");
+    event.target.classList.add("selected");
+}
+function addEvents() {
+    for (i = 0; i < color.length; i += 1) {
+        color[i].addEventListener("click", selecionaCor);
+    }
+}
+addEvents();
+
+function criaPaleta(cores) {
+    for (i = 0; i < cores.length; i += 1) {
+    let cor = document.createElement("div");
+    const paleta = document.getElementById("color-palette");
+    }
+}
+
+function colorirPixel(event) {
+    event.target.style.backgroundColor = "black";
+}
