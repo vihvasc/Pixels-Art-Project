e2e1fc1ccfd1c72992dcb63482b37f096ec5dc48
aklimak <alexandre.skk@gmail.com>
2021-08-22 20:43:34 -0300

    Adiciona troca de cor no click

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-window.onload = pageLoaded()
+window.onload = pageLoaded();
 
 function pageLoaded() {
     let selectFirstColor = document.getElementsByClassName('color')[0];
-    selectFirstColor.classList.add('selected')
+    selectFirstColor.classList.add('selected');
 }
+
+function switchColor(buttonClicked) {
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+    buttonClicked.classList.add('selected');
+}   
+
+
+
+
