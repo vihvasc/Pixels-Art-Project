dd3a158e499ac5256a87a127da2f4f36ef69573e
dfernan6 <danilo.afernandes@outlook.com>
2021-08-22 15:40:18 -0300

    Alterando o estilo do codigo, retirando comando do HTML e passando tudo para JS

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-let clearButton = document.getElementById("clear-board")
-let backgroundColorSelected = "black"
+let palleteColors = ['rgb(0, 0, 0)', 'rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)'];
+let colorsUl = document.querySelector('.colors-ul');
+let backgroundColorSelected = 'rgb(0,0,0)';
 
-function alterarCor(colors){
-    for(let i = 0; i < colors.length; i += 1){
+function addClassAndBackgroundToColors(colors){
+  for(let index = 0; index < colors.length; index += 1){
     let li = document.createElement("li")
-    if (i === 0) {
-    li.classList.add("selected")
-}
+    if (index === 0) {
+        li.classList.add('selected')
+    }
     li.classList.add("color")
-    li.style.backgroundColor = colors[i]
+    li.style.backgroundColor = colors[index]
     colorsUl.appendChild(li)
   }
 }
 
-clearButton.addEventListener("click", function() {
-    let pixelsList = document.getElementsByClassName("pixel")
-    for(pixel of pixelsList){
-    pixel.style.backgroundColor = "white";
+function creatPixelsBoard(width, heigth){
+    let boardElements = document.getElementsByClassName("pixel")
+    let pixelBoard = document.getElementById("pixel-board")
+    if(boardElements.length === 0){
+      for(let line = 1; line <= heigth; line += 1){
+        let lineOfPixels = document.createElement("div");
+        lineOfPixels.classList.add("pixel-line");
+        pixelBoard.appendChild(lineOfPixels);
+        for(let column = 1; column <= width; column += 1){
+          let block = document.createElement("div");
+          block.classList.add("pixel");
+          block.style.backgroundColor = "white";
+          lineOfPixels.appendChild(block)
+        }
+      }
     }
-    }) 
+  }
+
+  colorsUl.addEventListener('click', function(e) {
+    let element = e.target
+    backgroundColorSelected = element.style.backgroundColor
+  
+    let previousSelected = document.querySelector('.selected')
+    previousSelected.classList.remove('selected')
+  
+    element.classList.add('selected')
+    console.log(element.classList)
+  })
+  
+  addClassAndBackgroundToColors(palleteColors)
+  creatPixelsBoard(5, 5)
