274493f9b6aeecc0f768d6c49f13940bacfcb65a
joaocla <jc.claudio.bento@gmail.com>
2021-08-22 15:54:27 -0300

    Adiciona botao 'Limpar'

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function setPenColor(color){
 function setPixelColor(pixel){
   pixel.style.backgroundColor = penColor;
 }
+
+function cleanPixels(){
+  var pixels = document.querySelectorAll(".pixel");
+  for(let pixel of pixels){
+    pixel.style.backgroundColor = "#fff";
+  }
+}
