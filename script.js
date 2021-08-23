ef398b5df5ed917c671b9b7561d0a5f8b5cf39bc
dionelia <dionelia.pereira@gmail.com>
2021-08-23 19:29:41 -0300

    adciona requisito 6 e 7

--- script.js
+++ script.js
@@ -22,3 +22,52 @@ function createBoard(numberOfLinesCB, numberOfColumnsCB) {
 }
 
 createBoard(numberOfLines, numberOfColumns);
+
+
+let colorPixel = document.querySelectorAll('.pixel');
+
+function colorsBox(){
+for (let index = 0; index < colorPixel.length; index += 1){
+  colorPixel[index].addEventListener('click',function(event){
+    let acesso = colorsSelect.style.backgroundColor;
+    event.target.style.backgroundColor = acesso;
+  })
+}
+}
+colorsBox();
+
+let firstChild = document.querySelector('.color');
+firstChild.classList.add('selected');
+let colorsSelect = document.querySelector('.color');
+colorsSelect.classList.add('selected');
+
+
+let seleciona = document.querySelectorAll('.color')
+function selectClicks(){
+  for (let index = 0; index < seleciona.length; index +=1){
+    seleciona[index].addEventListener('click',function(event) {
+      colorsSelect.classList.remove('selected');
+      event.target.classList.add('selected');
+      colorsSelect = event.target;
+    });
+  }
+}
+selectClicks()
+
+function clearButton(){
+let buttonClear = document.createElement('button');
+buttonClear.id = 'clear-board';
+buttonClear.innerText = 'Limpar';
+let pixelBoard = document.getElementById('pixel-board');
+
+document.body.insertBefore(buttonClear, pixelBoard);
+buttonClear.addEventListener('click', function(){
+  for(let index = 0;index < colorPixel.length; index += 1){
+    colorPixel[index].style.backgroundColor = 'white';
+    }
+});
+}
+
+clearButton();
+
+
