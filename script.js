169194fddf21947e0d98c7162232a639e770bc3f
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-08-23 17:29:43 -0300

    lint aspas

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-const pixelBoard = document.getElementById("pixel-board");
+const pixelBoard = document.getElementById('pixel-board');
 for (let i = 0; i < 25; i += 1) {
-    const grid = document.createElement("div");
-    grid.className = "pixel";
+    const grid = document.createElement('div');
+    grid.className = 'pixel';
     pixelBoard.appendChild(grid);
-    console.log("olha eu aqui")
-
 }
-document.getElementById("blue").addEventListener("click", clickColor)
-document.getElementById("black").addEventListener("click", clickColor)
-document.getElementById("red").addEventListener("click", clickColor)
-document.getElementById("purple").addEventListener("click", clickColor)
+document.getElementById('blue').addEventListener('click', clickColor);
+document.getElementById('black').addEventListener('click', clickColor);
+document.getElementById('red').addEventListener('click', clickColor);
+document.getElementById('purple').addEventListener('click', clickColor);
 
 function clickColor (clickEvent) {
-    const selection = document.querySelector(".selected");
-    selection.classList.remove("selected");
-    clickEvent.target.classList.add("selected");
+    const selection = document.querySelector('.selected');
+    selection.classList.remove('selected');
+    clickEvent.target.classList.add('selected');
 }
-
