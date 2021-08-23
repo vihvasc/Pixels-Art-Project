ac4b8063bbca817260be0157844b08794e971596
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-08-23 17:06:40 -0300

    nem acredito q to conseguindo

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// let quadro = document.getElementsById("pixel-board");
-//  for (let i = 0; i < 25; i += 1) {
-//     let pixel = document.createElement("div");
-//     pixel.className = "pixel"
-// }
+const pixelBoard = document.getElementById("pixel-board");
+for (let i = 0; i < 25; i += 1) {
+    const grid = document.createElement("div");
+    grid.className = "pixel";
+    pixelBoard.appendChild(grid);
+    console.log("olha eu aqui")
+
+}
+
+function clickColor (clickEvent) {
+    const selection = document.querySelector(".selected");
+    selection.classList.remove("selected");
+    clickEvent.target.classList.add("selected");
+}
