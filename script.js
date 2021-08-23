cc53fb058212dd3dc560c62742d8a24e72faca3f
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-08-23 10:05:51 -0300

    welckson valentim - reduz os itens alertados pelo lint, referente ao projeto pixels art

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-let colorSelect = document.querySelectorAll('.color');
-let paletteColors = document.querySelector('#color-palette');
+const colorSelect = document.querySelectorAll('.color');
+const paletteColors = document.querySelector('#color-palette');
 function selecionaPixel(event) {
-    for (let index = 0; index < colorSelect.length; index += 1) {
-            colorSelect[index].classList.remove('selected');
-      }
-    event.target.classList.add('selected'); 
+  for (let index = 0; index < colorSelect.length; index += 1) {
+    colorSelect[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
 }
 paletteColors.addEventListener('click', selecionaPixel);
 
-let pixelBoard = document.querySelector('#pixel-board');
-let selectedColors = document.querySelector('.selected');
- function colorPixel(event) {
+const pixelBoard = document.querySelector('#pixel-board');
+
+function colorPixel(event) {
   if (colorSelect[0].classList.contains('selected')) {
     event.target.style.backgroundColor = 'black';
-  }
+    }
   else if (colorSelect[1].classList.contains('selected')) {
-  event.target.style.backgroundColor = 'red';
-  }
+    event.target.style.backgroundColor = 'red';
+    }
   else if (colorSelect[2].classList.contains('selected')) {
-  event.target.style.backgroundColor = 'blue';
-  }
+    event.target.style.backgroundColor = 'blue';
+    }
   else if (colorSelect[3].classList.contains('selected')) {
-  event.target.style.backgroundColor = 'green';
-  }
- }
+    event.target.style.backgroundColor = 'green';
+    }
+}
 pixelBoard.addEventListener('click', colorPixel);
 
-let buttonClearColors = document.querySelector('#clear-board');
-let pixelBoardColor = document.querySelector('#pixel-board');
-buttonClearColors.addEventListener('click', function() {
-  let backgroundColorPadrao = 'white';
-  let pixelsColorsTable = document.querySelectorAll('.pixel');
+const buttonClearColors = document.querySelector('#clear-board');
+const pixelBoardColor = document.querySelector('#pixel-board');
+buttonClearColors.addEventListener('click', function () {
+  const backgroundColorPadrao = 'white';
+  const pixelsColorsTable = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixelsColorsTable.length; index += 1) {
     if (pixelsColorsTable[index].style.backgroundColor !== backgroundColorPadrao) {
-        pixelsColorsTable[index].style.backgroundColor = backgroundColorPadrao;
+      pixelsColorsTable[index].style.backgroundColor = backgroundColorPadrao;
     }
   }
 });
-
-/* digesan 9 gotas 
-enterogermina plus uma vez por dia */
