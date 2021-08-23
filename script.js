28bbcf6ccf6ef2f3d2e15a15bb2679299952527d
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-08-22 22:34:20 -0300

    Tenta passar nos requisitos 9, 10 e  bônus 11

--- script.js
+++ script.js
@@ -1,40 +1,88 @@
-// const pixelBoard = document.getElementById('pixel-board');
+/* eslint-disable sonarjs/cognitive-complexity */
+/* eslint-disable max-lines-per-function */
 
-// for (let j = 0; j < 5; j += 1) {
-//  for (let i = 0; i < 5; i += 1) {
-//    const linesClass = document.createElement('div');
-//    linesClass.classList.add('pixel');
-//
-//    pixelBoard.appendChild(linesClass);
-//  }
+window.onload = function pixelArt() {
+  const pixelBoard = document.getElementById('pixel-board');
 
-// }
+  let tableSize = 5;
 
-const colorPalette = document.getElementsByClassName('color');
-const pixel = document.getElementsByClassName('pixel');
+  function table() {
+    for (let j = 0; j < tableSize; j += 1) {
+      const linesClass = document.createElement('div');
 
-function paintSelectedColor(event) {
-  const eventSelected = event;
-  const color = ['black', 'red', 'blue', 'green'];
-  for (let i = 0; i < colorPalette.length; i += 1) {
-    if (colorPalette[i].classList.contains('selected')) {
-      eventSelected.target.style.backgroundColor = color[i];
+      pixelBoard.appendChild(linesClass);
+      for (let i = 0; i < tableSize; i += 1) {
+        const ColumnClass = document.createElement('div');
+
+        ColumnClass.classList.add('pixel');
+
+        pixelBoard.appendChild(ColumnClass);
+      }
     }
   }
-}
-for (let i = 0; i < pixel.length; i += 1) {
-  pixel[i].addEventListener('click', paintSelectedColor);
-}
+  table();
 
-function selectedColor(event) {
-  for (let i = 0; i < colorPalette.length; i += 1) {
-    if (colorPalette[i].classList.contains('selected')) {
-      colorPalette[i].classList.remove('selected');
-    }
+  function tableNumberSize() {
+    const inputLine = document.querySelector('#board-size').value;
+    const invalidBoard = 'Board inválido!';
+
+    console.log(inputLine);
+    if (inputLine === null) {
+      alert(invalidBoard);
+    } else if (inputLine < 5) {
+      tableSize = 5;
+      alert(invalidBoard);
+    } else if (inputLine > 50) {
+      tableSize = 50;
+      alert(invalidBoard);
+    } else tableSize = inputLine;
+    console.log(inputLine);
+    pixelBoard.innerHTML = '';
+    table();
   }
-  event.target.classList.add('selected');
-  paintSelectedColor(event);
-}
-for (let i = 0; i < colorPalette.length; i += 1) {
-  colorPalette[i].addEventListener('click', selectedColor);
-}
+
+  function paint() {
+    const colorPalette = document.getElementsByClassName('color');
+    const pixel = document.getElementsByClassName('pixel');
+
+    function paintSelectedColor(event) {
+      const eventSelected = event;
+      const color = ['black', 'red', 'blue', 'green'];
+      for (let i = 0; i < colorPalette.length; i += 1) {
+        if (colorPalette[i].classList.contains('selected')) {
+          eventSelected.target.style.backgroundColor = color[i];
+        }
+      }
+    }
+
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].addEventListener('click', paintSelectedColor);
+    }
+
+    function selectedColor(event) {
+      for (let i = 0; i < colorPalette.length; i += 1) {
+        if (colorPalette[i].classList.contains('selected')) {
+          colorPalette[i].classList.remove('selected');
+        }
+      }
+      event.target.classList.add('selected');
+      paintSelectedColor(event);
+    }
+
+    for (let i = 0; i < colorPalette.length; i += 1) {
+      colorPalette[i].addEventListener('click', selectedColor);
+    }
+
+    function erase() {
+      for (let i = 0; i < pixel.length; i += 1) {
+        pixel[i].style.backgroundColor = 'white';
+      }
+    }
+    const confirmLine = document.querySelector('#generate-board');
+    const eraseBoard = document.querySelector('#clear-board');
+    confirmLine.addEventListener('click', tableNumberSize);
+    confirmLine.addEventListener('click', paint);
+    eraseBoard.addEventListener('click', erase);
+  }
+  paint();
+};
