64fdfd554314c0fc7a051890974833cd36c245fb
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-08-23 16:10:02 -0300

    ta indo uhulll

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let quadro = document.getElementsById("pixel-board");
- for (let i = 0; i < 25; i += 1) {
-    let pixel = document.createElement("div");
-    pixel.className = "pixel"
-}
+// let quadro = document.getElementsById("pixel-board");
+//  for (let i = 0; i < 25; i += 1) {
+//     let pixel = document.createElement("div");
+//     pixel.className = "pixel"
+// }
