677b95ba5ce0cb6a54e54cf23525a5b4c7d6885f
vinicius23-dev <nicius.cadedes@gmail.com>
2021-08-24 09:39:25 -0300

    cores aleatorias + retificação "uma cor de cada vez - selected"

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
+//FUNÇÃO LIMPAR
 const bottonLimpar = document.getElementById('clear-board');
 bottonLimpar.addEventListener('click', Limpar);
 
 function Limpar(){
     const allIn = document.getElementsByClassName('pixel');
-    allIn.style.backgroundColor = 'red';}
-    //document.body.style.backgroundColor = 'green'; }
+          
+        for (let i = 0; i < allIn.length; i += 1) {
+          allIn[i].style.backgroundColor = 'white';
+        }
+    }    
+      
+      //CORES ALEATORIAS, EXCETO O PRETO
+      function coresAleatorias() {
+        let paletaRed = document.querySelector('#red');
+        let paletaVerde = document.querySelector('#green');
+        let paletaAzul = document.querySelector('#blue');
+        let tresCores = [paletaRed, paletaVerde, paletaAzul];
+      
+        for (let index = 0; index < tresCores.length; index++) {
+          let resultadoUm = Math.random() * 255;
+          let resultadoDois = Math.random() * 255;
+          let resultadoTres = Math.random() * 255;
+      
+          tresCores[index].style.backgroundColor = 'rgb(' + resultadoUm + ', ' + resultadoDois + ', ' + resultadoTres + ')';
+        }
+      }
+      //Dos testes que fiz no npm test não estava conseguindo
+      //fazer a função carregar com o window.onload corretamente
+      coresAleatorias();
 
-    let color = document.getElementsByClassName("color");
 
-    function selecionaCor(event) {
-        const corSelecionada = document.querySelector(".selected");
-        corSelecionada.classList.remove("selected");
+
+    //Pegando cor selecionada e .select
+    let corPaleta = document.getElementsByClassName("color");
+
+    function selecionarCor(event) {
+        const SelecionaCor = document.querySelector(".selected");
+        SelecionaCor.classList.remove("selected");
         event.target.classList.add("selected");
     }
 
-    function addEvents() {
-        for (i = 0; i < color.length; i += 1) {
-            color[i].addEventListener("click", selecionaCor);
+    function corClick() {
+        for (i = 0; i < corPaleta.length; i += 1) {
+            corPaleta[i].addEventListener("click", selecionarCor);
         }
     }
-    addEvents();
+    corClick();
